e1de2a2547593fbaf100ad1d04c4d00ad291ffa1
Nitish Kumar <krnitish@live.in>
2021-02-10 23:05:17 +0530

    Algo 10-2-2020

--- Python/38minMaxStack.py
+++ Python/38minMaxStack.py
@@ -1,17 +1,22 @@
+# When heap data is in array form then
+# child1 = 2i + 1
+# child2 = 2i + 2
+# parents = floor((i/2)-1)
+# We can use these properties to solve problems efficiently
 class MinMaxStack:
     def __init__(self):
         self.minMaxStack = []
         self.stack = []
-    
-# O(1) | O(1)   
+
+# O(1) | O(1)
     def peek(self):
         return self.stack[len(self.stack) - 1]
-        
+
 # O(1) | O(1)
     def pop(self):
         self.minMaxStack.pop()
         return self.stack.pop()
-    
+
 # O(1) | O(1)
     def push(self, num):
         newMinMax = {"min": num, "max": num}
@@ -21,7 +26,7 @@ class MinMaxStack:
             newMinMax["max"] = max(lastMinMax["max"], num)
         self.minMaxStack.append(newMinMax)
         self.stack.append(num)
-    
+
 # O(1) | O(1)
     def getMin(self):
         return self.minMaxStack[len(self.minMaxStack) - 1]["min"]
@@ -29,16 +34,14 @@ class MinMaxStack:
 # O(1) | O(1)
     def getMax(self):
         return self.minMaxStack[len(self.minMaxStack) - 1]["max"]
-    
-    
+
+
 mmstk = MinMaxStack()
 mmstk.push(3)
 mmstk.push(6)
 mmstk.push(4)
 mmstk.push(8)
-#mmstk.push(1)
+# mmstk.push(1)
 print(mmstk.peek())
 print(mmstk.getMax())
 print(mmstk.getMin())
-
-               

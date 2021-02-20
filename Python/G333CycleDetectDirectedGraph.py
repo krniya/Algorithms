cde8c9958faef76c1a343c5f1f46d4838839c2ea
Nitish Kumar <krnitish@live.in>
2021-02-20 23:01:58 +0530

    Algo Added 20-02-2021

--- Python/G333CycleDetectDirectedGraph.py
+++ Python/G333CycleDetectDirectedGraph.py
@@ -10,35 +10,21 @@ class Graph:
         self.graph[u].append(v)
 
     def isCyclicUtil(self, v, visited, recStack):
-
-        # Mark current node as visited and
-        # adds to recursion stack
         visited[v] = True
         recStack[v] = True
-
-        # Recur for all neighbours
-        # if any neighbour is visited and in
-        # recStack then graph is cyclic
         for neighbour in self.graph[v]:
             if visited[neighbour] == False:
-                if self.isCyclicUtil(neighbour, visited, recStack) == True:
-                    return True
+                return self.isCyclicUtil(neighbour, visited, recStack)
             elif recStack[neighbour] == True:
                 return True
-
-        # The node needs to be poped from
-        # recursion stack before function ends
         recStack[v] = False
         return False
 
-    # Returns true if graph is cyclic else false
     def isCyclic(self):
         visited = [False] * self.V
         recStack = [False] * self.V
         for node in range(self.V):
-            if visited[node] == False:
-                if self.isCyclicUtil(node, visited, recStack) == True:
-                    return True
+            return self.isCyclicUtil(node, visited, recStack)
         return False
 
 

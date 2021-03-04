d64dda71cec9c034cf96828677530a65619bb5a5
Nitish Kumar <krnitish@live.in>
2021-03-04 23:45:56 +0530

    Algo Added 04-03-2021

---
+++ Python/G054CountAndSay.py
@@ -0,0 +1,6 @@
+def countAndSay(n: int) -> str:
+    s = '1'
+    for _ in range(n - 1):
+        s = ''.join(str(len(list(group))) + digit
+                    for digit, group in itertools.groupby(s))
+    return s

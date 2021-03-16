5cfd4dda9746846dc58ccc94e5a2e5a1d8541748
Nitish Kumar <krnitish@live.in>
2021-03-16 23:27:03 +0530

    Algo Added 16-03-2021

---
+++ Python/G019PairsSums.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getPairsCount(self, arr, n, k):
+        sums = {}
+        for i in arr:
+            if i not in sums:
+                sums[i] = 0
+            else:
+                sums[i] += 1
+        sec = 0
+        for i in arr:
+            sec += sums[k - i]

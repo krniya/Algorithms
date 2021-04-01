0438cfda0a0cbf710bf1771a263466ff77df19a3
Nitish Kumar <krnitish@live.in>
2021-04-01 23:09:31 +0530

    Data Struture Added - Stack - Updated

--- Python/Data Structures/stack.py
+++ Python/Data Structures/stack.py
@@ -1,21 +1,23 @@
 class Stack:
     def __init__(self) -> None:
         self.stk = []
-        self.len = 0
 
     def __len__(self):
-        return self.len
+        return len(self.stk)
 
     def isEmpty(self):
-        return self.len == 0
+        return len(self.stk) == 0
 
     def push(self, num):
         self.stk.append(num)
-        self.len += 1
 
     def pop(self):
         if self.isEmpty():
             print("Stack Alredy Empty")
             return
-        self.len -= 1
         return self.stk.pop()
+
+    def bottom(self):
+        if self.isEmpty():
+            print("Stack is Empty")
+        return self.stk[-1]

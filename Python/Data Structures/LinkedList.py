35c04ec14db5e2384f83262751bb20a232c36578
Nitish Kumar <krnitish@live.in>
2021-04-02 23:43:46 +0530

    Data Struture Added - LinkedList - Updated

--- Python/Data Structures/LinkedList.py
+++ Python/Data Structures/LinkedList.py
@@ -8,4 +8,30 @@ class LinkedList:
 
     def __init__(self) -> None:
         self.head = None
-        self
+        # self.next = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def addFirst(self, data):
+        new = self.Node(data, None)
+        if self.isEmpty():
+            self.head = next
+        else:
+            new.next = self.head
+            self.head = new
+        self.size += 1
+
+    def addLast(self, data):
+        new = self.Node(data, None)
+        looper = self.head
+        while looper.next is not None:
+            looper = looper.next
+        looper.next = new
+        self.size += 1
+
+    def addAny(self, data, idx):

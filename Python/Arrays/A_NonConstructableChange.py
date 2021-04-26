829596864c66ed615dd15770e9fb5c1e3970ae49
Nitish Kumar <krnitish@live.in>
2021-04-26 23:50:53 +0530

    Algo Added 26-04-2021

--- Python/Arrays/A_NonConstructableChange.py
+++ Python/Arrays/A_NonConstructableChange.py
@@ -8,4 +8,5 @@ def nonConstChange(coins):
     return currChangeCoin + 1
 
 
-print(nonConstChange([1, 2, 3, 5, 3, 2, 1, 3, 5, 6, 4, 3, 6, 5, 4, 3]))
+print(nonConstChange([1, 2, 3, 5, 3, 2, 1, 3,
+                      5, 6, 4, 3, 6, 5, 4, 3, 1, 4, 5, 3, 2]))

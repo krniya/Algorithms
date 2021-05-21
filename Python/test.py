bcaf803a0c2c1d29199d5a2a599b84aa3c4ebe90
Nitish Kumar <krnitish@live.in>
2021-05-21 23:57:51 +0530

    Algo Added 21-05-2021

--- Python/test.py
+++ Python/test.py
@@ -1,2 +1,11 @@
-def partyCancel(arr):
-    pass
+def twoSum(num, target):
+    map = {}
+    for i in range(len(num)):
+        if num[i] not in map:
+            map[target - num[i]] = i
+        else:
+            return map[num[i]], i + 1
+    return -1, -1
+
+
+print(twoSum([2, 5, 7, 1, 3, 9], 11))

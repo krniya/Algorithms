ed627326830099536be1930037c9e78aaf96925e
Nitish Kumar <krnitish@live.in>
2021-06-07 23:45:58 +0530

    Algo Added 07-06-2021

--- Python/Arrays/A_minRewards.py
+++ Python/Arrays/A_minRewards.py
@@ -6,12 +6,14 @@ def minRewards(scores):
         if scores[i] > scores[j]:
             rewards[i] = rewards[j] + 1
         else:
-            while j >= 0  and scores[j] > scores[j + 1]:
-                rewards[j] = max(rewards[j], rewards[j + 1]+ 1)
+            while j >= 0 and scores[j] > scores[j + 1]:
+                rewards[j] = max(rewards[j], rewards[j + 1] + 1)
                 j -= 1
     return sum(rewards)
 
 # O(n) | O(n)
+
+
 def minRewards1(scores):
     rewards = [1 for _ in scores]
     localMinIdxs = getLocalMinIdx(scores)
@@ -19,16 +21,17 @@ def minRewards1(scores):
         expandFromLocalMinIdx(localMinIdx, scores, rewards)
     return sum(rewards)
 
+
 def expandFromLocalMinIdx(localMinIdx, scores, rewards):
     leftIdx = localMinIdx - 1
-    while leftIdx >= 0 and scores[leftIdx] > scores[ leftIdx + 1 ]:
+    while leftIdx >= 0 and scores[leftIdx] > scores[leftIdx + 1]:
         rewards[leftIdx] = max(rewards[leftIdx], rewards[leftIdx + 1] + 1)
         leftIdx -= 1
     rightIdx = localMinIdx + 1
     while rightIdx < len(scores) and scores[rightIdx] > scores[rightIdx - 1]:
         rewards[rightIdx] = rewards[rightIdx - 1] + 1
-    
-    
+
+
 def getLocalMinIdx(arr):
     if len(arr) == 1:
         return [0]
@@ -45,9 +48,11 @@ def getLocalMinIdx(arr):
     return localMinIdxs
 
 # O(n) | O(n)
+
+
 def minRewards2(scores):
     rewards = [1 for _ in scores]
-    for i in range(1,len(scores)):
+    for i in range(1, len(scores)):
         if scores[i] > scores[i - 1]:
             rewards[i] = rewards[i-1] + 1
     for i in reversed(range(len(scores) - 1)):
@@ -56,5 +61,4 @@ def minRewards2(scores):
     return sum(rewards)
 
 
-
-print(minRewards2([8,4,2,1,3,6,7,9,5]))
+print(minRewards2([8, 4, 2, 1, 3, 6, 7, 9, 5, 10]))

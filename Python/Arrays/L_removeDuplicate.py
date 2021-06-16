9086f475f18fb149aa93352f4d4754a5f4f667f5
Nitish Kumar <krnitish@live.in>
2021-06-16 23:38:45 +0530

    Algo Added 16-06-2021

---
+++ Python/Arrays/L_removeDuplicate.py
@@ -0,0 +1,15 @@
+def removeDuplicate(nums):
+    i = 1
+    count = 0
+    num = nums[0]
+    while i < len(nums):
+        if nums[i] == num:
+            nums.pop(i)
+            count += 1
+        else:
+            num = nums[i]
+            i += 1
+    return count
+
+
+print(removeDuplicate([0, 1, 1, 2, 2, 2, 2, 3, 4, 4, 5]))

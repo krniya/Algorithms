7fd43f46a0e1d7b5f49d41f742b01e7e2a6856de
Nitish Kumar <krnitish@live.in>
2021-06-19 23:55:46 +0530

    Algo Added 19-06-2021

---
+++ Python/Arrays/L_firstMissingPositive.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def firstMissingPositive(nums: List[int]) -> int:
+    nums.append(0)
+    n = len(nums)
+    for i in range(len(nums)):  # delete those useless elements
+        if nums[i] < 0 or nums[i] >= n:
+            nums[i] = 0
+    for i in range(len(nums)):  # use the index as the hash to record the frequency of each number
+        nums[nums[i] % n] += n
+    for i in range(1, len(nums)):
+        if nums[i]/n == 0:
+            return i
+    return n

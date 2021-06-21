6113eeff3dde0e7dcaab978206f319475d7ed7a5
Nitish Kumar <krnitish@live.in>
2021-06-21 23:35:02 +0530

    Algo Added 21-06-2021

---
+++ Python/Arrays/L_longestPallindrome.py
@@ -0,0 +1,12 @@
+def longestPallindrome(s: str) -> str:
+    res = ""
+    for i in range(len(s)):
+        res = max(helper(s, i, i), helper(s, i, i+1), res, key=len)
+    return res
+
+
+def helper(s, l, r):
+    while 0 <= l and r < len(s) and s[l] == s[r]:
+        l -= 1
+        r += 1
+    return s[l+1:r]
--- Python/Arrays/L_longestSubStrWORep.py
+++ Python/Arrays/L_longestSubStrWORep.py
@@ -1,5 +1,16 @@
 from typing import List
 
 
-def longestSubString(str: str) -> int:
-    res = []
+def longestSubString(s: str) -> int:
+    used = {}
+    max_length = start = 0
+    for i, c in enumerate(s):
+        if c in used and start <= used[c]:
+            start = used[c] + 1
+        else:
+            max_length = max(max_length, i - start + 1)
+        used[c] = i
+    return max_length
+
+
+print(longestSubString('abcabcabc'))

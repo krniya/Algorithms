511dc713d7b8eeda7dd5331627af27cd1f84d82c
Nitish Kumar <krnitish@live.in>
2021-06-23 23:52:24 +0530

    Algo Added 23-06-2021

---
+++ Python/Strings/L_longestValidParenthesis.py
@@ -0,0 +1,12 @@
+def longestValidParensthesis(s: str) -> int:
+    stk, max_len = [(")", -1)], 0
+    for i in range(len(s)):
+        if s[i] == ")" and stk[-1][0] == "(":
+            stk.pop()
+            max_len = max(max_len, i-stk[-1][1])
+        else:
+            stk.append((s[i], i))
+    return max_len
+
+
+print(longestValidParensthesis(')(()())(())))()('))

6c64b9c22d3190004f6ea4932f6a2f886842d70d
Nitish Kumar <krnitish@live.in>
2021-07-08 16:58:44 +0530

    Algo Added 08-07-2021

---
+++ Python/Dynamic Programming/F_howSum.py
@@ -0,0 +1,35 @@
+# Brute Force
+# Time O(n^m * m)
+# Space O(m)
+
+# def howSum(tar, arr):
+#     if tar == 0:
+#         return []
+#     if tar < 0:
+#         return None
+#     for num in arr:
+#         rem = tar - num
+#         remRes = howSum(rem, arr)
+#         if remRes is not None:
+#             return remRes + [num]
+#     return None
+
+
+def howSum(tar, arr, memo={}):
+    if tar in memo:
+        return memo[tar]
+    if tar == 0:
+        return []
+    if tar < 0:
+        return None
+    for num in arr:
+        rem = tar - num
+        remRes = howSum(rem, arr, memo)
+        if remRes is not None:
+            memo[tar] = remRes + [num]
+            return memo[tar]
+    memo[tar] = None
+    return None
+
+
+print(howSum(300, [7, 4]))

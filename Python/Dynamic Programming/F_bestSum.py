7d4b20b72bcb1f8abb7871da51aa735b8eb75fe2
Nitish Kumar <krnitish@live.in>
2021-07-20 23:49:02 +0530

    Algo Added 20-07-2021

--- Python/Dynamic Programming/F_bestSum.py
+++ Python/Dynamic Programming/F_bestSum.py
@@ -1,2 +1,17 @@
 def bestSum(targetSum, numbers):
-    pass
+    if targetSum == 0:
+        return []
+    if targetSum < 0:
+        return None
+    soretest = None
+    for num in numbers:
+        rem = targetSum - num
+        remComb = bestSum(rem, numbers)
+        if remComb != None:
+            comb = remComb + [num]
+            if soretest == None or len(comb) < len(soretest):
+                soretest = comb
+    return soretest
+
+
+print(bestSum(7, [2, 3, 4, 7]))

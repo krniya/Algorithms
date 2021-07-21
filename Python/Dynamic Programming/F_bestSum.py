6671ad8bcb9afa76fa74ceb60190406891f6a537
Nitish Kumar <krnitish@live.in>
2021-07-21 23:54:28 +0530

    Algo Added 21-07-2021

--- Python/Dynamic Programming/F_bestSum.py
+++ Python/Dynamic Programming/F_bestSum.py
@@ -1,3 +1,5 @@
+# Time: O(n^m * m) Space: O(m^2)
+
 def bestSum(targetSum, numbers):
     if targetSum == 0:
         return []
@@ -11,6 +13,28 @@ def bestSum(targetSum, numbers):
             comb = remComb + [num]
             if soretest == None or len(comb) < len(soretest):
                 soretest = comb
+    return soretest
+
+# Time: O() Space: O()
+
+
+def bestSum2(targetSum, numbers, memo={}):
+    if targetSum in memo:
+        return memo[targetSum]
+    if targetSum == 0:
+        return []
+    if targetSum < 0:
+        return None
+    soretest = None
+    for num in numbers:
+        rem = targetSum - num
+        remComb = bestSum(rem, numbers, memo)
+        if remComb != None:
+            comb = remComb + [num]
+            if soretest == None or len(comb) < len(soretest):
+                soretest = comb
+
+    memo[targetSum] = soretest
     return soretest
 
 

32807d7e904893d1bdc6e28f97e1ce64d12dead9
Nitish Kumar <krnitish@live.in>
2021-08-26 23:27:00 +0530

    Algo Added 26-08-2021

--- Python/Strings/A_longestPallindromeSubstring.py
+++ Python/Strings/A_longestPallindromeSubstring.py
@@ -4,14 +4,19 @@ def longestpalindromeSubstring(str):
     for i in range(1, len(str)):
         odd = getLongestPalindrome(str, i-1, i+1)
         even = getLongestPalindrome(str, i-1, i)
-        longest = max(odd, even, key = lambda x: x[1] - x[0])
-        currentLongest = max(longest, currentLongest, key = lambda x: x[1] - x[0])
+        longest = max(odd, even, key=lambda x: x[1] - x[0])
+        currentLongest = max(longest, currentLongest,
+                             key=lambda x: x[1] - x[0])
     return str[currentLongest[0]:currentLongest[1]]
-        
+
+
 def getLongestPalindrome(str, left, right):
     while left >= 0 and right < len(str):
         if str[left] != str[right]:
             break
-        left-= 1
-        right+= 1
+        left -= 1
+        right += 1
     return [left + 1, right]
+
+
+print(longestpalindromeSubstring('asdfaffgdsdfsfsfsdfsd'))

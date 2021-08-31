e3062efba9b682caede0468cf636f27a485bd21a
Nitish Kumar <krnitish@live.in>
2021-08-31 23:37:02 +0530

    Algo added 31-08-2021

--- Python/Strings/G_wrapWord.py
+++ Python/Strings/G_wrapWord.py
@@ -1,2 +1,45 @@
-def wrapWord(arr, n):
-    pass
+INF = 2147483647
+
+
+def printSolution(p, n):
+    k = 0
+    if p[n] == 1:
+        k = 1
+    else:
+        k = printSolution(p, p[n] - 1) + 1
+    print('Line number ', k, ': From word no. ',
+          p[n], 'to ', n)
+    return k
+
+
+def solveWordWrap(l, n, M):
+    extras = [[0 for i in range(n + 1)]
+              for i in range(n + 1)]
+    lc = [[0 for i in range(n + 1)]
+          for i in range(n + 1)]
+    c = [0 for i in range(n + 1)]
+    p = [0 for i in range(n + 1)]
+    for i in range(n + 1):
+        extras[i][i] = M - l[i - 1]
+        for j in range(i + 1, n + 1):
+            extras[i][j] = (extras[i][j - 1] -
+                            l[j - 1] - 1)
+    for i in range(n + 1):
+        for j in range(i, n + 1):
+            if extras[i][j] < 0:
+                lc[i][j] = INF
+            elif j == n and extras[i][j] >= 0:
+                lc[i][j] = 0
+            else:
+                lc[i][j] = (extras[i][j] *
+                            extras[i][j])
+    c[0] = 0
+    for j in range(1, n + 1):
+        c[j] = INF
+        for i in range(1, j + 1):
+            if (c[i - 1] != INF and
+                lc[i][j] != INF and
+                    ((c[i - 1] + lc[i][j]) < c[j])):
+                c[j] = c[i-1] + lc[i][j]
+                p[j] = i
+    printSolution(p, n)

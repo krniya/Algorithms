40100671a84263b7a2f4f571224c1c925748a506
Nitish Kumar <krnitish@live.in>
2021-09-11 00:11:40 +0530

    Algo Added 11-09-2021

--- Python/Arrays/G_MinimumJump.py
+++ Python/Arrays/G_MinimumJump.py
@@ -1,10 +1,21 @@
 def minJumps(arr, n):
-	count = i = 0
-	while i < n - 1:
-	    if arr[i] == 0:
-	        return -1
-	    i += arr[i]
-	    count += 1
-	return count
+    if len(arr) <= 1 : 
+        return 0 
+    if arr[0] == 0 :  
+        return -1 
+    maxReach = arr[0]; 
+    step = arr[0]; 
+    jump = 1; 
+    for i in range(1,len(arr)):
+        if  i == len(arr) - 1 : 
+                return jump
+        maxReach = max(maxReach, i+arr[i])
+        step-=1;
+        if  step == 0 : 
+            jump+=1
+            if i>=maxReach : 
+                return -1
+            step = maxReach - i 
+    return -1
 
 print(minJumps([2, 3, 1, 1, 2, 4, 2, 0, 1, 1], 10))

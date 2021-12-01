e2fc464d88721aa74f42ac845c45bf7cbd33a004
Nitish Kumar <krnitish@live.in>
2021-12-01 13:45:25 +0530

    Algo Added 01-12-2021

---
+++ Python/Arrays/G_GetMinDifference.py
@@ -0,0 +1,36 @@
+def getMinDiff(arr, n, k):
+    v = []
+    taken = [0]*n
+    for i in range(n):
+        if arr[i]-k >= 0:
+            v.append([arr[i]-k,i])
+        v.append([arr[i] + k,i])
+    v.sort()
+    elements_in_range = 0;
+    left = 0
+    right = 0
+    while elements_in_range < n and right < len(v) :
+        if taken[v[right][1]] == 0 :
+            elements_in_range+=1;
+            
+        taken[v[right][1]]+=1;
+        right+=1;
+    ans = v[right - 1][0] - v[left][0]
+    while right < len(v) :
+        if(taken[v[left][1]] == 1) :
+            elements_in_range-=1;
+        taken[v[left][1]]-=1;
+        left+=1 
+        while elements_in_range < n and right < len(v) : 
+            if taken[v[right][1]] == 0 :
+                elements_in_range+=1
+                
+            taken[v[right][1]]+=1
+            right+=1
+        if(elements_in_range == n) : 
+            ans = min(ans, v[right - 1][0] - v[left][0])
+        else :
+            break
+    return ans
+
+print(getMinDiff([2, 6, 3, 4, 7, 2, 10, 3, 2, 1], 10, 5))
--- Python/Searching & Sorting/A_insertionSort.py
+++ Python/Searching & Sorting/A_insertionSort.py
@@ -13,3 +13,4 @@ def swap(i, j, arr):
 
 
 
+   

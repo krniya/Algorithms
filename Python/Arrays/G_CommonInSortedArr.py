db364ab1419cc81faa853ba3b89bc73ac43d4157
Nitish Kumar <krnitish@live.in>
2021-12-10 16:16:21 +0530

    Algo Added 10-12-2021

--- Python/Arrays/G_CommonInSortedArr.py
+++ Python/Arrays/G_CommonInSortedArr.py
@@ -1,29 +1,23 @@
-import sys
-def findCommon(ar1, ar2, ar3, n1,n2, n3) :
-    i =j=k= 0
-    prev1 = prev2 = prev3 = -sys.maxsize - 1
-    while (i < n1 and j < n2 and k < n3) :
-        while (ar1[i] == prev1 and i < n1-1) :
-            i += 1
-        while (ar2[j] == prev2 and j < n2) :
-            j += 1
-        while (ar3[k] == prev3 and k < n3) :
-            k += 1
-        if (ar1[i] == ar2[j] and ar2[j] == ar3[k]) :
-            print(ar1[i],end=" ")
-            prev1 = ar1[i]
-            prev2 = ar2[j]
-            prev3 = ar3[k]
+def findCommon(A, B, C, n1,n2, n3) :
+    i = 0
+    j = 0
+    k = 0 
+    last = float("-inf")
+    res = []
+    while i < n1 and j < n2 and k < n3:
+        if A[i] == B[j] == C[k]:
+            if last != A[i]:
+                res.append (A[i])
+                last = A[i]
             i += 1
             j += 1
-            k += 1
-        elif (ar1[i] < ar2[j]) :
-            prev1 = ar1[i]
+            k += 1    
+        elif min (A[i], B[j], C[k]) == A[i]:
             i += 1
-        elif (ar2[j] < ar3[k]) :
-            prev2 = ar2[j]
+        elif min (A[i], B[j], C[k]) == B[j]:
             j += 1
-        else :
-            prev3 = ar3[k]
+        else:
             k += 1
+    return res
  
+print(findCommon([1, 5, 10, 20, 40, 80], [6, 7, 20, 80, 100], [3, 4, 15, 20, 30, 70, 80, 120],6,5,8))
--- Python/test.py
+++ Python/test.py
@@ -1,16 +1,21 @@
-def countPairs(arr, k):
-    pairs = {}
-    for num in arr:
-        if num in pairs:
-            pairs[num] += 1
+def countInSorted(a,b,c):
+    res = []
+    i=j=k=0
+    last=float("-inf")
+    while i < len(a) and j < len(b) and k < len(c):
+        if a[i] == b[j] and b[j] == c[k]:
+            if a[i] != last:
+                res.append(a[i])
+                last = a[i]
+            i+=1
+            j+=1
+            k+=1
+        elif a[i] == min(a[i], b[j], c[k]):
+            i+=1
+        elif b[j] == min(a[i], b[j], c[k]):
+            j+=1
         else:
-            pairs[num] =1
-    twice_count = 0
-    for num in arr:
-        if (k-num) in pairs:
-            twice_count += pairs[k - num]
-        if (k - num == num):
-            twice_count -= 1
-    return int(twice_count / 2)
-
-print(countPairs([3,2,4,3,1], 5))
+            k+=1
+    return res
+        
+print(countInSorted([1, 5, 10, 20, 40, 80], [6, 7, 20, 80, 100], [3, 4, 15, 20, 30, 70, 80, 120]))

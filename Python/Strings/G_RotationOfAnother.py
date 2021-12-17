6734734e48311a472a1646bf0dab74ecb9795b39
Nitish Kumar <krnitish@live.in>
2021-12-18 00:53:04 +0530

    Algo Added 18-12-2021

--- Python/Strings/G_RotationOfAnother.py
+++ Python/Strings/G_RotationOfAnother.py
@@ -17,19 +17,5 @@ def rotation(str1, str2):
     return True
 
 
-def areRotations(string1, string2):
-    size1 = len(string1)
-    size2 = len(string2)
-    temp = ''
-    if size1 != size2:
-        return 0
-    temp = string1 + string1
-    if (temp.count(string2) > 0):
-        return 1
-    else:
-        return 0
-string1 = "AACD"
-string2 = "ACDA"
 
-print(areRotations(string1, string2))
 

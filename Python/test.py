cdc16a8c8d72d59cb74c56bd0c1aae08b503d865
Nitish Kumar <krnitish@live.in>
2021-12-23 23:22:15 +0530

    Algo Added 23-12-2021

--- Python/test.py
+++ Python/test.py
@@ -1,15 +1,9 @@
-def rotate90Clockwise(A):
-    N = len(A[0])
-    for i in range(N // 2):
-        for j in range(i, N - i - 1):
-            temp = A[i][j]
-            A[i][j] = A[N - 1 - j][i]
-            A[N - 1 - j][i] = A[N - 1 - i][N - 1 - j]
-            A[N - 1 - i][N - 1 - j] = A[j][N - 1 - i]
-            A[j][N - 1 - i] = temp
-    return A
+def fib(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
 
-print(rotate90Clockwise([[1,2,3,4],
-                         [5,6,7,8],
-                         [9,10,11,12],
-                         [13,14,15,16]]))
+print(fib(100))

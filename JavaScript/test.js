bf7dc9b4112266016d11f3e5eb06e4b8fdbcff10
Nitish Kumar <krnitish@live.in>
2021-12-28 23:36:12 +0530

    Algo Added 28-12-2021

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,1 +1,11 @@
-console.log(11 & 3);
+let map1 = new Map([
+    ["a", 1],
+    ["b", 1],
+    ["c", 1],
+]);
+let map2 = new Map([
+    ["a", 2],
+    ["b", 2],
+]);
+let spreadMap = new Map([...map1, ...map2]);
+console.log(spreadMap);

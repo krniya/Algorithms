7010aca71d9e1b26fbfa7ff6ed53df002be52188
Nitish Kumar <krnitish@live.in>
2022-01-01 23:31:19 +0530

    Algo Added 01-01-2022

--- Python/test.py
+++ Python/test.py
@@ -1,35 +1,32 @@
-def levenshteinDistance(str1, str2):
-    matr = [[x for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
-    for i in range(1,len(str1)+1):
-        matr[i][0] += matr[i-1][0]
-    for i in range(1,len(str1)+1):
-        for j in range(1,len(str2)+1):
-            if str1[i-1] == str2[j-1]:
-                matr[i][j] = matr[i-1][j-1]
-            else:
-                matr[i][j] = 1 + min(matr[i-1][j-1], matr[i][j-1], matr[i-1][j])
-    return matr[-1][-1]
-
-def levenshteinDistance1(str1, str2):
-    small = str1 if len(str1) < len(str2) else str2
-    big = str1 if len(str1) >= len(str2) else str2
-    evenEdits = [x for x in range(len(small) + 1)]
-    oddEdits = [None for x in range(len(small) + 1)]
-    for i in range(1, len(big) + 1):
-        if i % 2 == 1:
-            currentEdits = oddEdits
-            previousEdits = evenEdits
-        else:
-            currentEdits = evenEdits
-            previousEdits = oddEdits
-        currentEdits[0] = i
-        for j in range(1, len(small) + 1):
-            if big[i - 1] == small[j - 1]:
-                currentEdits[j] = previousEdits[j - 1]
-            else:
-                currentEdits[j] = 1 + min(previousEdits[j - 1],
-                                          previousEdits[j], currentEdits[j-1])
-    return evenEdits[-1] if len(big) % 2 == 0 else oddEdits[-1]
-
-
-print(levenshteinDistance1("abcd", "acdbe"))
+def getMinDiff(self, arr, n, k):
+    v = []
+    taken = [0]*n     
+    for i in range(n):
+        if arr[i]-k >= 0:
+            v.append([arr[i]-k,i])
+        v.append([arr[i] + k,i])
+    v.sort()
+    elements_in_range = 0;
+    left = 0
+    right = 0
+    while elements_in_range < n and right < len(v) :
+        if taken[v[right][1]] == 0 :
+            elements_in_range+=1;
+        taken[v[right][1]]+=1;
+        right+=1;
+    ans = v[right - 1][0] - v[left][0]
+    while right < len(v) :
+        if(taken[v[left][1]] == 1) :
+            elements_in_range-=1;
+        taken[v[left][1]]-=1;
+        left+=1
+        while elements_in_range < n and right < len(v) : 
+            if taken[v[right][1]] == 0 :
+                elements_in_range+=1 
+            taken[v[right][1]]+=1
+            right+=1
+        if(elements_in_range == n) : 
+            ans = min(ans, v[right - 1][0] - v[left][0])
+        else :
+            break
+    return ans

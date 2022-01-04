b1775f3ed643210b564640baad9a1e728cdef9e0
Nitish Kumar <krnitish@live.in>
2022-01-04 23:47:59 +0530

    Algo Added 04-01-2022 Update

--- Python/Strings/G_WordWrap.py
+++ Python/Strings/G_WordWrap.py
@@ -7,9 +7,11 @@ def wordBreak(s, words):
     return ok[-1]
 
 def word_break(s, words):
-    d = [False] * len(s)    
+    d = [False] * len(s)
+    temp=[]
     for i in range(len(s)):
         for w in words:
+            temp.append(s[i-len(w)+1:i+1])
             if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
                 d[i] = True
     return d[-1]

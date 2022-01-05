8df4c01341c57dbfba64d345bf5a5732dc59b272
Nitish Kumar <krnitish@live.in>
2022-01-05 23:51:47 +0530

    Algo Added 05-01-2022 update

---
+++ Python/Strings/L_longestCommonPrefix.py
@@ -0,0 +1,11 @@
+def longestCommonPrefix(m):
+    if not m: return ''
+    s1 = min(m)
+    s2 = max(m)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            return s1[:i] #stop until hit the split index
+    return s1
+
+
+print(longestCommonPrefix(["apple", "appy", "apam"]))

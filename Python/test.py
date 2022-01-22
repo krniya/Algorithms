c9510eeb1ef5943fab288d18cd0218b12b42afe3
Nitish Kumar <krnitish@live.in>
2022-01-22 23:18:34 +0530

    Algo Added 22-01-2022

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,6 @@
-def conversion(a,b,n):
-    return int(str(a), n) + int(str(b),n)
+def contiSum(nums):
+    for i in range(1, len(nums)):
+        if nums[i-1] > 0:
+            nums[i] += nums[i-1]
+    return max(nums)
 
-
-print(conversion(1011,1011,2))

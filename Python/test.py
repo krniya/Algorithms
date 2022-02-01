7b7558c227517636a546b3c5e5d546f088305141
Nitish Kumar <krnitish@live.in>
2022-02-02 01:53:33 +0530

    Algo Added 02-02-2022

--- Python/test.py
+++ Python/test.py
@@ -1,22 +1,35 @@
-from functools import lru_cache
+def numIslands(grid) -> int:
+    count = 0
+    visited = [[False for value in row] for row in grid]
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if visited[i][j]:
+                continue
+            nodeToExp= [[i,j]]
+            currSize = 0
+            while len(nodeToExp):
+                curr = nodeToExp.pop()
+                a= curr[0]
+                b= curr[1]
+                if visited[a][b]:
+                    continue
+                visited[a][b] = True
+                if grid[a][b] == "0":
+                    continue
+                currSize += 1
+                unvisited = []
+                if a > 0 and not visited[a-1][b]:
+                    unvisited.append([a-1,b])
+                if a < len(grid) - 1 and not visited[a+1][b]:
+                    unvisited.append([a+1,b])
+                if b > 0 and not visited[a][b-1]:
+                    unvisited.append([a,b-1])
+                if b < len(grid[0])-1 and not visited[a][b+1]:
+                    unvisited.append([a,b+1])
+                for unvisi in unvisited:
+                    nodeToExp.append(unvisi)
+            if currSize > 0:
+                count+=1
+        return count
 
-def numDecodings(s:str) -> int:
-    if len(s) == 0 or s is None:
-        return 0
-    def dfs(string):
-        if len(string)>0:
-            if string[0] == '0':
-                return 0
-        if string == "" or len(string) == 1:
-            return 1
-        if int(string[0:2]) <= 26:
-            first = dfs(string[1:])
-            second = dfs(string[2:])
-            return first+second
-        else:
-            return dfs(string[1:])
-
-    result_sum = dfs(s)
-    return result_sum
-
-print(numDecodings("1121"))
+print(numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]))

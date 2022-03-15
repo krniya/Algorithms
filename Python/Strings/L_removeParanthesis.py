0818d90faea354055abd096c587e9a0aef75072b
Nitish Kumar <krnitish@live.in>
2022-03-15 23:44:35 +0530

    Algo Added 15-03-2022

---
+++ Python/Strings/L_removeParanthesis.py
@@ -0,0 +1,14 @@
+def minRemoveToMakeValid(s: str) -> str:
+    s = list(s)
+    stack = []
+    for i, char in enumerate(s):
+        if char == '(':
+            stack.append(i)
+        elif char == ')':
+            if stack:
+                stack.pop()
+            else:
+                s[i] = ''
+    while stack:
+        s[stack.pop()] = ''
+    return ''.join(s)

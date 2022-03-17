785aa0df9d027a53fa005cdef22ee6d0df81b561
Nitish Kumar <krnitish@live.in>
2022-03-17 16:14:02 +0530

    Algo updated 17-03-2022

---
+++ Python/Strings/L_encodeDecode.py
@@ -0,0 +1,24 @@
+
+
+def encode(strs):
+    res = ""
+    for s in strs:
+        res += str(len(s)) + "#" + s
+    return res
+
+
+def decode(str):
+    res, i = [], 0
+    while i < len(str):
+        j = i
+        while str[j] != "#":
+            j += 1
+        length = int(str[i:j])
+        res.append(str[j + 1: j+ 1 + length])
+        i = j + 1 + length
+    return res
+
+s = ["hi", "hello", "elephant"]
+a = encode(s)
+print(a)
+print(decode(a))

41cc4df321ab03628d287b8c52dc0610fc30b2a7
Nitish Kumar <krnitish@live.in>
2022-03-21 23:14:48 +0530

    Algo added 21-03-2022

---
+++ Python/Strings/L_PartitionLabel.py
@@ -0,0 +1,12 @@
+def partitionLabels(s):
+    rightmost = {c:i for i, c in enumerate(s)}
+    left, right = 0, 0
+    result = []
+    for i, letter in enumerate(s):
+        right = max(right,rightmost[letter])
+        if i == right:
+            result += [right-left + 1]
+            left = i+1
+    return result
+
+print(partitionLabels("ababcbacadefegdehijhklij"))

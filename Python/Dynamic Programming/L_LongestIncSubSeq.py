23a82a6970d0eef44db7d1150916e2eb84a1c224
Nitish Kumar <krnitish@live.in>
2022-04-12 22:50:08 +0530

    Updated LeetCode 300. Longest Increasing Subsequence

--- Python/Dynamic Programming/L_LongestIncSubSeq.py
+++ Python/Dynamic Programming/L_LongestIncSubSeq.py
@@ -19,3 +19,12 @@ def longestIncSubSeq(nums):
         else:
             sub[pos] = val
     return len(sub)
+
+class Solution:
+    def lengthOfLIS(self, nums) -> int:
+        LIS = [1] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i],1+LIS[j])
+        return max(LIS)

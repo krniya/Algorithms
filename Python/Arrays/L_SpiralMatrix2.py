811eca3b6d4366f31ce241411699b7646f133325
Nitish Kumar <krnitish@live.in>
2022-04-13 18:09:09 +0530

    Added Leetcode 59. Spiral Matrix II

---
+++ Python/Arrays/L_SpiralMatrix2.py
@@ -0,0 +1,10 @@
+def generateMatrix(n):
+    A = [[0] * n for _ in range(n)]
+    i, j, di, dj = 0, 0, 0, 1
+    for k in range(n*n):
+        A[i][j] = k + 1
+        if A[(i+di)%n][(j+dj)%n]:
+            di, dj = dj, -di
+        i += di
+        j += dj
+    return A

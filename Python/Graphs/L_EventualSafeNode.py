df614f7ca208c506e183998cbce1bf663d091d62
Nitish Kumar <krnitish@live.in>
2022-04-18 23:12:23 +0530

    Added Leetcode 802. Find Eventual Safe States

---
+++ Python/Graphs/L_EventualSafeNode.py
@@ -0,0 +1,14 @@
+def eventualSafeNodes(graph):
+        def explore(i):
+            visited[i] = 0
+            for v in graph[i]:
+                if visited[v] == 0 or (visited[v] == -1 and explore(v)): return True
+            visited[i] = 1
+            res.append(i)
+            return False
+        visited, res = [-1] * len(graph), []
+        for i in range(len(graph)):
+            if visited[i] == -1: explore(i)
+        return sorted(res)
+
+print(eventualSafeNodes([[1,2],[2,3],[5],[0],[5],[],[]]))
--- Python/test.py
+++ Python/test.py
@@ -1,78 +1,36 @@
-from typing import List
+def digitSum(s,k):
+        while len(s) > k:
+            a = list(s)
+            s=""
+            i=0
+            while i < len(a):
+                k1 = k
+                sum = 0
+                while i < len(a) and k1>0:
+                    sum += int(a[i])
+                    i+=1
+                    k1-=1
+                z = s + str(sum)
+                s=z
+        return s
 
+print(digitSum("1111122225",3))
 
-class ATM:
-    
-    def __init__(self):
-        self.cc = [0,0,0,0,0]
+# def minRound(tasks):
+#         hm = {}
+#         for task in tasks:
+#             if task in hm:
+#                 hm[task] += 1
+#             else:
+#                 hm[task] = 0
+#         res = 0
+#         for v in hm.values():
+#             if v == 1:
+#                 return -1
+#             op = v // 3
+#             if v%3:
+#                 op+=1
+#             res+=op
+#         return res
 
-    def deposit(self, bc: List[int]) -> None:
-        for i in range(5):
-            self.cc[i]+=bc[i]
-
-    def withdraw(self, amt: int) -> List[int]:
-        if amt%10:
-            return [-1]
-        curr = []
-        for n in self.cc:
-            curr.append(n)
-        ans = [0,0,0,0,0]
-        def reset():
-            self.cc = curr
-        if self.cc[4] and amt >= 500:
-            n = amt//500
-            amt %= 500
-            ans[4] = n
-            if self.cc[4] >= n:
-                self.cc[4] -= n
-            else:
-                reset()
-                return [-1]
-        if self.cc[3] and amt >= 200:
-            n = amt//200
-            amt %= 200
-            ans[3] = n
-            if self.cc[3] >= n:
-                self.cc[3] -= n
-            else:
-                reset()
-                return [-1]
-        if self.cc[2] and amt >= 100:
-            n = amt//100
-            amt %= 100
-            ans[2] = n
-            if self.cc[2] >= n:
-                self.cc[2] -= n
-            else:
-                reset()
-                return [-1]
-        if self.cc[1] and amt >= 50:
-            n = amt//50
-            amt %= 50
-            ans[1] = n
-            if self.cc[1] >= n:
-                self.cc[1] -= n
-            else:
-                reset()
-                return [-1]
-        if self.cc[0] and amt >= 20:
-            n = amt//20
-            amt %= 20
-            ans[0] = n
-            if self.cc[0] >= n:
-                self.cc[0] -= n
-            else:
-                reset()
-                return [-1]
-        if amt == 0:
-            return ans
-        else: 
-            reset()
-            return [-1]
-
-obj = ATM()
-obj.deposit([0,0,1,2,1])
-print(obj.withdraw(600))
-obj.deposit([0,1,0,1,1])
-print(obj.withdraw(600))
-print(obj.withdraw(550))
+# print(minRound([2,2,3,3,2,4,4,4,4,4]))

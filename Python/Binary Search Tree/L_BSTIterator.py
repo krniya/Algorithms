a198aa544386a51a2bd8dbd78fc2928993d3a8a5
Nitish Kumar <krnitish@live.in>
2022-04-21 00:29:45 +0530

    Added Leetcode 173. Binary Search Tree Iterator

---
+++ Python/Binary Search Tree/L_BSTIterator.py
@@ -0,0 +1,21 @@
+class BSTIterator:
+    
+    def __init__(self, root):
+        self.stack = list()
+        self.pushAll(root)
+
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        return self.stack
+
+    # @return an integer, the next smallest number
+    def next(self):
+        tmpNode = self.stack.pop()
+        self.pushAll(tmpNode.right)
+        return tmpNode.val
+        
+    def pushAll(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
+        

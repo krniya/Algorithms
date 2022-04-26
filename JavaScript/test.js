454ddcd4c43a7b6f8c85efa4bc254a063badcc53
Nitish Kumar <krnitish@live.in>
2022-04-26 23:04:49 +0530

    Added Leetcode 1584. Min Cost to Connect All Points
    - Tested JS theory

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,11 +1,1 @@
-let map1 = new Map([
-    ["a", 1],
-    ["b", 1],
-    ["c", 1],
-]);
-let map2 = new Map([
-    ["a", 2],
-    ["b", 2],
-]);
-let spreadMap = new Map([...map1, ...map2]);
-console.log(spreadMap);
+console.log(1 + 2 + 3 + 4 + "5");

abc4a3989088ced59c050b2fc9958db09a7c8f04
Nitish Kumar <krnitish@live.in>
2022-05-01 20:26:15 +0530

    Updated Leetcode 10. Regular Expression Matching

--- Python/Dynamic Programming/L_regExMatching.py
+++ Python/Dynamic Programming/L_regExMatching.py
@@ -12,3 +12,24 @@ def isMatch(s, p):
                 if p[i - 2] == s[j - 1] or p[i - 2] == '.':
                     table[i][j] |= table[i][j - 1]
     return table[-1][-1]
+
+def isMatch(s, p):
+        cache = {}
+        def dfs(i,j):
+            if (i,j) in cache:
+                return cache[(i,j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+            match = (i < len(s) and (s[i] == p[j] or p[j] == "." ))
+            if (j + 1) < len(p) and p[j+1] == "*":
+                cache[(i,j)] = dfs(i,j+2) or (match and dfs(i+1, j))
+                return cache[(i,j)]
+            if match:
+                cache[(i,j)] = dfs(i+1,j+1)
+                return cache[(i,j)]
+            cache[(i,j)] = False
+            return False
+        
+        return dfs(0,0)

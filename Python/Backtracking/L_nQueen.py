895b0d8a15ca9065b7eea32a76d7bbb826293e13
Nitish Kumar <krnitish@live.in>
2022-05-06 01:38:40 +0530

    Added Leetcode 51. N-Queens

---
+++ Python/Backtracking/L_nQueen.py
@@ -0,0 +1,23 @@
+def solveNQueens(n):
+        col,nd,pd = set(),set(),set()
+        res = []
+        board=[["."] * n for _ in range(n)]
+        def backtrack(r):
+            if r == n:
+                c = ["".join(row) for row in board]
+                res.append(c)
+                return
+            for c in range(n):
+                if c in col or (r+c) in pd or (r-c) in nd:
+                    continue
+                col.add(c)
+                pd.add(r+c)
+                nd.add(r-c)
+                board[r][c] = "Q"
+                backtrack(r+1)
+                col.remove(c)
+                pd.remove(r+c)
+                nd.remove(r-c)
+                board[r][c] = "."
+        backtrack(0)
+        return res

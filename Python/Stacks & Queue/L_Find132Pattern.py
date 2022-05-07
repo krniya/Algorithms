3e612b6b71f6cba246f0af893264f44854a0bc8b
Nitish Kumar <krnitish@live.in>
2022-05-07 23:24:22 +0530

    Added Leetcode 456. 132 Pattern

---
+++ Python/Stacks & Queue/L_Find132Pattern.py
@@ -0,0 +1,12 @@
+def find132pattern(nums) -> bool:
+        stack = []
+        curMin = nums[0]
+        
+        for n in nums[1:]:
+            while stack and n >= stack[-1][0]:
+                stack.pop()
+            if stack and n > stack[-1][1]:
+                return True
+            stack.append([n,curMin])
+            curMin = min(curMin, n)
+        return False

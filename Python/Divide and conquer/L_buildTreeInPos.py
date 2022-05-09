01bfbdd2ec3889f1b8f872d1a07065123ecf1174
Nitish Kumar <krnitish@live.in>
2022-05-10 00:51:37 +0530

    Added Leetcode 106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ Python/Divide and conquer/L_buildTreeInPos.py
@@ -0,0 +1,8 @@
+def buildTree(inorder, postorder):
+        if not inorder or not postorder:
+            return None
+        root = TreeNode(postorder[-1])
+        mid = inorder.index(postorder[-1])
+        root.left = self.buildTree(inorder[:mid], postorder[:mid])
+        root.right = self.buildTree(inorder[mid+1:], postorder[mid:-1])
+        return root

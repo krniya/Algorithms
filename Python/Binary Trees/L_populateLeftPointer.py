e83dbdc5737afef732b9a476c35d097372974c7e
Nitish Kumar <krnitish@live.in>
2022-05-13 23:14:58 +0530

    Added Leetcode 117. Populating Next Right Pointers in Each Node II

---
+++ Python/Binary Trees/L_populateLeftPointer.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+def connect(root):
+        node = root
+        while node:
+            curr = dummy = Node(0)
+            while node:
+                if node.left:
+                    curr.next = node.left
+                    curr = curr.next
+                if node.right:
+                    curr.next = node.right
+                    curr = curr.next
+                node = node.next
+            node = dummy.next
+               
+        return root

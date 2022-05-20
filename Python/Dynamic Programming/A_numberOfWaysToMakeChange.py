e3b175ad62411ed03b95c701f028f10f511d40b4
Nitish Kumar <krnitish@live.in>
2022-05-21 00:54:23 +0530

    Updated Leetcode 518. Coin Change 2

--- Python/Dynamic Programming/A_numberOfWaysToMakeChange.py
+++ Python/Dynamic Programming/A_numberOfWaysToMakeChange.py
@@ -8,5 +8,20 @@ def numberOfWaysToMakeChange(n, denoms):
                 ways[amount] += ways[amount - denom]
     return ways[n]
 
+def change(amt, coins) -> int:
+        dp = {}
+        def dfs(i,a):
+            if a == amt:
+                return 1
+            if a > amt:
+                return 0
+            if i == len(coins):
+                return 0
+            if (i,a) in dp:
+                return dp[(i,a)]
+            dp[(i,a)] = dfs(i,a+coins[i]) + dfs(i+1, a)
+            return dp[(i,a)]
+        return dfs(0,0)
+
 
 print(numberOfWaysToMakeChange(10, [1, 2, 3, 4]))

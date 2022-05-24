feed303aa9c095f197407fb7201291ab5f75c954
Nitish Kumar <krnitish@live.in>
2022-05-24 23:12:00 +0530

    Added Leetcode 239. Sliding Window Maximum

---
+++ Python/Stacks & Queue/L_maxSlidingWindow.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def maxSlidingWindow( nums, k):
+        op = []
+        q = deque()
+        l=r=0
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+            if l > q[0]:
+                q.popleft()
+            if (r+1) >= k:
+                op.append(nums[q[0]])
+                l+=1
+            r+=1
+        return op
+
+print(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))

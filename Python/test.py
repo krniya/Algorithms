317abac08ec2ce06a0b7c5944a3cbd4dda4257a3
Nitish Kumar <krnitish@live.in>
2022-05-28 23:36:21 +0530

    Changes for Biweekly contest

--- Python/test.py
+++ Python/test.py
@@ -1,2 +1,23 @@
-a = [1,2,3,4,5,6,7,8,9,45,23,65,34,23,6453,534,5,234,342,423,423,4565]
-print(a[-1:-11:-1])
+from collections import defaultdict
+from tkinter import W
+
+
+def largestWordCount(messages, senders) -> str:
+        hm = defaultdict()
+        n = len(messages)
+        def split(s):
+            w= 1
+            for c in s:
+                w+= c ==""
+            return w
+        for i in range(n):
+            hm[senders[i]]= [split(messages[i])]
+        res = ""
+        cnt = 0
+        for k in hm:
+            if k>=cnt:
+                cnt=k
+                res = max(res,hm[k])
+        return res
+
+print(largestWordCount(["Hello userTwooo","Hi userThree","Wonderful day Alice","Nice day userThree"],["Alice","userTwo","userThree","Alice"]))
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,6 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cout<<"Hello World";
-}
+class Solution {
+public:
+    // splitong the words
+    int divider(string &str){
+        int noOfWord = 1;
+        for(char ch: str){
+            noOfWord += ch == ' ';
+        }
+        return noOfWord;
+    }
+    
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        map<string,int> hm;
+        int n;
+        n = messages.size();
+        for(int i=0;i<n;i++){
+            hm[senders[i]] += divider(messages[i]);
+        }
+        string res;
+        int count = 0;
+        for(auto [key, val]: hm){
+            if(val >= count){
+                count = val;
+                res = max(res, key);
+            }
+        }
+        return res;
+    }
+};

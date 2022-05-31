1ee63be53351266e1e52fb4dd98cd1ad5f1b76b3
Nitish Kumar <krnitish@live.in>
2022-05-31 21:21:18 +0530

    Added Leetcode 209. Minimum Size Subarray Sum

---
+++ Python/Arrays/L_minSizeSubArrSum.py
@@ -0,0 +1,10 @@
+def minSubArrayLen(target: int, nums) -> int:
+        l, total = 0,0
+        res = float("inf")
+        for r in range(len(nums)):
+            total += nums[r]
+            while total >= target:
+                res = min(r-l+1, res)
+                total -= nums[l]
+                l += 1
+        return 0 if res == float("inf") else res

803e8693762e404def0d682fed534cfe89e71322
Nitish Kumar <krnitish@live.in>
2022-06-02 12:21:15 +0530

    Added Leetcode CPP 867. Transpose Matrix

---
+++ cpp/Array/L_transposeMatrix.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> res(n, vector<int>(m,0));
+        for (int i=0;i<m;++i) {
+            for(int j=0;j<n;++j) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+
+int main() {
+    
+}

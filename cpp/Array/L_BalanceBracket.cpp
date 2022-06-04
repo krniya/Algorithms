c661b45fb9506134d101650e6f2285f2f2e77e14
Nitish Kumar <krnitish@live.in>
2022-06-05 01:20:52 +0530

    Added CPP Balance Brackets

--- cpp/Array/L_BalanceBracket.cpp
+++ cpp/Array/L_BalanceBracket.cpp
@@ -1,17 +1,26 @@
 #include<bits/stdc++.h>
 #include <iostream>
 using namespace std;
-unordered_map<char,int> symbol = {
-        {"[",-1}, 
+// unordered_map<char,int> symbols = {
+//         {"[",-1}, 
+//         {"(",-2}, 
+//         {"{",-3}, 
+//         {"]", 1}, 
+//         {")", 2}, 
+//         {"}", 3}
+//     };
+
+
+bool isBalance(string s) {
+    stack<char> st;
+    std::unordered_map<std::string, int> symbol(
+    {
+    {"[",-1}, 
         {"(",-2}, 
         {"{",-3}, 
         {"]", 1}, 
         {")", 2}, 
-        {"}", 3}
-    };
-bool isBalance(string s) {
-    stack<char> st;
-    
+        {"}", 3} });
 
     for(char c:s) {
         if (symbol[c] < 0) {
@@ -32,4 +41,3 @@ int main() {
     bool res = isBalance(brak);
     cout << res;
 }
-

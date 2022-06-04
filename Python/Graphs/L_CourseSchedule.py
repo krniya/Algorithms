f323e7fb712df5dc3e1e317241ec6ce1975bd19d
Nitish Kumar <krnitish@live.in>
2022-06-04 13:12:32 +0530

    Updated Leetcode 207. Course Schedule

--- Python/Graphs/L_CourseSchedule.py
+++ Python/Graphs/L_CourseSchedule.py
@@ -15,3 +15,20 @@ def courseSchedule(numCourse, prerequisites):
     for crs in range(numCourse):
         if not dfs(crs): return False
     return True
+
+def courseSchedule2(n, prerequisites):
+        G = [[] for i in range(n)]
+        degree = [0] * n
+        for j, i in prerequisites:
+            G[i].append(j)
+            degree[j] += 1
+        bfs = [i for i in range(n) if degree[i] == 0]
+        for i in bfs:
+            for j in G[i]:
+                degree[j] -= 1
+                if degree[j] == 0:
+                    bfs.append(j)
+        return len(bfs) == n
+
+
+print(courseSchedule2(3,[[0,1],[0,2],[1,2]]))

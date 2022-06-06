cbde577d1a94a444e0ff450dcb24f126035ce13f
Nitish Kumar <krnitish@live.in>
2022-06-06 22:54:03 +0530

    Updated Leetcode 417. Pacific Atlantic Water Flow

--- Python/Graphs/L_PasificAtlanticOcean.py
+++ Python/Graphs/L_PasificAtlanticOcean.py
@@ -24,4 +24,29 @@ def pacificAtlantic(heights):
                     res.append([r,c])
         return res
 
+def pacificAtlantic(matrix):
+        if not matrix:
+            return []
+        p_visited = set()
+        a_visited = set()
+        rows, cols = len(matrix), len(matrix[0])
+        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        def traverse(i, j, visited):
+            if (i, j) in visited:
+                return
+            visited.add((i, j))
+            # Traverse neighbors.
+            for direction in directions:
+                next_i, next_j = i + direction[0], j + direction[1]
+                if 0 <= next_i < rows and 0 <= next_j < cols:
+                    if matrix[next_i][next_j] >= matrix[i][j]:
+                        traverse(next_i, next_j, visited)
+        for row in range(rows):
+            traverse(row, 0, p_visited)
+            traverse(row, cols - 1, a_visited)
+        for col in range(cols):
+            traverse(0, col, p_visited)
+            traverse(rows - 1, col, a_visited)
+        return list(p_visited & a_visited)
+
 print(pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]))

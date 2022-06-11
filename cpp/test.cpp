27cbdb7fa94551b3c0c4e240521de1feb9c7bd80
Nitish Kumar <krnitish@live.in>
2022-06-11 23:19:48 +0530

    Added Leetocde CPP 2300. Successful Pairs of Spells and Potions

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool strongPasswordCheckerII(string p) {
-        if(p.size() < 8) return false;
-        bool low = false , upper = false ,digit = false ,special = false;
-        for(auto c : p){
-            if(c>='a' and c<='z') low = true;
-            else if(c>='A' and c <='Z') upper = true;
-            else if(isdigit(c)) digit = true;
-            else special = true;
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (int a: spells) {
+            long need = (success + a - 1) / a;
+            auto it = lower_bound(potions.begin(), potions.end(), need);
+            res.push_back(potions.end() - it);
         }
-        //Check the 6th condition
-        for(int i=0;i+1<p.size();i++) if(p[i] == p[i+1]) return false;
-        if(low and upper and digit and special) return true;
-        return false;
+        return res;
     }
 
 int main() {
-    cout<<strongPasswordCheckerII("NiYa@1233");
+    vector<int> s = {5,1,3};
+    vector<int> p = {1,2,3,4,5};
+    int suc = 7;
+    vector<int> a = successfulPairs(s,p,suc);
 }

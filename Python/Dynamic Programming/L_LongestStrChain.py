51c3e00f491a18159bfea7f3a18f4b5457d90bb9
Nitish Kumar <krnitish@live.in>
2022-06-15 23:38:18 +0530

    Added Leetcode 1048. Longest String Chain

---
+++ Python/Dynamic Programming/L_LongestStrChain.py
@@ -0,0 +1,5 @@
+def longestStrChain(words) -> int:
+        dp = {}
+        for w in sorted(words, key=len):
+            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))
+        return max(dp.values())

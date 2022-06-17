043abcc0a1b720a7eeedf560270aa743b12d211b
Nitish Kumar <krnitish@live.in>
2022-06-17 23:45:51 +0530

    Updated Leetcode 125. Valid Palindrome

--- Python/Strings/L_validatePalindromeStr.py
+++ Python/Strings/L_validatePalindromeStr.py
@@ -2,4 +2,26 @@ import re
 def isPalindrome(s):
     str = re.findall("[A-Za-z0-9]", s.lower())
     return str == str[::-1]
+
+
+def isPalindrome(s: str) -> bool:
+        nStr = ""
+        for ch in s:
+            if ch.isalnum():
+                nStr += ch.lower()
+        return nStr == nStr[::-1]
+
+
+def isPalindrome(s: str) -> bool:
+        l,r = 0, len(s)-1
+        while l<=r:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
         

1d9ccbc890c095ef7b1a1f818b45688abe6508a1
Nitish Kumar <krnitish@live.in>
2022-06-19 23:23:36 +0530

    OOP LibraryManagement Added Librarian class

--- Python/Design/LibraryManagement/Accounts.py
+++ Python/Design/LibraryManagement/Accounts.py
@@ -12,3 +12,18 @@ class Account(ABC):
     def resetPassword(self):
         None
 
+class Librarian(Account):
+    def __init__(self, id, password, person, status=AccountStatus.ACTIVE) -> None:
+        super().__init__(id, password, person, status)
+
+    def add_book_items(self, bookItems):
+        pass
+
+    def blockMember(self, member):
+        pass
+
+    def unBlockMember(self, member):
+        pass
+
+
+

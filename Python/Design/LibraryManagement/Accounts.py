6f17ce822d059936fe2c58ccebaef4369e3897ca
Nitish Kumar <krnitish@live.in>
2022-06-19 23:39:47 +0530

    OOD LibraryManagement added Member class

--- Python/Design/LibraryManagement/Accounts.py
+++ Python/Design/LibraryManagement/Accounts.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 from mimetypes import init
-from Python.Design.LibraryManagement.DataElement import AccountStatus
+from Python.Design.LibraryManagement.DataElement import AccountStatus, Constants
 
 class Account(ABC):
     def __init__(self,id,password,person,status= AccountStatus.ACTIVE) -> None:
@@ -26,4 +27,21 @@ class Librarian(Account):
         pass
 
 
+class Member(Account):
+    def __init__(self, id, password, person, status=AccountStatus.ACTIVE) -> None:
+        super().__init__(id, password, person, status)
+        self.__dateOfMemberShip = datetime.date.today()
+        self.__totalBooksCheckedOut = 0
+
+    def getTotalBooksCheckedOut(self):
+        return self.__totalBooksCheckedOut
+
+    def increaseTotalBookCheckedOut(self):
+        pass
+
+    def checkoutBookItem(self, bookItem):
+        if self.__totalBooksCheckedOut >= Constants.MAX_BOOK_PER_USER:
+            print("User Already checked-out alloted max number of books")
+            return False
+            
 

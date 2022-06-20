ebeb39e1eb8790fb598812c7325a6f4f2511b3f6
Nitish Kumar <krnitish@live.in>
2022-06-21 00:08:02 +0530

    Updated Leetcode 820. Short Encoding of Words

--- Python/Strings/L_shortEncodeWord.py
+++ Python/Strings/L_shortEncodeWord.py
@@ -4,3 +4,16 @@ def minimumLengthEncoding(words) -> int:
             for i in range(1, len(w)):
                 s.discard(w[i:])
         return sum(len(w) + 1 for w in s)
+
+
+def minimumLengthEncoding(words) -> int:
+        trie, ans = {}, 0
+        for word in words:
+            node = trie
+            for c in reversed(word):
+                if '$' in node: ans -= node.pop('$')
+                node = node.setdefault(c, {})
+            if not node:
+                node['$'] = len(word) + 1
+                ans += node['$']
+        return ans

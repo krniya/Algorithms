4cbe4de0455aef29125c12200132666eee7e7a9a
Nitish Kumar <krnitish@live.in>
2022-06-22 23:56:02 +0530

    Updated Leetcode 215. Kth Largest Element in an Array

--- Python/Arrays/L_KthLargestElement.py
+++ Python/Arrays/L_KthLargestElement.py
@@ -15,4 +15,18 @@ def findKthLargest(nums, k: int) -> int:
             else: return nums[p]
         return quicksel(0, len(nums) - 1)
 
+def findKthLargest(nums, k: int) -> int:
+        if not nums: return
+        pivot = random.choice(nums)
+        left =  [x for x in nums if x > pivot]
+        mid  =  [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+        L, M = len(left), len(mid)
+        if k <= L:
+            return findKthLargest(left, k)
+        elif k > L + M:
+            return findKthLargest(right, k - L - M)
+        else:
+            return mid[0]
+
 print(findKthLargest([3,2,5,1,4,7,6], 3))

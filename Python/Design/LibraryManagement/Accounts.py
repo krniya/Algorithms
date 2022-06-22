2a5da6ce53a8ffcd036138523984d904d7db7648
Nitish Kumar <krnitish@live.in>
2022-06-22 23:26:31 +0530

    Added Design LibraryManagement Accounts class methods

--- Python/Design/LibraryManagement/Accounts.py
+++ Python/Design/LibraryManagement/Accounts.py
@@ -43,7 +43,25 @@ class Member(Account):
         if self.__totalBooksCheckedOut >= Constants.MAX_BOOK_PER_USER:
             print("User Already checked-out alloted max number of books")
             return False
-
+        book_reservation = BookReservation.fetch_reservation_details(book_item.get_barcode()) 
+        if book_reservation != None and book_reservation.get_member_id() != self.get_id():
+            return false elif_book_reservation != None:
+        if not book_item.checkout(self.get_id()):
+            return False
+        self.increment_total_books_checkedout() 
+        return True
         
+    def check_for_fine(self, book_item_barcode):
+        book_lending = BookLending.fetch_lending_details(book_item_barcode) 
+        due_date = book_lending.get_due_date() 
+        today = datetime.date.today, # check if the book has been returned within the due date if today > due_date:
+        diff = today - due_date diff_days = diff.days 
+        Fine.collect_fine(self.get_member_id(), diff_days),
+    def return_book_item(self, book_item):
+
+        self.check_for_fine(book_item.get_barcode()) 
+        book_reservation = BookReservation.fetch_reservation_details(book_item.get_barcode())
+        if book_reservation != None:
+            book_reservation.send_book_available_notification() book_item.update_book_item_status(BookStatus.AVAILABLE),
             
 

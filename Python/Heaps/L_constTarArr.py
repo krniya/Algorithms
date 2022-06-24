66d3c280a775fdf5d77826c6931e9ba09d1cd39c
Nitish Kumar <krnitish@live.in>
2022-06-24 23:48:26 +0530

    Added leetcode 1354. Construct Target Array With Multiple Sums

---
+++ Python/Heaps/L_constTarArr.py
@@ -0,0 +1,16 @@
+import heapq
+
+
+def isPossible(A) -> bool:
+        total = sum(A)
+        A = [-a for a in A]
+        heapq.heapify(A)
+        while True:
+            a = -heapq.heappop(A)
+            total -= a
+            if a == 1 or total == 1: return True
+            if a < total or total == 0 or a % total == 0:
+                return False
+            a %= total
+            total += a
+            heapq.heappush(A, -a)

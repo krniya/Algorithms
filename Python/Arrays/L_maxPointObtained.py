9c89781138c8c68cbdc23c1107d335560dbe999d
Nitish Kumar <krnitish@live.in>
2022-06-27 00:47:04 +0530

    Added Leetcode 1423. Maximum Points You Can Obtain from Cards

---
+++ Python/Arrays/L_maxPointObtained.py
@@ -0,0 +1,7 @@
+def maxScore(cardPoints, k: int) -> int:
+        s = sum(cardPoints[:k])
+        res = s
+        for i in range(1, k+1):
+            s += cardPoints[-i] - cardPoints[k-i]
+            res = max(res, s)
+        return res

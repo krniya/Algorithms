b05d95383caee5a4a50f21a68266728c977a1434
Nitish Kumar <krnitish@live.in>
2022-06-27 23:57:48 +0530

    Added Leetcode CPP 2320. Count Number of Ways to Place Houses

---
+++ cpp/Dynamic Programming/L_housePlacementPattern.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll mod = 1e9+7;
+int countHousePlacements(int n) {
+    ll house=1, space=1;
+    ll total = house+space;
+    for(int i=2;i<=n;i++){
+        house = space;
+        space = total;
+        total = (house+space)%mod;
+    }
+    return (total*total)%mod;
+}
+
+int main() {
+    cout << minDistance("sea","eat");
+
+}

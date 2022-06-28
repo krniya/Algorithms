af7e2c29d0c094ab3640465b5fcd9dbc1e129dc1
Nitish Kumar <krnitish@live.in>
2022-06-28 13:00:24 +0530

    Added Leetcode CPP 1647. Minimum Deletions to Make Character Frequencies Unique

---
+++ cpp/Array/L_minDelForUniqFreq.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minDeletions(string s) {
+    int cnt[26] = {}, res = 0;
+    unordered_set<int> used;
+    for (char ch : s)
+        ++cnt[ch - 'a'];
+    for (int i = 0; i < 26; ++i)
+        for (; cnt[i] > 0 && !used.insert(cnt[i]).second; --cnt[i])
+            ++res;
+    return res;
+}
+
+int main() {
+    cout << minDistance("sea","eat");
+
+}

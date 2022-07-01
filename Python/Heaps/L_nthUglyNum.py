d4b6dc5a0539ac26cdbbf61c66208cfdee12b52d
Nitish Kumar <krnitish@live.in>
2022-07-01 23:42:24 +0530

    Added Leetcode 264. Ugly Number II

---
+++ Python/Heaps/L_nthUglyNum.py
@@ -0,0 +1,10 @@
+import heapq
+
+
+def nthUglyNumber(n: int) -> int:
+        N, m, S = [1], 1, set()
+        for _ in range(n):
+            while m in S: m = heapq.heappop(N)
+            S.add(m)
+            for i in [2,3,5]: heapq.heappush(N,i*m)
+        return m

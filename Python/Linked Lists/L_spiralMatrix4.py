c3074a441cbe31fc3659c0b446fd7802edf3a081
Nitish Kumar <krnitish@live.in>
2022-07-03 23:51:59 +0530

    Added Leetcode 2326. Spiral Matrix IV

---
+++ Python/Linked Lists/L_spiralMatrix4.py
@@ -0,0 +1,13 @@
+def spiralMatrix(m: int, n: int, head):
+        num = m * n
+        res = [[-1 for j in range(n)] for i in range(m)]
+        x, y = 0, 0
+        dx, dy = 1, 0
+        while head:
+            res[y][x] = head.val
+            if x + dx < 0 or x + dx >= n or y + dy < 0 or y + dy >= m or res[y+dy][x+dx] != -1:
+                dx, dy = -dy, dx
+            x = x + dx
+            y = y + dy
+            head = head.next
+        return res

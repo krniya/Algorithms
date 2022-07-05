bf4ee3ee5df0c1479351931bbf55d5f917c73be1
Nitish Kumar <krnitish@live.in>
2022-07-05 19:42:02 +0530

    Added Design Vechile Registration System

--- Python/test.py
+++ Python/test.py
@@ -1,24 +1,13 @@
-from operator import indexOf
+class Player:
+    teamName = 'Liverpool' # class variables
+    def __init__(self, name):
+        self.name = name # creating instance variables
 
+    @staticmethod
+    def demo():
+        print("I am a static method.")
 
-class TenderManagement:
-    def __init__(self, data) -> None:
-        self.attrMap = {
-            "Lead Time":[0,1],
-            "Tender Value":[0,2],
-            "Project Duration":[0,3],
-            "Vendor Rating":[1,4],
-        }
-        self.tendData = data
+p1 = Player("lol")
+p1.demo()
+Player.demo()
 
-    def tenderAttribute(self,tenderAtt):
-        attr , pos = self.attrMap[tenderAtt]
-        val = [x[pos] for x in self.tendData]
-        if attr:
-            return self.tendData[val.index(max(val))]
-        else:
-            return self.tendData[val.index(min(val))]
-
-    def printSen(self, tenderAtt):
-        vendorName = self.tenderAttribute(tenderAtt)
-        return "Mr Maharaj, you should award the tender to " + vendorName

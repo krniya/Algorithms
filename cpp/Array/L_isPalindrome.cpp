e7c03257748b7d89e38788859e05ed564c8d7357
Nitish Kumar <krnitish@live.in>
2022-07-09 16:22:11 +0530

    Added Leetcode CPP 125. Valid Palindrome

---
+++ cpp/Array/L_isPalindrome.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string s) {
+    for(int i=0,j=s.size() - 1; i<j; i++,j--) {
+        while(!isalnum(s[i]) && i<j) i++;
+        while(!isalnum(s[j]) && i<j) j--;
+        if (tolower(s[i]) != tolower(s[j])) return false;
+    }
+    return true;
+}
+
+int main() {
+
+}

2c1d21077f69ef0c34eeb5067625d4c07bc5c1b5
Nitish Kumar <krnitish@live.in>
2022-07-11 23:33:02 +0530

    Added Leetcode CPP 42. Trapping Rain Water

---
+++ cpp/Array/L_trapRainWater.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int trap(vector<int>& height) {
+    int i = 0;
+    int j = height.size() - 1;
+    
+    int maxLeft = height[i];
+    int maxRight = height[j];
+    
+    int result = 0;
+    
+    while (i < j) {
+        if (maxLeft <= maxRight) {
+            i++;
+            maxLeft = max(maxLeft, height[i]);
+            result += maxLeft - height[i];
+        } else {
+            j--;
+            maxRight = max(maxRight, height[j]);
+            result += maxRight - height[j];
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+
+}
+

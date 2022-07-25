f0521c927cf0eac98f10995c82e8c6ee6ebd7cd7
Nitish Kumar <krnitish@live.in>
2022-07-25 23:27:36 +0530

    Updated Leetcode 34. Find First and Last Position of Element in Sorted Array

--- Python/Arrays/L_searchRange.py
+++ Python/Arrays/L_searchRange.py
@@ -17,3 +17,22 @@ def searchRange(nums, target):
         
         left, right = binarySearchLeft(nums, target), binarySearchRight(nums, target)
         return (left, right) if left <= right else [-1, -1]
+
+def searchRange(nums, target):
+    def search(x):
+            lo, hi = 0, len(nums)           
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] < x:
+                    lo = mid+1
+                else:
+                    hi = mid                    
+            return lo
+        
+    lo = search(target)
+    hi = search(target+1)-1
+    
+    if lo <= hi:
+        return [lo, hi]
+            
+    return [-1, -1]

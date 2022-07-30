86f204747dc8b9090730df6425961bc5bb240337
Nitish Kumar <krnitish@live.in>
2022-07-31 01:30:45 +0530

    Added Leetcode 110. Balanced Binary Tree

---
+++ Python/Binary Trees/L_balancedBT.py
@@ -0,0 +1,7 @@
+def isBalanced(root) -> bool:
+        def dfs(root):
+            if not root: return [True, 0]
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1]-right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
+        return dfs(root)[0]

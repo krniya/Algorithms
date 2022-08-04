b6dedcddb82bce72eedffda64e86ccbf021a592f
Nitish Kumar <krnitish@live.in>
2022-08-04 23:58:33 +0530

    Added Leetcode CPP 621. Task Scheduler

---
+++ cpp/Heap/L_taskScheduler.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mp;
+        int count = 0;
+        for(auto e : tasks)
+        {
+            mp[e]++;
+            count = max(count, mp[e]);
+        }
+        
+        int ans = (count-1)*(n+1);
+        for(auto e : mp) if(e.second == count) ans++;
+        return max((int)tasks.size(), ans);
+    }
+
+int main() {
+
+}

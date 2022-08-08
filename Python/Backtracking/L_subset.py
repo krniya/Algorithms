0a530523891b043a7f8cbbe67a98405b11bf8678
Nitish Kumar <krnitish@live.in>
2022-08-09 00:14:43 +0530

    Updated Leetcode 78. Subsets

--- Python/Backtracking/L_subset.py
+++ Python/Backtracking/L_subset.py
@@ -12,3 +12,20 @@ def subsets(nums):
         if nums:
             backtrack(nums,[])
         return res
+
+
+
+def subsets(nums):
+        res = []
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            backtrack(i+1, subset)
+            subset.pop()
+            backtrack(i+1, subset)
+            
+        if nums:
+            backtrack(0,[])
+        return res

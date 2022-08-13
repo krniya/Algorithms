243fbe09289de3cae3d18e277df54ff92564e551
Nitish Kumar <krnitish@live.in>
2022-08-13 23:48:12 +0530

    Updated Leetcode 133. Clone Graph

--- Python/Graphs/L_CloneGraph.py
+++ Python/Graphs/L_CloneGraph.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node:
     def __init__(self,val=0,neighbors=None):
         self.val = val
@@ -14,3 +17,21 @@ def cloneGraph(node):
             node.neighbors.append(clone(neighbor))
         return copy
     return clone(node) if node else None
+
+
+def cloneGraph(node: 'Node') -> 'Node':
+        if not node:
+            return None
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft() 
+            cur_clone = clones[cur.val]            
+
+            for ngbr in cur.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+                    
+                cur_clone.neighbors.append(clones[ngbr.val])
+                
+        return clones[node.val]

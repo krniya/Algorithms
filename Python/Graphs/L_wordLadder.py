ca5694735de076a17e5166f98db7658a1d90c1f9
Nitish Kumar <krnitish@live.in>
2022-08-17 23:56:07 +0530

    Added Leetcode 127. Word Ladder

---
+++ Python/Graphs/L_wordLadder.py
@@ -0,0 +1,29 @@
+from collections import defaultdict, deque
+
+def ladderLength(beginWord, endWord, wordList) -> int:
+        if endWord not in wordList:
+            return 0
+
+        nei = defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j + 1 :]
+                nei[pattern].append(word)
+
+        visit = set([beginWord])
+        q = deque([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j + 1 :]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res += 1
+        return 0

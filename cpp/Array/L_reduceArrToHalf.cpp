adb53a85e465385e501812244389dee309ee5e3d
Nitish Kumar <krnitish@live.in>
2022-08-18 23:10:40 +0530

    Added Leetcode CPP 1338. Reduce Array Size to The Half

---
+++ cpp/Array/L_reduceArrToHalf.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int minSetSize(vector<int>& arr) {
+    unordered_map<int, int> cnt;
+    for (int x : arr) ++cnt[x];
+    
+    vector<int> frequencies;
+    for (auto [_, freq] : cnt) frequencies.push_back(freq);
+    sort(frequencies.begin(), frequencies.end());
+    
+    int ans = 0, removed = 0, half = arr.size() / 2, i = frequencies.size() - 1;
+    while (removed < half) {
+        ans += 1;
+        removed += frequencies[i--];
+    }
+    return ans;
+}
+
+
+int main() {
+
+}

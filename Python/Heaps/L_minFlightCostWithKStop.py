e66df5b71c1b589e834814a2b9ce8f0d830616f3
Nitish Kumar <krnitish@live.in>
2022-08-21 01:18:13 +0530

    Updated Leetcode 787. Cheapest Flights Within K Stops

--- Python/Heaps/L_minFlightCostWithKStop.py
+++ Python/Heaps/L_minFlightCostWithKStop.py
@@ -1,3 +1,7 @@
+import collections
+import heapq
+
+
 def findCheapestPrice(n: int, flights, src: int, dst: int, k: int) -> int:
         prices = [float("inf")] * n
         prices[src] = 0
@@ -10,3 +14,21 @@ def findCheapestPrice(n: int, flights, src: int, dst: int, k: int) -> int:
                     tempPrice[dest] = prices[src] + cost
             prices = tempPrice
         return prices[dst] if prices[dst] != float("inf") else -1
+
+
+def findCheapestPrice(n: int, flights, src: int, dst: int, K: int) -> int:
+        graph = collections.defaultdict(dict)
+        for s, d, w in flights:
+            graph[s][d] = w
+        pq = [(0, src, K+1)]
+        vis = [0] * n
+        while pq:
+            w, x, k = heapq.heappop(pq)
+            if x == dst:
+                return w
+            if vis[x] >= k:
+                continue
+            vis[x] = k
+            for y, dw in graph[x].items():
+                heapq.heappush(pq, (w+dw, y, k-1))
+        return -1

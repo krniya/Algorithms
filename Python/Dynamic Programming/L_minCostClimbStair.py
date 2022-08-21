fe9be8fe5da9385026ba11fd4f26a9d97c1fea62
Nitish Kumar <krnitish@live.in>
2022-08-21 23:10:24 +0530

    Updated Leetcode 746. Min Cost Climbing Stairs

--- Python/Dynamic Programming/L_minCostClimbStair.py
+++ Python/Dynamic Programming/L_minCostClimbStair.py
@@ -17,3 +17,8 @@ def minCostClimbingStairs(cost) -> int:
             first = second
             second = curr
         return min(first,second)
+
+def minCostClimbingStairs(cost) -> int:
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])

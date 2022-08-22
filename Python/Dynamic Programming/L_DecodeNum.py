82d5c4e28deddd6baa6ad17e571fa0a95ebffaa5
Nitish Kumar <krnitish@live.in>
2022-08-23 00:17:18 +0530

    Updated Leetcode 91. Decode Ways

--- Python/Dynamic Programming/L_DecodeNum.py
+++ Python/Dynamic Programming/L_DecodeNum.py
@@ -1,3 +1,6 @@
+from functools import cache
+
+
 def numDecodings(s):
     dp = { len(s): 1}
     def dfs(i):
@@ -23,5 +26,20 @@ def numDecodings2(s):
         if (i+1 < len(s) and (s[i]=="1" or (s[i]=="2" and s[i+1] in "0123456"))):
             dp[i] += dp[i+2]
     return dp[0]
+
+
+def numDecodings3(s):
+        n = len(s)
+        @cache
+        def dfs(i):
+            if i == n:
+                return 1
+            if s[i] == "0":
+                return 0
+            res = dfs(i+1)
+            if i + 1 < n and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456"):
+                res += dfs(i+2)
+            return res
+        return dfs(0)
 
 print(numDecodings2("121376"))

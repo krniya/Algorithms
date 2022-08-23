38443f0db95ba689249d6f515dab5bf6488d5bd2
Nitish Kumar <krnitish@live.in>
2022-08-23 23:04:38 +0530

    Added Leetcode 234. Palindrome Linked List

---
+++ Python/Linked Lists/L_palindromeLinkedList.py
@@ -0,0 +1,12 @@
+def isPalindrome(head) -> bool:
+        slow, fast, prev = head, head, None
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        prev, slow, prev.next = slow, slow.next, None
+        while slow:
+            slow.next, prev, slow = prev, slow, slow.next
+        fast, slow = head, prev
+        while slow:
+            if fast.val != slow.val: return False
+            fast, slow = fast.next, slow.next
+        return True

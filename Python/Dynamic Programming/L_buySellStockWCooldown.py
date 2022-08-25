aefc0a1da680a0996a54d51631f0640014b83abb
Nitish Kumar <krnitish@live.in>
2022-08-26 00:08:24 +0530

    Updated Leetcode 309. Best Time to Buy and Sell Stock with Cooldown

--- Python/Dynamic Programming/L_buySellStockWCooldown.py
+++ Python/Dynamic Programming/L_buySellStockWCooldown.py
@@ -14,3 +14,14 @@ def maxProfit(prices) -> int:
                 cache[(i, buying)] = max(sell, cooldown)
             return cache[(i, buying)]
         return dfs(0, True)
+
+def maxProfit(prices) -> int:
+        n = len(prices)
+        if n <= 1:
+            return 0
+        dp = [[0, 0] for i in range(n+1)]
+        dp[1][1] = 0 - prices[0] 
+        for i in range(2, n+1):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i-1])    #don't buy or sell
+            dp[i][1] = max(dp[i-2][0] - prices[i-1], dp[i-1][1])     # buy on i-th day or don't buy
+        return max(dp[-1])

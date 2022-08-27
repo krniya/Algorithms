7ce7ca14b895a96b9f6f556c22b9323bb885c78b
Nitish Kumar <krnitish@live.in>
2022-08-27 16:14:08 +0530

    Updated Leetcode 36. Valid Sudoku

--- Python/Arrays/L_ValidateSudoku.py
+++ Python/Arrays/L_ValidateSudoku.py
@@ -1,3 +1,6 @@
+import collections
+
+
 def isValidSudoku(board):
     return (is_row_valid(board) and
             is_col_valid(board) and
@@ -33,8 +36,23 @@ def is_unit_valid(unit):
     return len(set(unit)) == len(unit)
 
 
+def isValidSudoku(board) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squr = collections.defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                curr = board[r][c]
+                if curr in rows[r] or curr in cols[c] or curr in squr[(r//3, c//3)]:
+                    return False
+                rows[r].add(curr)
+                cols[c].add(curr)
+                squr[(r//3, c//3)].add(curr)
+        return True
+
+
 print(isValidSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], [
       "4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))
 
-# print(isValidSudoku([["8", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], [
-#       "4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))

a126ffc743357a2de73454bc525f5f22bea5138d
Nitish Kumar <krnitish@live.in>
2022-08-29 23:19:39 +0530

    Added Leetcode CPP 70. Climbing Stairs

---
+++ cpp/Dynamic Programming/L_climbingStairs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int first = 1, second = 2, result = 0;
+        for (int i = 2; i < n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+
+int main() {
+
+}

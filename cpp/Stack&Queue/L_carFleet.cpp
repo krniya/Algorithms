79add1594fda89b712bc86a6db989224b4ca5773
Nitish Kumar <krnitish@live.in>
2022-08-30 23:00:17 +0530

    Added Leetcode CPP 853. Car Fleet

---
+++ cpp/Stack&Queue/L_carFleet.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int carFleet(int target, vector<int>& position, vector<int>& speed) {
+    int n = position.size();
+    vector<pair<int, double>> cars;
+    for (int i = 0; i < n; i++) {
+        double time = (double) (target - position[i]) / speed[i];
+        cars.push_back({position[i], time});
+    }
+    sort(cars.begin(), cars.end());
+    double maxTime = 0.0;
+    int result = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        double time = cars[i].second;
+        if (time > maxTime) {
+            maxTime = time;
+            result++;
+        }
+    }
+    return result;
+}
+int main() {
+
+}

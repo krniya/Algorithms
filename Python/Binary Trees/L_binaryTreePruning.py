1360c3b3d45d15079d9ae547070b5fb14d31740b
Nitish Kumar <krnitish@live.in>
2022-09-06 23:57:22 +0530

    Updated Leetcode 814. Binary Tree Pruning

--- Python/Binary Trees/L_binaryTreePruning.py
+++ Python/Binary Trees/L_binaryTreePruning.py
@@ -11,3 +11,11 @@ def pruneTree(root):
             return left or right or root.val
         dfs(root)
         return root if root.left or root.right or root.val else None
+
+
+def pruneTree(root):
+        if not root: return None
+        root.left = pruneTree(root.left)
+        root.right = pruneTree(root.right)
+        if not root.left and not root.right and not root.val: return None
+        return root

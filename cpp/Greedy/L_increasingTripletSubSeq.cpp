b44737450e628110449dd78a63622a6d6ab60979
Nitish Kumar <krnitish@live.in>
2022-10-11 13:46:05 +0530

    Added Leetcode CPP 334. Increasing Triplet Subsequence

---
+++ cpp/Greedy/L_increasingTripletSubSeq.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool increasingTriplet(vector<int>& nums) {
+    int first = INT_MAX, second = INT_MAX;
+    for(int n: nums) {
+        if(n <= first) first = n;
+        else if(n <= second) second = n;
+        else return true;
+    }
+    return false;
+}

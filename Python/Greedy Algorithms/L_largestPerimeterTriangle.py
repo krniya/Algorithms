e59fb6043bb9204d28bf52e854b63ef792841734
Nitish Kumar <krnitish@live.in>
2022-10-12 20:38:24 +0530

    Added Leetcode 976. Largest Perimeter Triangle

---
+++ Python/Greedy Algorithms/L_largestPerimeterTriangle.py
@@ -0,0 +1,6 @@
+def largestPerimeter(A) -> int:
+        A = sorted(A)[::-1]
+        for i in range(len(A) - 2):
+            if A[i] < A[i + 1] + A[i + 2]:
+                return A[i] + A[i + 1] + A[i + 2]
+        return 0

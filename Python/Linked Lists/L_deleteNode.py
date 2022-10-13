7abb8ef0595fd4b6747f69b32020c640be2de26f
Nitish Kumar <krnitish@live.in>
2022-10-13 20:22:57 +0530

    Added Leetcode 237. Delete Node in a Linked List

---
+++ Python/Linked Lists/L_deleteNode.py
@@ -0,0 +1,7 @@
+def deleteNode(node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next

59e86899a94373d6c246bf9e535f6b6a88d635d6
Nitish Kumar <krnitish@live.in>
2022-10-17 22:30:56 +0530

    Updated Leetcode CPP 1832. Check if the Sentence Is Pangram

--- cpp/String/L_sentencePangram.cpp
+++ cpp/String/L_sentencePangram.cpp
@@ -8,3 +8,15 @@ bool checkIfPangram(string sentence) {
     }
     return checked.size() == 26;
 }
+
+bool checkIfPangram(string sentence) {
+    
+    vector<int> freq(26);                      //create a frequency vector 
+    
+    for(auto ch:sentence) freq[ch-'a']++;      //update count of each character
+    
+    for(auto it:freq){                         //traverse freq vector
+        if(it==0) return false;                //if any aplhabet's occurence is 0
+    }                                          //return false;
+    return true;
+}

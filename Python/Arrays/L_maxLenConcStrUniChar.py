ed49387d6d4cd6128bd569c6777a24388602cc1b
Nitish Kumar <krnitish@live.in>
2022-10-24 16:17:05 +0530

    Updated Leetcode 1239. Maximum Length of a Concatenated String with Unique Characters

--- Python/Arrays/L_maxLenConcStrUniChar.py
+++ Python/Arrays/L_maxLenConcStrUniChar.py
@@ -8,3 +8,23 @@ def maxLength(A) -> int:
                 dp.append(a | c)
         return max(len(a) for a in dp)
 
+
+def maxLength(arr) -> int:
+        n = len(arr)
+        ans = 0
+        for mask in range(1 << n):
+            seen = set()
+            isValid = True
+            strSize = 0
+            for i in range(n):
+                if (mask >> i) & 1 == 0: continue
+                for c in arr[i]:
+                    if c in seen:   # If c is already existed then it's invalid subsequence.
+                        isValid = False
+                        break
+                    seen.add(c)  # mark as character `c` is already seen
+                    strSize += 1
+                if not isValid: break  # prune when there is a duplicate
+            if isValid and strSize > ans:
+                ans = strSize
+        return ans

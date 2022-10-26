3c5a0270a283743c500a3dfea3069d2ec73c04fd
Nitish Kumar <krnitish@live.in>
2022-10-26 22:21:01 +0530

    Added Leetcode CPP 523. Continuous Subarray Sum

---
+++ cpp/Array/L_continuousSubarrSum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkSubarraySum(vector<int>& nums, int k) {
+    if(nums.size()<2)
+        return false;
+    unordered_map<int, int> mp;
+    mp[0]=-1;
+    int runningSum=0;
+    for(int i=0;i<nums.size();i++)
+    {
+        runningSum+=nums[i];
+        if(k!=0) runningSum = runningSum%k;
+        if(mp.find(runningSum)!=mp.end())
+        {
+            if(i-mp[runningSum]>1)
+                return true;
+        }
+        else mp[runningSum]=i;          
+    }
+    return false;
+}

a5d9d1b4718a5b16338cb6d2176ebc689c9a356f
Nitish Kumar <krnitish@live.in>
2022-10-28 22:27:41 +0530

    Added Leetcode CPP  49. Group Anagrams

---
+++ cpp/String/L_groupAnagram.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+    }

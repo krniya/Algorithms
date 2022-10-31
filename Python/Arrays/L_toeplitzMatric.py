e6ade6e13c03bc54fdcba4ed33550353b3fc4a6b
Nitish Kumar <krnitish@live.in>
2022-10-31 21:38:43 +0530

    Added Leetcode 766. Toeplitz Matrix

---
+++ Python/Arrays/L_toeplitzMatric.py
@@ -0,0 +1,6 @@
+def isToeplitzMatrix(matrix) -> bool:
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i-1][j-1] != matrix[i][j]:
+                    return False
+        return True

1e276fdc5b0d978822b6b9fa4037da9bcacd9b8b
Nitish Kumar <krnitish@live.in>
2022-11-12 20:40:53 +0530

    Updated Leetcode 763. Partition Labels

--- Python/Strings/L_PartitionLabel.py
+++ Python/Strings/L_PartitionLabel.py
@@ -9,4 +9,20 @@ def partitionLabels(s):
             left = i+1
     return result
 
+
+
+def partitionLabels(s):
+        lastIndex = {}
+        for idx, ch in enumerate(s):
+            lastIndex[ch] = idx
+        res = []
+        size = end = 0
+        for idx, ch in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[ch])
+            if idx == end:
+                res.append(size)
+                size = 0
+        return res
+
 print(partitionLabels("ababcbacadefegdehijhklij"))

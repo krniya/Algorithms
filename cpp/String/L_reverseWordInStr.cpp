2bb49f2189acac618cce6fd75faffe582c0916a5
Nitish Kumar <krnitish@live.in>
2022-11-13 20:23:30 +0530

    Added Leetcode CPP 151. Reverse Words in a String

---
+++ cpp/String/L_reverseWordInStr.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        int l = 0, r = 0, i = 0, n = s.size();
+        while (i < n) {
+            while (i < n && s[i] == ' ') i++;
+            while (i < n && s[i] != ' ') s[r++] = s[i++];
+            
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                s[r++] = ' ';
+                l = r;
+            }
+            i++;
+        }
+        if (r > 0) s.resize(r - 1);
+        return s;
+    }

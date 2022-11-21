b8c618176e7947b87c0efbbbc976433c26aa8ec8
Nitish Kumar <krnitish@live.in>
2022-11-21 21:54:13 +0530

    Added Leetcode 1926. Nearest Exit from Entrance in Maze

---
+++ Python/Graphs/L_nearestExitInMaze.py
@@ -0,0 +1,15 @@
+def nearestExit(maze, entrance) -> int:
+        row = len(maze)
+        col = len(maze[0])
+        er, ec = entrance
+        maze[er][ec] = "+"
+        q = [(er,ec,0)]
+        while len(q):
+            i,j,c = q.pop(0)
+            if (i in [0,row-1] or j in [0,col-1]) and (er,ec) != (i,j):
+                return c
+            for x, y in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
+                if 0 <= x <row and 0 <= y <col and maze[x][y] == ".":
+                    maze[x][y] = "+"
+                    q.append((x,y,c+1))
+        return -1

779c27498c64f8a59fbcf226c227d622abe8d529
Nitish Kumar <krnitish@live.in>
2022-11-27 11:48:37 +0530

    Added Leetcode 446. Arithmetic Slices II - Subsequence

---
+++ Python/Dynamic Programming/L_ArithmeticSlicesII.py
@@ -0,0 +1,16 @@
+def numberOfArithmeticSlices(nums) -> int:
+        n = len(nums)
+        dp = [defaultdict(int) for _ in range(n)]
+        
+        ans = 0
+        for i in range(1, n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                cnt = 0
+                if diff in dp[j]:
+                    cnt = dp[j][diff]
+                    
+                dp[i][diff] += cnt + 1
+                ans += cnt
+                
+        return ans

695a8515f3917bc463605ef34349686f869ac150
Nitish Kumar <krnitish@live.in>
2022-12-03 13:19:11 +0530

    Added Leetcode CPP 451. Sort Characters By Frequency

---
+++ cpp/String/L_sortCharByFreq.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string frequencySort(string s) {
+        int counts[256] = {0};
+        for (char ch : s)
+            ++counts[ch];
+        sort(s.begin(), s.end(), [&](char a, char b) { 
+            return counts[a] > counts[b] || (counts[a] == counts[b] && a < b); 
+        });
+        return s;
+    }

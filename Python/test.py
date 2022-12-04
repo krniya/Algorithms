fa12b098ca4c0ea387d15bc6588835ccf30e8a45
Nitish Kumar <krnitish@live.in>
2022-12-04 20:38:47 +0530

    Added Leetcode 2256. Minimum Average Difference

--- Python/test.py
+++ Python/test.py
@@ -1,8 +1,1 @@
-
-data = open("C:\\Users\\NiYa\\Documents\\Algorithms\\Python\\data.txt", 'w+')
-file = open("C:\\Users\\NiYa\\Documents\\Algorithms\\Python\\test.txt", 'r')
-for each in file:
-    if each[0].isalpha():
-        data.write(each)
-
-
+print(0 // 0)

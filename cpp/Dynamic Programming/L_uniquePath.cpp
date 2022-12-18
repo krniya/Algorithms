9966716eef43423871149dd85de4aa818df26b08
Nitish Kumar <krnitish@live.in>
2022-12-18 22:37:07 +0530

    Added Leetcode CPP 62. Unique Paths

---
+++ cpp/Dynamic Programming/L_uniquePath.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int uniquePaths(int m, int n) {
+        vector<int> cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }

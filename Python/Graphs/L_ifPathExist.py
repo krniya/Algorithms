2c25ee0668f6a1f231e51ffad49a6d835c905bbb
Nitish Kumar <krnitish@live.in>
2022-12-19 22:04:20 +0530

    Added Leetcode 1971. Find if Path Exists in Graph

---
+++ Python/Graphs/L_ifPathExist.py
@@ -0,0 +1,30 @@
+def validPath(n: int, edges, source: int, destination: int) -> bool:
+        parent = [i for i in range(n)]
+        rank = [1] * (n)
+        
+        def find(n):
+            if n == parent[n]: return n
+            parent[n] = find(parent[n])
+            return parent[n]
+        
+        # Union
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        
+        for n1, n2 in edges:
+            union(n1, n2)
+
+        find(source)
+        find(destination)   
+        return parent[source] == parent[destination]
+
+print(validPath(10, [[2,6],[4,7],[1,2],[3,5],[7,9],[6,4],[9,8],[0,1],[3,0]],3,5))

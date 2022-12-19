e0cb78fe5378c55007d00019f0aab2b3bf7598a1
Nitish Kumar <krnitish@live.in>
2022-12-19 22:06:37 +0530

    Updated Leetcode 1971. Find if Path Exists in Graph

--- Python/Graphs/L_ifPathExist.py
+++ Python/Graphs/L_ifPathExist.py
@@ -27,4 +27,28 @@ def validPath(n: int, edges, source: int, destination: int) -> bool:
         find(destination)   
         return parent[source] == parent[destination]
 
+def validPath(n: int, edges, start: int, end: int) -> bool:
+        queue = [start]
+        graph = {}
+        for vertex in range(n):
+            graph[vertex] = []
+        
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+        
+        visited = {start}
+        
+        while len(queue) != 0:
+            node = queue.pop(0)
+            if node == end:
+                return True
+            
+            for vertex in graph[node]:
+                if vertex not in visited:
+                    visited.add(vertex)
+                    queue.append(vertex)
+        
+        return False
+
 print(validPath(10, [[2,6],[4,7],[1,2],[3,5],[7,9],[6,4],[9,8],[0,1],[3,0]],3,5))

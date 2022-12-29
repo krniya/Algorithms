499b75e93dbfd13847736dfee3ef5c85a7a315ae
Nitish Kumar <krnitish@live.in>
2022-12-29 22:15:35 +0530

    Added Leetcode 1834. Single-Threaded CPU

---
+++ Python/Heaps/L_singleThreadedCPU.py
@@ -0,0 +1,19 @@
+import heapq
+
+def getOrder(tasks):
+        res = []
+        tasks = sorted([(t[0], t[1], i) for i, t in enumerate(tasks)])
+        i = 0
+        h = []
+        time = tasks[0][0]
+        while len(res) < len(tasks):
+            while (i < len(tasks)) and (tasks[i][0] <= time):
+                heapq.heappush(h, (tasks[i][1], tasks[i][2])) # (processing_time, original_index)
+                i += 1
+            if h:
+                t_diff, original_index = heapq.heappop(h)
+                time += t_diff
+                res.append(original_index)
+            elif i < len(tasks):
+                time = tasks[i][0]
+        return res

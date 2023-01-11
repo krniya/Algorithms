7d52946891cf522157d8604ec0d876c92fe90eeb
Nitish Kumar <krnitish@live.in>
2023-01-11 22:19:19 +0530

    Added Leetcode 1443. Minimum Time to Collect All Apples in a Tree

---
+++ Python/Binary Trees/L_minTimeToCollectAllApple.py
@@ -0,0 +1,23 @@
+from typing import List
+from collections import defaultdict
+
+
+def minTime(n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        tree = defaultdict(list)
+        for s,e in edges:
+            tree[s].append(e)
+            tree[e].append(s)
+        
+        def dfs(node,par):
+            
+            res = 0
+            for nei in tree[node]:
+                if nei != par:
+                    res += dfs(nei,node)
+            
+            if res or hasApple[node]:
+                return res + 2
+
+            return res
+
+        return max(dfs(0,-1)-2, 0)

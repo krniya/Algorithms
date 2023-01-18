6a65c0ee0212d10817c8ee869d5d2e0d43fe49f6
Nitish Kumar <krnitish@live.in>
2023-01-18 21:12:30 +0530

    Added Leetcode 918. Maximum Sum Circular Subarray

---
+++ Python/Dynamic Programming/L_circularMaxSubArr.py
@@ -0,0 +1,13 @@
+def maxSubarraySumCircular(A) -> int:
+        if len(A) == 0:
+            return 0
+        maxTotal,maxSoFar,minSoFar,minTotal,s = A[0], A[0], A[0], A[0],A[0]
+        for i in range(1, len(A)):
+            maxSoFar = max(A[i], maxSoFar + A[i])
+            maxTotal = max(maxTotal, maxSoFar)            
+            minSoFar = min(A[i], minSoFar + A[i])            
+            minTotal = min(minTotal, minSoFar)            
+            s += A[i]
+        if(s == minTotal):
+            return maxTotal
+        return max(s - minTotal, maxTotal);

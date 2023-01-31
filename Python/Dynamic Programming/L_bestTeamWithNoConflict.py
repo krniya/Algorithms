5165e80243af462579d3f185c70fb7177180461f
Nitish Kumar <krnitish@live.in>
2023-01-31 22:23:18 +0530

    Added Leetcode 1626. Best Team With No Conflicts

---
+++ Python/Dynamic Programming/L_bestTeamWithNoConflict.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def bestTeamScore(scores: List[int], ages: List[int]) -> int:
+        dp = [0]*(1+max(ages))     
+                                              
+        score_age = sorted(zip(scores, ages))
+                                             
+        for score, age in score_age:         
+                                             
+            dp[age] = score + max(dp[:age+1])  
+                                               
+        return max(dp)

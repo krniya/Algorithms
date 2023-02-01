4e955ad4f2582406addf9f85ef8a364af92525b2
Nitish Kumar <krnitish@live.in>
2023-02-01 23:18:04 +0530

    Added Leetcode CPP 1071. Greatest Common Divisor of Strings

---
+++ cpp/String/L_greatestCommonDivisorrString.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string gcdOfStrings(string str1, string str2) {
+    if(str1 + str2 == str2 + str1){
+        return str1.substr(0, gcd(str1.length(), str2.length()));
+    }
+    return "";
+}
+

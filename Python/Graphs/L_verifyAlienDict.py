82bca95765f4f17aed5ad5762101a7fda65f7b22
Nitish Kumar <krnitish@live.in>
2023-02-02 22:25:08 +0530

    Added Leetcode 953. Verifying an Alien Dictionary

---
+++ Python/Graphs/L_verifyAlienDict.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def isAlienSorted(words: List[str], order: str) -> bool:
+        ind = {c: i for i, c in enumerate(order)}
+        for a, b in zip(words, words[1:]):
+            if len(a) > len(b) and a[:len(b)] == b:
+                return False
+            for s1, s2 in zip(a, b):
+                if ind[s1] < ind[s2]:
+                    break
+                elif ind[s1] > ind[s2]:
+                    return False
+        return True

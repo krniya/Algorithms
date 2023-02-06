2a2da9140c007f94b894faa833082cc14790fd56
Nitish Kumar <krnitish@live.in>
2023-02-06 22:58:38 +0530

    Added Leetcode CPP 1470. Shuffle the Array

---
+++ cpp/Array/L_shuffleTheArr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> shuffle(vector<int>& nums, int n) {
+        int len = nums.size();
+        for(int i = n; i < len; i++) {
+            nums[i] = (nums[i] << 10) | nums[i - n];
+        }
+        int index = 0;
+        for(int i = n; i < len; i++, index += 2) {
+            nums[index] = nums[i] & 1023;
+            nums[index + 1] = nums[i] >> 10;
+        }
+        return nums;
+    }

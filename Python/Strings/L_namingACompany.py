a67a8724f95a5c9e73fb023ebb9650d8067784ab
Nitish Kumar <krnitish@live.in>
2023-02-09 23:16:45 +0530

    Added Leetcode 2306. Naming a Company

---
+++ Python/Strings/L_namingACompany.py
@@ -0,0 +1,14 @@
+from typing import List
+from collections import defaultdict
+
+def distinctNames(ideas: List[str]) -> int:
+        count = defaultdict(set)
+        for a in ideas:
+            count[a[0]].add(hash(a[1:]))
+        res = 0
+        for a, seta in count.items():
+            for b, setb in count.items():
+                if a >= b: continue
+                same = len(seta & setb)
+                res += (len(seta) - same) * (len(setb) - same)
+        return res * 2

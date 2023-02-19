d780485efcfe5e8e7a0fbad040c9ebc1b2fc916f
Nitish Kumar <krnitish@live.in>
2023-02-19 23:05:38 +0530

    Added Leetcode 103. Binary Tree Zigzag Level Order Traversal

---
+++ Python/Binary Trees/L_BTZigZagLevelOrdTrav.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def zigzagLevelOrder(root):
+        if not root: return []
+        queue = deque([root])
+        result, direction = [], 1
+        
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:  queue.append(node.left)
+                if node.right: queue.append(node.right)
+            result.append(level[::direction])
+            direction *= (-1)
+        return result

ea02438c02b2e2fc530407b845f91df5aec3ac34
Nitish Kumar <krnitish@live.in>
2023-02-21 23:13:52 +0530

    Added Leetcode 540. Single Element in a Sorted Array

---
+++ Python/Arrays/L_singleEleInSortedArr.py
@@ -0,0 +1,9 @@
+def singleNonDuplicate(nums) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if (mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid%2 == 0 and nums[mid] == nums[mid + 1]):
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]

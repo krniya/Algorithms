df1a48ea421e53c7bc74cc98ecbbf0d040d8a826
Nitish Kumar <krnitish@live.in>
2023-02-22 23:11:59 +0530

    Added Leetcode 1011. Capacity To Ship Packages Within D Days

---
+++ Python/Arrays/L_capToShipWithinDdays.py
@@ -0,0 +1,12 @@
+def shipWithinDays(weights, D: int) -> int:
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid, need, cur = (left + right) // 2, 1, 0
+            for w in weights:
+                if cur + w > mid:
+                    need += 1
+                    cur = 0
+                cur += w
+            if need > D: left = mid + 1
+            else: right = mid
+        return left

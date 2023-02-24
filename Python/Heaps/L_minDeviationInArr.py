b290ceb72339beadce991638c36b73d8aec55e7b
Nitish Kumar <krnitish@live.in>
2023-02-24 22:23:53 +0530

    Added Leetcode 1675. Minimize Deviation in Array

---
+++ Python/Heaps/L_minDeviationInArr.py
@@ -0,0 +1,13 @@
+def minimumDeviation(A) -> int:
+        pq = []
+        for a in A:
+            heapq.heappush(pq, -a * 2 if a % 2 else -a)
+        res = float('inf')
+        mi = -max(pq)
+        while len(pq) == len(A):
+            a = -heapq.heappop(pq)
+            res = min(res, a - mi)
+            if a % 2 == 0:
+                mi = min(mi, a // 2)
+                heapq.heappush(pq, -a // 2)
+        return res

7fdf50035a1d15f10be36d8dd597c0ebc5f8c7c3
Nitish Kumar <krnitish@live.in>
2023-02-25 23:25:52 +0530

    Updated Leetcode 121. Best Time to Buy and Sell Stock

--- Python/Dynamic Programming/L_bestTimeBuySellStock.py
+++ Python/Dynamic Programming/L_bestTimeBuySellStock.py
@@ -10,3 +10,16 @@ def maxProfit(prices):
             sell += 1
         return maxProfit
     
+
+def maxProfit(prices):
+        left = 0 #Buy
+        right = 1 #Sell
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] #our current Profit
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit

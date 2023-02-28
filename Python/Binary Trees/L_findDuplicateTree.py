5d6884b6da6d99d83918f3f51147a4d44714372d
Nitish Kumar <krnitish@live.in>
2023-02-28 22:33:42 +0530

    Added Leetcode 652. Find Duplicate Subtrees

---
+++ Python/Binary Trees/L_findDuplicateTree.py
@@ -0,0 +1,12 @@
+import collections
+
+def findDuplicateSubtrees(root):
+        def trv(root):
+            if not root: return "null"
+            struct = "%s,%s,%s" % (str(root.val), trv(root.left), trv(root.right))
+            nodes[struct].append(root)
+            return struct
+        
+        nodes = collections.defaultdict(list)
+        trv(root)
+        return [nodes[struct][0] for struct in nodes if len(nodes[struct]) > 1]

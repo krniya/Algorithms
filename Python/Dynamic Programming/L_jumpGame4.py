f6860a8158a7932e3f457aa9e46752b22c3b51db
Nitish Kumar <krnitish@live.in>
2023-03-05 22:49:11 +0530

    Added Leetcode 1345. Jump Game IV

--- Python/Dynamic Programming/L_jumpGame4.py
+++ Python/Dynamic Programming/L_jumpGame4.py
@@ -1,15 +1,37 @@
-from collections import deque
-
-
-def maxResult(nums, k: int) -> int:
-        n = len(nums)
-        dq = deque([0])  # store index of `nums` elements, elements is in decreasing order, the front is the maximum element.
-        for i in range(1, n):
-            # nums[i] = max(nums[i-k], nums[i-k+1],.., nums[i-1]) + nums[i] = nums[dq.front()] + nums[i]
-            nums[i] = nums[dq[0]] + nums[i]
-            # Add a nums[i] to the deq
-            while dq and nums[dq[-1]] <= nums[i]: dq.pop()  # Eliminate elements less or equal to nums[i], which will never be chosen in the future
-            dq.append(i)
-            # Remove if the last element is out of window size k
-            if i - dq[0] >= k: dq.popleft()
-        return nums[n - 1]
+def minJumps(arr) -> int:
+        n = len(arr)
+        if n == 1:
+            return 0
+        
+        # Create a dictionary to store the indices of each value in the array
+        indices = defaultdict(list)
+        for i, num in enumerate(arr):
+            indices[num].append(i)
+        
+        # Initialize variables for the BFS algorithm
+        visited = set()
+        q = deque([(0, 0)]) # (index, steps)
+        
+        while q:
+            index, steps = q.popleft()
+            
+            # Check if we have reached the end of the array
+            if index == n - 1:
+                return steps
+            
+            # Add neighbors to the queue
+            for neighbor in indices[arr[index]]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    q.append((neighbor, steps + 1))
+            
+            # Clear the list of indices for this value, since we have already visited them
+            indices[arr[index]] = []
+            
+            # Add adjacent indices to the queue
+            if index - 1 not in visited and index - 1 >= 0:
+                visited.add(index - 1)
+                q.append((index - 1, steps + 1))
+            if index + 1 not in visited and index + 1 < n:
+                visited.add(index + 1)
+                q.append((index + 1, steps + 1))

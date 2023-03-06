568834297f7dd598ed58b293bdc402480d245a77
Nitish Kumar <krnitish@live.in>
2023-03-06 23:00:37 +0530

    Added Leetcode CPP 1539. Kth Missing Positive Number

---
+++ cpp/Array/L_KthMissingPosNum.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int findKthPositive(vector<int>& arr, int k) {
+        int lo = 0, hi = arr.size();
+        while (lo < hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (arr[mid] - mid > k) hi = mid;
+        else lo = mid + 1;
+    }
+    return lo + k;
+    }

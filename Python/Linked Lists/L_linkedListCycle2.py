49aaeed82264c70cb66cdf210ef50f8ea7707316
Nitish Kumar <krnitish@live.in>
2023-03-09 21:47:20 +0530

    Added Leetcode 142. Linked List Cycle II

---
+++ Python/Linked Lists/L_linkedListCycle2.py
@@ -0,0 +1,13 @@
+def detectCycle(head):
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                slow = head
+                while slow != fast:
+                    fast = fast.next
+                    slow = slow.next
+                return slow
+        return None

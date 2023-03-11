fc0bbc5beed272dd39363747a7b10709adfb99ee
Nitish Kumar <krnitish@live.in>
2023-03-11 22:56:34 +0530

    Added Leetcode CPP 109. Convert Sorted List to Binary Search Tree

---
+++ cpp/Binary Tree/L_convertSortedListIntoBST.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+ TreeNode* sortedListToBST(ListNode* head) {
+        if(head == NULL) return NULL;
+        if(head->next == NULL) return new TreeNode(head->val);
+        ListNode* middle = getMiddle(head);
+        TreeNode* root = new TreeNode(middle->val);
+        root->right = sortedListToBST(middle->next);
+        middle->next = NULL;
+        root->left = sortedListToBST(head);
+        return root;
+    }
+    
+    ListNode* getMiddle(ListNode* head){
+        //if(head == NULL || head->next==NULL) return NULL;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* prev = NULL;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
+        }
+        if(prev!=NULL) prev->next = NULL;
+        return slow;
+    }

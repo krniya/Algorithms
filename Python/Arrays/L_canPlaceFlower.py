49a11778c729cf7138d5f3200f538b2130216165
Nitish Kumar <krnitish@live.in>
2023-03-20 22:56:52 +0530

    Added Leetcode 605. Can Place Flowers

---
+++ Python/Arrays/L_canPlaceFlower.py
@@ -0,0 +1,10 @@
+def canPlaceFlowers(flowerbed, n: int) -> bool:
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False

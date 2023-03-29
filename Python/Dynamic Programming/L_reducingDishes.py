459b26dd6e38649e9b16d1fe7be71fbcbdc45087
Nitish Kumar <krnitish@live.in>
2023-03-29 22:57:34 +0530

    Added leetcode 1402. Reducing Dishes

---
+++ Python/Dynamic Programming/L_reducingDishes.py
@@ -0,0 +1,10 @@
+def maxSatisfaction(satisfaction) -> int:
+        satisfaction.sort(reverse=True)
+        n = len(satisfaction)
+        presum, res = 0, 0
+        for i in range(n):
+            presum += satisfaction[i]
+            if presum < 0:
+                break
+            res += presum
+        return res

a43ae3479bbd36e8b778a5a89bc95f352dd54025
Nitish Kumar <krnitish@live.in>
2023-03-30 23:03:35 +0530

    Added Leetcode 87. Scramble String

---
+++ Python/Dynamic Programming/L_scrambleString.py
@@ -0,0 +1,18 @@
+def isScramble(s1: str, s2: str) -> bool:
+        m ={}
+        def func(s1, s2):
+            if (s1, s2) in m:
+                return m[(s1, s2)]
+            if not sorted(s1) == sorted(s2):
+                return False
+            if len(s1) == 1:
+                return True
+            
+
+            for i in range(1, len(s1)):
+                if func(s1[:i], s2[-i:]) and func(s1[i:], s2[:-i]) or func(s1[:i], s2[:i]) and func(s1[i:], s2[i:]):
+                    m[(s1, s2)] = True
+                    return True
+            m[(s1, s2)] = False
+            return False
+        return func(s1, s2)

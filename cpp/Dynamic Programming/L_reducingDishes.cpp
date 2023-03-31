0000a972de166a6cd3548ec910c64b257ba34a2e
Nitish Kumar <krnitish@live.in>
2023-03-31 22:17:18 +0530

    Added Leetcode CPP 1402. Reducing Dishes

---
+++ cpp/Dynamic Programming/L_reducingDishes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxSatisfaction(vector<int>& satisfaction) {
+        vector<int>v=satisfaction;
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+        int sum=0,ans=0;
+        for(int i=0;i<v.size();i++){
+            if(sum+v[i]>0){
+                ans+=sum+v[i];
+                sum+=v[i];
+            }
+        }
+        return ans;
+    }

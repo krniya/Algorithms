89ac3235f7e83a8113bcc2de437c03a36d7b5f98
Nitish Kumar <krnitish@live.in>
2023-04-02 22:46:26 +0530

    Updated Leetcode 2300. Successful Pairs of Spells and Potions

--- Python/Arrays/L_sucessFullPotionPair.py
+++ Python/Arrays/L_sucessFullPotionPair.py
@@ -9,3 +9,23 @@ def successfulPairs(spells: List[int], potions: List[int], success: int) -> List
             factor = (success + s - 1) // s
             pairs.append(len(potions) - bisect.bisect_left(potions, factor))
         return pairs
+
+
+def successfulPairs(spells: List[int], potions: List[int], success: int) -> List[int]:
+        n = len(spells)
+        m = len(potions)
+        pairs = [0] * n
+        potions.sort()
+        for i in range(n):
+            spell = spells[i]
+            left = 0
+            right = m - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                product = spell * potions[mid]
+                if product >= success:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            pairs[i] = m - left
+        return pairs

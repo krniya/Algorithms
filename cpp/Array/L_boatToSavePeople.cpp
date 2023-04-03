e7c97dca54b78e513f92802db5c3ef174e1fcad1
Nitish Kumar <krnitish@live.in>
2023-04-03 22:31:23 +0530

    Added Leetcode CPP 881. Boats to Save People

---
+++ cpp/Array/L_boatToSavePeople.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+nt numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        
+        int i = 0, j = people.size() - 1,cnt = 0;
+        
+        while(i <= j)
+        {   
+            // lightest person + heaviest person sum <= limit
+            // they can go together
+            if(people[i] + people[j] <= limit)
+            {
+                ++i;
+                --j;
+            }
+            // if sum is over the limit,
+            // heaviest will go alone.
+            else
+                --j;
+            
+            ++cnt;  // number of boats
+        }
+        
+        return cnt;
+    }

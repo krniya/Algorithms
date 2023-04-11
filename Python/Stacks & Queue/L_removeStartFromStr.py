6dd00b363b8d68f5d91b29e2666aad03cca673f8
Nitish Kumar <krnitish@live.in>
2023-04-11 21:43:18 +0530

    Added Leetcode 2390. Removing Stars From a String

---
+++ Python/Stacks & Queue/L_removeStartFromStr.py
@@ -0,0 +1,8 @@
+def removeStars(s: str) -> str:
+        ans=[]
+        for i in s:
+            if i=='*':
+                ans.pop()
+            else:
+                ans+=[i]
+        return "".join(ans)

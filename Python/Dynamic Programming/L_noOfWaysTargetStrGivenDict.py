c412dfb172ecf24335a2e37194734e1d144e7dba
Nitish Kumar <krnitish@live.in>
2023-04-16 23:02:20 +0530

    Added Leetcode 1639. Number of Ways to Form a Target String Given a Dictionary

---
+++ Python/Dynamic Programming/L_noOfWaysTargetStrGivenDict.py
@@ -0,0 +1,18 @@
+def numWays(words, target: str) -> int:
+        n = len(words[0])
+        m = len(target)
+        mod = 10**9 + 7
+        dp = [0] * (m+1)
+        dp[0] = 1
+        
+        count = [[0] * 26 for _ in range(n)]
+        for i in range(n):
+            for word in words:
+                count[i][ord(word[i]) - ord('a')] += 1
+        
+        for i in range(n):
+            for j in range(m-1, -1, -1):
+                dp[j+1] += dp[j] * count[i][ord(target[j]) - ord('a')]
+                dp[j+1] %= mod
+        
+        return dp[m]

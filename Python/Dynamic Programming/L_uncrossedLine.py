293506f59791f1c40d5fc5237bc1799ae5a11a7f
Nitish Kumar <krnitish@live.in>
2023-05-11 22:53:19 +0530

    Added Leetcode 1035. Uncrossed Lines

---
+++ Python/Dynamic Programming/L_uncrossedLine.py
@@ -0,0 +1,14 @@
+def maxUncrossedLines(nums1, nums2) -> int:
+        dp = {}
+        def dfs(i,j):
+            if i == len(nums1) or j == len(nums2):
+                return 0
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if nums1[i] == nums2[j]:
+                dp[(i,j)] = 1 + dfs(i+1, j+1)
+            else:
+                dp[(i,j)] = max(dfs(i+1, j), dfs(i,j+1))
+            return dp[(i,j)]
+        
+        return dfs(0,0)

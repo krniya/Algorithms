8a931e7cb43254981d2ec5a2474122d38f228c17
Nitish Kumar <krnitish@live.in>
2023-05-18 22:47:53 +0530

    Added Leetcode CPP 1557. Minimum Number of Vertices to Reach All Nodes

---
+++ cpp/Graph/L_minNoOfVirticesToReachNode.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> res, seen(n);
+        for (auto& e: edges)
+            seen[e[1]] = 1;
+        for (int i = 0; i < n; ++i)
+            if (seen[i] == 0)
+                res.push_back(i);
+        return res;
+    }

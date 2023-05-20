e688fd895655bb5eadba1ad4ab1d4ba5c151dd7d
Nitish Kumar <krnitish@live.in>
2023-05-20 22:50:23 +0530

    Added Leetcode CPP 399. Evaluate Division

--- cpp/Linked List/L_maxTwinSumLL.cpp
+++ cpp/Linked List/L_maxTwinSumLL.cpp
@@ -1,28 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(NULL) {}
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(NULL) {}
 };
 
-int pairSum(ListNode* head) {
-        ListNode* slow = head;
-	ListNode* fast = head;
+int pairSum(ListNode *head)
+{
+	ListNode *slow = head;
+	ListNode *fast = head;
 	int maxVal = 0;
 
 	// Get middle of linked list
-	while(fast && fast -> next)
+	while (fast && fast->next)
 	{
-		fast = fast -> next -> next;
-		slow = slow -> next;
+		fast = fast->next->next;
+		slow = slow->next;
 	}
 
 	// Reverse second part of linked list
 	ListNode *nextNode, *prev = NULL;
-	while (slow) {
+	while (slow)
+	{
 		nextNode = slow->next;
 		slow->next = prev;
 		prev = slow;
@@ -30,12 +33,12 @@ int pairSum(ListNode* head) {
 	}
 
 	// Get max sum of pairs
-	while(prev)
+	while (prev)
 	{
-		maxVal = max(maxVal, head -> val + prev -> val);
-		prev = prev -> next;
-		head = head -> next;
+		maxVal = max(maxVal, head->val + prev->val);
+		prev = prev->next;
+		head = head->next;
 	}
 
 	return maxVal;
-    }
+}

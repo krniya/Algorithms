311ff39ccae3bf454f6446a01c31a5bb840071b6
Nitish Kumar <krnitish@live.in>
2023-05-26 22:02:40 +0530

    Added Leetcode 1140. Stone Game II

---
+++ Python/Dynamic Programming/L_stoneGame2.py
@@ -0,0 +1,10 @@
+def stoneGameII(A) -> int:
+        N = len(A)
+        for i in range(N - 2, -1, -1):
+            A[i] += A[i + 1]
+        from functools import lru_cache
+        @lru_cache(None)
+        def dp(i, m):
+            if i + 2 * m >= N: return A[i]
+            return A[i] - min(dp(i + x, max(m, x)) for x in range(1, 2 * m + 1))
+        return dp(0, 1)

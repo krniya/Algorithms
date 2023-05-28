a844933adf33377085f411c0c9625286e0a85f8e
Nitish Kumar <krnitish@live.in>
2023-05-28 22:57:07 +0530

    Added Leetcode 1547. Minimum Cost to Cut a Stick

---
+++ Python/Dynamic Programming/L_minCostToCutStick.py
@@ -0,0 +1,14 @@
+def minCost(n: int, cuts) -> int:
+        dp = {}
+        def dfs(l,r):
+            if r - l == 1:
+                return 0
+            if (l,r) in dp:
+                return dp[(l,r)]
+            res = float("inf")
+            for c in cuts:
+                if l < c < r:
+                    res = min(res, (r-l) + dfs(l,c) + dfs(c, r))
+            dp[(l,r)]= res= 0 if res == float("inf") else res
+            return res
+        return dfs(0,n)

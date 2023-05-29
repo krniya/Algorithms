b557b9c6cc4e93e3f6698701fe711a3fac9bf36e
Nitish Kumar <krnitish@live.in>
2023-05-29 13:22:59 +0530

    Added Leetcode JS 2620. Counter

---
+++ JavaScript/LeetCodeJSChallenge/D2_2620_Counter.js
@@ -0,0 +1,13 @@
+var createCounter = function (n) {
+    let count = n;
+    return function () {
+        return count++;
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */

68b366673c54247ee050809033bd02bf27094e84
Nitish Kumar <krnitish@live.in>
2023-06-01 22:10:24 +0530

    Added Leetcode JS 2666. Allow One Function Call

---
+++ JavaScript/LeetCodeJSChallenge/D8_2666_allowOneFnCall.js
@@ -0,0 +1,10 @@
+var once = function (fn) {
+    let called = false;
+    return function (...args) {
+        if (called) {
+            return undefined;
+        }
+        called = true;
+        return fn(...args);
+    };
+};

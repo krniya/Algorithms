eac2d2d3c95d6e28f0597d58d8440990c710c9d9
Nitish Kumar <krnitish@live.in>
2023-06-05 14:07:44 +0530

    Added Leetcode 1232. Check If It Is a Straight Line

---
+++ Python/Arrays/L_checkIfIsAStraightLine.py
@@ -0,0 +1,10 @@
+def checkStraightLine(coordinates) -> bool:
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+
+        for i in range(2, len(coordinates)):
+            x, y = coordinates[i]
+            if (x - x0) * (y1 - y0) != (y - y0) * (x1 - x0):
+                return False
+
+        return True

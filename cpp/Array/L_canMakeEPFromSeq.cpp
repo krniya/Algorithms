49c8bab7b0fd9e3583e5646960b79522064bc46e
Nitish Kumar <krnitish@live.in>
2023-06-06 22:12:52 +0530

    Added leetcode CPP 1502. Can Make Arithmetic Progression From Sequence

---
+++ cpp/Array/L_canMakeEPFromSeq.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canMakeArithmeticProgression(vector<int> &arr)
+{
+    std::sort(arr.begin(), arr.end());
+    int diff = arr[1] - arr[0];
+
+    for (int i = 2; i < arr.size(); i++)
+    {
+        if (arr[i] - arr[i - 1] != diff)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}

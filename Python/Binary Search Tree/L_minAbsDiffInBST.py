546843188b4715cc96a7d32b7636416a848c19e0
Nitish Kumar <krnitish@live.in>
2023-06-14 22:18:14 +0530

    Added Leetcode 530. Minimum Absolute Difference in BST

---
+++ Python/Binary Search Tree/L_minAbsDiffInBST.py
@@ -0,0 +1,13 @@
+def getMinimumDifference(root) -> int:
+        cur, stack, minDiff, prev = root, [], 10**5, -10**5
+        
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            node = stack.pop()
+            minDiff = min(minDiff, node.val - prev)
+            prev = node.val
+            cur = node.right
+        
+        return minDiff

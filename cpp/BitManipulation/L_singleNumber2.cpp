cbf7581b64dbc5598115dd29d7f9104ca05e2007
Nitish Kumar <krnitish@live.in>
2023-07-04 22:39:04 +0530

    Added Leetcode CPP 137. Single Number II

---
+++ cpp/BitManipulation/L_singleNumber2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    unordered_map<int, int> m;
+    for (auto x : nums)
+    {
+        m[x]++;
+    }
+    for (auto x : m)
+    {
+        if (x.second == 1)
+        {
+            return x.first;
+        }
+    }
+    return -1;
+}

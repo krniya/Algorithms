d8fe93005932bf2f4308cb0bd712fefb98916e3d
Nitish Kumar <krnitish@live.in>
2023-07-08 23:22:02 +0530

    Added Leetcode 2551. Put Marbles in Bags

---
+++ Python/Arrays/L_putMarblesInBag.py
@@ -0,0 +1,14 @@
+def putMarbles(weights, k: int) -> int:
+        n = len(weights)
+        pair_weights = [0] * (n - 1)
+        for i in range(n - 1):
+            pair_weights[i] = weights[i] + weights[i + 1]
+        pair_weights.sort()
+        
+        # Get the difference between the largest k - 1 values and the 
+        # smallest k - 1 values.
+        answer = 0
+        for i in range(k - 1):
+            answer += pair_weights[n - 2 - i] - pair_weights[i]
+            
+        return answer

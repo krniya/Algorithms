d06930514ebe03608d305702a9beb91499e01346
Nitish Kumar <krnitish@live.in>
2023-07-12 23:22:38 +0530

    Updated Leetcode 802. Find Eventual Safe States

--- Python/Graphs/L_EventualSafeNode.py
+++ Python/Graphs/L_EventualSafeNode.py
@@ -10,5 +10,25 @@ def eventualSafeNodes(graph):
         for i in range(len(graph)):
             if visited[i] == -1: explore(i)
         return sorted(res)
+    
+    
+def eventualSafeNodes(graph):
+        safe = {}
+        n = len(graph)
+        def dfs(i):
+            if i in safe:
+                return safe[i]
+            safe[i] = False
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return False
+            safe[i] = True
+            return True
+        
+        ans = []
+        for i in range(n):
+            if dfs(i):
+                ans.append(i)
+        return ans
 
 print(eventualSafeNodes([[1,2],[2,3],[5],[0],[5],[],[]]))

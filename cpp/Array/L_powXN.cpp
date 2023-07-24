9f64cff6652ca05f4b043ec240cb5b3af91e41d8
Nitish Kumar <krnitish@live.in>
2023-07-24 22:39:31 +0530

    Added Leetcode CPP 50. Pow(x, n)

---
+++ cpp/Array/L_powXN.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double myPow(double x, int n)
+{
+    if (n < 0)
+        return 1.0 / pow(x, (long long)-1 * n);
+    return pow(x, (long long)n);
+}
+
+double pow(double x, long long n)
+{
+    if (n == 0)
+        return 1;
+    if (n % 2 == 0)
+        return pow(x * x, n / 2);
+    else
+        return x * pow(x * x, (n - 1) / 2);
+}

6e14433e3e93bbdabaf46019069d271ff5649065
Nitish Kumar <krnitish@live.in>
2023-08-07 22:18:37 +0530

    Updated Leetcode 74. Search a 2D Matrix

--- Python/Arrays/L_searchIn2DMatrix2.py
+++ Python/Arrays/L_searchIn2DMatrix2.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def searchMatrix(matrix, target: int) -> bool:
         i, j = 0, len(matrix[0]) - 1
         while i < len(matrix) and j > -1:
@@ -7,4 +10,30 @@ def searchMatrix(matrix, target: int) -> bool:
                 j-=1
             else:
                 return True
+        return False
+    
+    
+def search_matrix(matrix: List[List[int]], target: int) -> bool:
+        row, col = len(matrix), len(matrix[0])
+        top, bot = 0, row - 1
+        while top <= bot:
+            mid = (top + bot) // 2
+            if target < matrix[mid][0]:
+                bot = mid - 1
+            elif target > matrix[mid][-1]:
+                top = mid + 1
+            else:
+                break
+        if top > bot:
+            return False
+        fRow = (top + bot) // 2
+        left, right = 0, col - 1
+        while left <= right:
+            m = (left + right) // 2
+            if target > matrix[fRow][m]:
+                left = m + 1
+            elif target < matrix[fRow][m]:
+                right = m - 1
+            else:
+                 return True
         return False

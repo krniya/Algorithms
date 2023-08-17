af0370a41267c6b98051e87ca282274ef53d1a6e
Nitish Kumar <krnitish@live.in>
2023-08-17 22:58:53 +0530

    Updated Leetcode 542. 01 Matrix

--- Python/Graphs/L_01Matrix.py
+++ Python/Graphs/L_01Matrix.py
@@ -1,6 +1,9 @@
+from collections import deque
+
+
 def updateMatrix(matrix):
     visited = set()
-    from collections import deque
+
     q = deque()
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
@@ -17,5 +20,27 @@ def updateMatrix(matrix):
                 visited.add((newX, newY))
                 q.append((newX, newY))
     return matrix
+
+
+def updateMatrix1(mat):
+        m, n = len(mat), len(mat[0])
+        DIR = [0, 1, 0, -1, 0]
+
+        q = deque([])
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    q.append((r, c))
+                else:
+                    mat[r][c] = -1  # Marked as not processed yet!
+
+        while q:
+            r, c = q.popleft()
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i + 1]
+                if nr < 0 or nr == m or nc < 0 or nc == n or mat[nr][nc] != -1: continue
+                mat[nr][nc] = mat[r][c] + 1
+                q.append((nr, nc))
+        return mat
 
 print(updateMatrix([[0,0,0],[0,1,0],[1,1,1]]))

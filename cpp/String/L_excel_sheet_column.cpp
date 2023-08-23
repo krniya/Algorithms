ad8bcd19d8ead828a94d28742d5df3cabbb6d1b6
Nitish Kumar <krnitish@live.in>
2023-08-23 20:14:13 +0530

    Added Leetcode CPP 168. Excel Sheet Column Title

---
+++ cpp/String/L_excel_sheet_column.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string convertToTitle(int n)
+{
+    string res;
+    char tmp;
+    while (n)
+    {
+        n -= 1;
+        tmp = 'A' + n % 26;
+        res = tmp + res;
+        n /= 26;
+    }
+    return res;
+}

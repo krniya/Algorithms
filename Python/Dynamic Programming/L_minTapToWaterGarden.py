f246c732d69f87c96de0f826b73780398b5f407b
Nitish Kumar <krnitish@live.in>
2023-08-31 22:47:59 +0530

    Added Leetcode 1326. Minimum Number of Taps to Open to Water a Garden

---
+++ Python/Dynamic Programming/L_minTapToWaterGarden.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def minTaps(n: int, ranges: List[int]) -> int:
+    max_range = [0] * (n + 1)
+    for i in range(len(ranges)):
+        left = max(0, i - ranges[i])
+        right = min(n, i + ranges[i])
+        max_range[left] = max(max_range[left], right)
+    end = farthest = taps = 0
+    i = 0
+    while end < n:
+        while i <= end:
+            farthest = max(farthest, max_range[i])
+            i += 1
+        if farthest <= end:
+            return -1  # Unable to cover the entire garden
+        end = farthest
+        taps += 1
+    return taps
+      

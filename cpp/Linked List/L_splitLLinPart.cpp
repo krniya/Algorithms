ae529bdc0dcf9bc5bb30103d196346062d803984
Nitish Kumar <krnitish@live.in>
2023-09-06 23:01:58 +0530

    Added Leetcode CPP 725. Split Linked List in Parts

---
+++ cpp/Linked List/L_splitLLinPart.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+vector<ListNode *> splitListToParts(ListNode *head, int k)
+{
+    int length = 0;
+    ListNode *current = head;
+    vector<ListNode *> parts;
+
+    while (current)
+    {
+        length++;
+        current = current->next;
+    }
+
+    int base_size = length / k, extra = length % k;
+    current = head;
+
+    for (int i = 0; i < k; ++i)
+    {
+        int part_size = base_size + (extra > 0);
+        ListNode *part_head = nullptr, *part_tail = nullptr;
+
+        for (int j = 0; j < part_size; ++j)
+        {
+            if (!part_head)
+            {
+                part_head = part_tail = current;
+            }
+            else
+            {
+                part_tail->next = current;
+                part_tail = part_tail->next;
+            }
+
+            if (current)
+            {
+                current = current->next;
+            }
+        }
+
+        if (part_tail)
+        {
+            part_tail->next = nullptr;
+        }
+
+        parts.push_back(part_head);
+        extra = max(extra - 1, 0);
+    }
+
+    return parts;
+}

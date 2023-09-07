7d82a6c89e834b7bedeef84909b7d160db2b15a6
Nitish Kumar <krnitish@live.in>
2023-09-07 22:39:06 +0530

    Updated Leetcode 92. Reverse Linked List II

--- Python/Linked Lists/L_reverseLinkedList2.py
+++ Python/Linked Lists/L_reverseLinkedList2.py
@@ -11,3 +11,25 @@ def reverseBetween(head, left: int, right: int):
             temp.next = prev.next
             prev.next = temp
         return dummy.next
+    
+    
+    
+    
+    
+def reverseBetween(head, left: int, right: int):    
+    temp = ListNode(-1)
+    prev = temp
+    temp.next = head
+    
+    for i in range(left - 1):
+        prev = prev.next
+    
+    cur = prev.next
+    
+    for i in range(right - left):
+        ptr = prev.next
+        prev.next = cur.next
+        cur.next = cur.next.next
+        prev.next.next = ptr
+    
+    return temp.next

5ff78dff686cf88d85bde5de4157f748730bbf96
Nitish Kumar <krnitish@live.in>
2023-09-13 23:28:35 +0530

    Updated Leetcode CPP 141. Linked List Cycle

--- cpp/Linked List/L_detectCycleLL.cpp
+++ cpp/Linked List/L_detectCycleLL.cpp
@@ -1,30 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
- 
 
-
-bool hasCycle(ListNode *head) {
-    if (!head) return false;
+bool hasCycle(ListNode *head)
+{
+    if (!head)
+        return false;
     ListNode *fast = head;
     ListNode *slow = head;
-    while(fast && fast->next) {
+    while (fast && fast->next)
+    {
         slow = slow->next;
         fast = fast->next->next;
-        if (fast == slow) {
+        if (fast == slow)
+        {
             return true;
         }
     }
     return false;
 }
 
+bool hasCycle(ListNode *head)
+{
+    unordered_set<ListNode *> st;
+    while (head != NULL)
+    {
+        if (st.find(head) != st.end())
+        {
+            return true;
+        }
+        st.insert(head);
+        head = head->next;
+    }
+    return false;
+}
 
-
-int main() {
-    cout << minDistance("sea","eat");
-
+int main()
+{
 }

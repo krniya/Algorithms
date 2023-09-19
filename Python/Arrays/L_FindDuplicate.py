e4fc555811ab9e0391f2b472803fa21aed09eaeb
Nitish Kumar <krnitish@live.in>
2023-09-19 23:00:32 +0530

    Updated Leetcode 287. Find the Duplicate Number

--- Python/Arrays/L_FindDuplicate.py
+++ Python/Arrays/L_FindDuplicate.py
@@ -13,3 +13,18 @@ def findDuplicate(nums: List[int]) -> int:
         t = nums[t]
         h = nums[h]
     return t
+
+def findDuplicate1(nums: List[int]) -> int:
+        slow,fast=0,0
+        while True:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+            if slow==fast:
+                break
+
+        slow2=0
+        while True:
+            slow=nums[slow]
+            slow2=nums[slow2]
+            if slow==slow2:
+                return slow   

569d0090b94d71d751b7231d61956c6547548435
Nitish Kumar <krnitish@live.in>
2023-09-20 20:56:50 +0530

    Updated Leetcode 1658. Minimum Operations to Reduce X to Zero

--- Python/Arrays/L_minOprToredToZero.py
+++ Python/Arrays/L_minOprToredToZero.py
@@ -13,3 +13,36 @@ def minOperations(self, nums: List[int], x: int) -> int:
                 max_len = max(max_len, end_idx - start_idx + 1)
 
         return len(nums) - max_len if found else -1
+
+
+class Solution:
+2
+    def minOperations(self, nums: List[int], x: int) -> int:
+3
+        target = sum(nums) - x
+4
+        curr_sum, max_len = 0, 0
+5
+        start_idx = 0
+6
+        found = False
+7
+        for end_idx in range(len(nums)):
+8
+            curr_sum += nums[end_idx]
+9
+            while start_idx <= end_idx and curr_sum > target:
+10
+                curr_sum -= nums[start_idx]
+11
+                start_idx += 1
+12
+            if curr_sum == target:
+13
+                found = True
+14
+                max_len = max(max_len, end_idx - start_idx + 1)
+15
+​
+16
+        return len(nums) - max_len if found else -1

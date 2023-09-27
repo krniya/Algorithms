4315a6362ab59a60825567f59500b94b2a786907
Nitish Kumar <krnitish@live.in>
2023-09-27 21:36:31 +0530

    Added Leetcode 880. Decoded String at Index

---
+++ Python/Strings/L_decodeStringAtIdx.py
@@ -0,0 +1,20 @@
+def decodeAtIndex(s: str, k: int):
+    length = 0
+    i = 0
+    
+    while length < k:
+        if s[i].isdigit():
+            length *= int(s[i])
+        else:
+            length += 1
+        i += 1
+    
+    for j in range(i-1, -1, -1):
+        char = s[j]
+        if char.isdigit():
+            length //= int(char)
+            k %= length
+        else:
+            if k == 0 or k == length:
+                return char
+            length -= 1

fadebe6544ea5b585fb18b4bb2283e171913c6a7
Nitish Kumar <krnitish@live.in>
2023-09-28 21:47:54 +0530

    Added Leetcode CPP 905. Sort Array By Parity

---
+++ cpp/Array/L_sortArrByParity.cpp
@@ -0,0 +1,12 @@
+vector<int> sortArrayByParity(vector<int>& A) {
+    int j=0;
+    for(int i=0;i<A.size();i++)
+    {
+        if(A[i]%2==0)
+        {
+            swap(A[i],A[j]);
+            j++;
+        }
+    }
+    return A;
+}

9db9cdc34b691a6be087a599fdf6add4ae20d473
Nitish Kumar <krnitish@live.in>
2023-10-01 22:24:15 +0530

    Updated Leetcode 557. Reverse Words in a String III

--- Python/Strings/L_reverseWordInStr3.py
+++ Python/Strings/L_reverseWordInStr3.py
@@ -5,4 +5,20 @@ def reverseWords(s: str) -> str:
             res.append(word[::-1])
         return " ".join(res) 
 
+def reverseWords(self, s: str) -> str:
+        s = s.strip().split()                   #Converting s into a list to get rid of spaces
+        out = []
+        for word in s:                          #Reversing each word of the list using two-pointers
+            i = 0
+            j = (len(word) - 1)
+            while (i < j):
+                word = list(word)
+                word[i], word[j] = word[j], word[i]
+                i += 1
+                j -= 1
+            a = (''.join(word))
+            out.append(a)
+        return(' '.join(out))
+
+
 print(reverseWords("Let's take LeetCode contest"))

8a6785f8c42c77ae730105b8959840e54283b10a
Nitish Kumar <krnitish@live.in>
2023-10-08 23:11:43 +0530

    Added Leetcode 1458. Max Dot Product of Two Subsequences

---
+++ Python/Dynamic Programming/L_maxDotProdOfTwoSubseq.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def maxDotProduct(A: List[int], B: List[int]) -> int:
+        n, m = len(A), len(B)
+        dp = [[0] * (m) for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                dp[i][j] = A[i] * B[j]
+                if i and j: dp[i][j] += max(dp[i - 1][j - 1], 0)
+                if i: dp[i][j] = max(dp[i][j], dp[i - 1][j])
+                if j: dp[i][j] = max(dp[i][j], dp[i][j - 1])
+        return dp[-1][-1]

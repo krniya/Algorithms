5b21fbddd61a06f988271b18262d7004faf3e657
Nitish Kumar <krnitish@live.in>
2023-10-13 23:02:39 +0530

    Updated Leetcode CPP 746. Min Cost Climbing Stairs

--- cpp/Dynamic Programming/L_minCostClimbingStair.cpp
+++ cpp/Dynamic Programming/L_minCostClimbingStair.cpp
@@ -1,10 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int minCostClimbingStairs(vector<int>& cost) {
+int minCostClimbingStairs(vector<int> &cost)
+{
     int downOne = 0;
     int downTwo = 0;
-    for (int i = 2; i <= cost.size(); i++) {
+    for (int i = 2; i <= cost.size(); i++)
+    {
         int temp = downOne;
         downOne = min(downOne + cost[i - 1], downTwo + cost[i - 2]);
         downTwo = temp;
@@ -12,6 +14,15 @@ int minCostClimbingStairs(vector<int>& cost) {
     return downOne;
 }
 
-int main() {
+int minCostClimbingStairs(vector<int> &cost)
+{
+    for (int i = 2; i < cost.size(); i++)
+    {
+        cost[i] += min(cost[i - 1], cost[i - 2]);
+    }
+    return min(cost[cost.size() - 1], cost[cost.size() - 2]);
+}
 
+int main()
+{
 }

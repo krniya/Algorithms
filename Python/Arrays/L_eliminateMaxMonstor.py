a69c3ab85018a77272104bb036af735e7aae0df8
Nitish Kumar <krnitish@live.in>
2023-11-07 23:24:58 +0530

    Added Leetcode 1921. Eliminate Maximum Number of Monsters

---
+++ Python/Arrays/L_eliminateMaxMonstor.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def eliminateMaximum(dist: List[int], speed: List[int]) -> int:
+        time_to_city = [dist[i] / speed[i] for i in range(len(dist))]
+        time_to_city.sort()
+        
+        for i in range(len(time_to_city)):
+
+            if time_to_city[i] <= i:
+                return i
+
+        return len(dist)

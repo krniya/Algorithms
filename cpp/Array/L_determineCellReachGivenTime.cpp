78b1443b1bb8aeb915d07f0d92a66476247f42c6
Nitish Kumar <krnitish@live.in>
2023-11-08 18:34:31 +0530

    Added Leetcode CPP 2849. Determine if a Cell Is Reachable at a Given Time

---
+++ cpp/Array/L_determineCellReachGivenTime.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isReachableAtTime(int sx, int sy, int fx, int fy, int t)
+{
+    if (sx == fx && sy == fy)
+        return t != 1;
+    return (max(abs(sx - fx), abs(sy - fy)) <= t);
+}

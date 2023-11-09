28dbae4981828706b0cd0bb68fe69604ea780f34
Nitish Kumar <krnitish@live.in>
2023-11-09 23:13:26 +0530

    Added Leetcode 1759. Count Number of Homogenous Substrings

---
+++ Python/Strings/L_CountNoHomoSubStr.py
@@ -0,0 +1,11 @@
+def countHomogenous(s: str) -> int:
+    left = 0
+    res = 0
+    for right in range(len(s)):
+        if s[left] == s[right]:
+            res += right - left + 1
+        else:
+            res += 1
+            left = right
+    
+    return res % (10**9 + 7)

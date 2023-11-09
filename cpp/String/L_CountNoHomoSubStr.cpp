652f3cc1f8518f8fc853f54894df561aa3555234
Nitish Kumar <krnitish@live.in>
2023-11-09 23:15:10 +0530

    Added Leetcode CPP 1759. Count Number of Homogenous Substrings

---
+++ cpp/String/L_CountNoHomoSubStr.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countHomogenous(string s)
+{
+    int left = 0;
+    long long res = 0;
+
+    for (int right = 0; right < s.length(); right++)
+    {
+        if (s[left] == s[right])
+        {
+            res += right - left + 1;
+        }
+        else
+        {
+            res += 1;
+            left = right;
+        }
+    }
+
+    return (int)(res % (1000000007));
+}

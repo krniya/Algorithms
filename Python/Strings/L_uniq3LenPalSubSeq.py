7b43de5ab996db8b6dcfdcaa5ca155fbe9448acc
Nitish Kumar <krnitish@live.in>
2023-11-14 23:02:07 +0530

    Added Leetcode 1930. Unique Length-3 Palindromic Subsequences

---
+++ Python/Strings/L_uniq3LenPalSubSeq.py
@@ -0,0 +1,12 @@
+def countPalindromicSubsequence(s: str) -> int:
+    res = 0
+    uniq = set(s)
+    
+    for c in uniq:
+        start = s.find(c) # search a character from the beginning
+        end = s.rfind(c) # search a character from the last index
+        
+        if start < end:
+            res += len(set(s[start+1:end]))
+    
+    return res

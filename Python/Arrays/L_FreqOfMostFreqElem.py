c451628e7f702c302b378170356af680961d8724
Nitish Kumar <krnitish@live.in>
2023-11-18 23:10:03 +0530

    Added Leetcode 1838. Frequency of the Most Frequent Element

---
+++ Python/Arrays/L_FreqOfMostFreqElem.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def maxFrequency(nums: List[int], k: int) -> int:
+    nums.sort()
+    left = right = res = total = 0
+
+    while right < len(nums):
+        total += nums[right]
+
+        while nums[right] * (right - left + 1) > total + k:
+            total -= nums[left]
+            left += 1
+        
+        res = max(res, right - left + 1)
+        right += 1
+    
+    return res

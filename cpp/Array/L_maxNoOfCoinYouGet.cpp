91ff5708945f8cef9a20871d917f4d001d439d70
Nitish Kumar <krnitish@live.in>
2023-11-24 22:03:09 +0530

    Added Leetcode CPP 1561. Maximum Number of Coins You Can Get

---
+++ cpp/Array/L_maxNoOfCoinYouGet.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxCoins(vector<int> &piles)
+{
+    sort(piles.begin(), piles.end());
+    int res = 0;
+
+    for (int i = piles.size() / 3; i < piles.size(); i += 2)
+    {
+        res += piles[i];
+    }
+
+    return res;
+}

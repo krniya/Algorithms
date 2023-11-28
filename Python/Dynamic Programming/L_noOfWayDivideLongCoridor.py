29258a7a36fab21cf6727f4225f77b0ad293d17f
Nitish Kumar <krnitish@live.in>
2023-11-28 22:59:27 +0530

    Added Leetcode 2147. Number of Ways to Divide a Long Corridor

---
+++ Python/Dynamic Programming/L_noOfWayDivideLongCoridor.py
@@ -0,0 +1,14 @@
+def numberOfWays(corridor):
+    seat, res, plant = 0, 1, 0
+    for i in corridor:
+        if i=='S':
+            seat += 1
+        else:
+            if seat == 2:
+                plant += 1
+        if seat == 3:
+            res = res*(plant+1) % (10**9 + 7)
+            seat , plant = 1 , 0
+    if seat != 2:
+        return 0
+    return res

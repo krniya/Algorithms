7bcdcc181271bc1099b593cffa47424b242113a6
Nitish Kumar <krnitish@live.in>
2023-12-05 23:33:36 +0530

    Added Leetcode CPP 2264. Largest 3-Same-Digit Number in String

---
+++ cpp/String/L_largest3digitNoinStr.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string largestGoodInteger(string num)
+{
+    int result = -1;
+    for (int i = 0; i + 2 < num.length(); i++)
+    {
+        if (num[i] == num[i + 1] && num[i] == num[i + 2])
+        {
+            result = std::max(result, num[i] - '0');
+        }
+    }
+    return (result == -1) ? "" : std::string(3, '0' + result);
+}

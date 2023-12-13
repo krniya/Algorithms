568bad0aa2a94a50f09cb92a69acd177ab26230f
Nitish Kumar <krnitish@live.in>
2023-12-13 21:21:07 +0530

    Added Leetcode 1582. Special Positions in a Binary Matrix

---
+++ Python/Arrays/L_specialPosInBinMat.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def numSpecial(mat: List[List[int]]) -> int:
+        def get_column_sum(col_idx):
+            return sum(row[col_idx] for row in mat)
+
+        special = 0
+        for row in mat:
+            if sum(row) == 1:
+                col_idx = row.index(1)
+                special += get_column_sum(col_idx) == 1
+
+        return special

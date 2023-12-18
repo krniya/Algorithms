de33a977ef6ea50e62f794dea00c5a2cadb5df06
Nitish Kumar <krnitish@live.in>
2023-12-18 21:31:36 +0530

    Added Leetcode CPP 1913. Maximum Product Difference Between Two Pairs

---
+++ cpp/Array/L_maxProdDiffTwoPair.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProductDifference(vector<int> &nums)
+{
+    int largest = 0, secondLargest = 0;
+    int smallest = INT_MAX, secondSmallest = INT_MAX;
+
+    for (int n : nums)
+    {
+        if (n < smallest)
+        {
+            secondSmallest = smallest;
+            smallest = n;
+        }
+        else if (n < secondSmallest)
+        {
+            secondSmallest = n;
+        }
+
+        if (n > largest)
+        {
+            secondLargest = largest;
+            largest = n;
+        }
+        else if (n > secondLargest)
+        {
+            secondLargest = n;
+        }
+    }
+
+    return (largest * secondLargest) - (smallest * secondSmallest);
+}

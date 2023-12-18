ef23f5c0cce190fcc1658d97791186aa6ed7fd7b
Nitish Kumar <krnitish@live.in>
2023-12-18 21:30:23 +0530

    Added leetcode 1913. Maximum Product Difference Between Two Pairs

---
+++ Python/Arrays/L_maxProdDiffTwoPair.py
@@ -0,0 +1,18 @@
+def maxProductDifference(nums):
+    largest, secondLargest = 0, 0
+    smallest, secondSmallest = float('inf'), float('inf')
+
+    for n in nums:
+        if n < smallest:
+            secondSmallest = smallest
+            smallest = n
+        elif n < secondSmallest:
+            secondSmallest = n
+
+        if n > largest:
+            secondLargest = largest
+            largest = n
+        elif n > secondLargest:
+            secondLargest = n
+
+    return (largest * secondLargest) - (smallest * secondSmallest)

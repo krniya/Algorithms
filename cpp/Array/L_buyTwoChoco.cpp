041d2b843130055a09f519152572ac7b7679ff9b
Nitish Kumar <krnitish@live.in>
2023-12-20 23:12:13 +0530

    Added Leetcode CPP 2706. Buy Two Chocolates

---
+++ cpp/Array/L_buyTwoChoco.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int buyChoco(vector<int> &prices, int money)
+{
+    int firstMinCost = INT_MAX;
+    int secondMinCost = INT_MAX;
+
+    for (int p : prices)
+    {
+        if (p < firstMinCost)
+        {
+            secondMinCost = firstMinCost;
+            firstMinCost = p;
+        }
+        else
+        {
+            secondMinCost = min(secondMinCost, p);
+        }
+    }
+
+    int leftover = money - (firstMinCost + secondMinCost);
+
+    return leftover >= 0 ? leftover : money;
+}

37c0992d091025cea2dd78f5a9ca210d9dc509d7
Nitish Kumar <krnitish@live.in>
2023-12-28 23:36:38 +0530

    Updated Leetcode 1578. Minimum Time to Make Rope Colorful

--- Python/Greedy Algorithms/L_minTimeToRopeColorful.py
+++ Python/Greedy Algorithms/L_minTimeToRopeColorful.py
@@ -6,3 +6,23 @@ def minCost(s: str, cost) -> int:
             res += min(max_cost, cost[i])
             max_cost = max(max_cost, cost[i])
         return res
+    
+    
+def minCost(colors: str, neededTime) -> int:
+        totalTime = 0
+        i = 0
+        j = 0
+
+        while i < len(neededTime) and j < len(neededTime):
+            currTotal = 0
+            currMax = 0
+
+            while j < len(neededTime) and colors[i] == colors[j]:
+                currTotal += neededTime[j]
+                currMax = max(currMax, neededTime[j])
+                j += 1
+
+            totalTime += currTotal - currMax
+            i = j
+
+        return totalTime

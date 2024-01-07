b500d03127ef5ee0121cd9ace47d4e64d2142077
Nitish Kumar <krnitish@live.in>
2024-01-07 23:10:02 +0530

    Updated Leetcode 446. Arithmetic Slices II - Subsequence

--- Python/Dynamic Programming/L_ArithmeticSlicesII.py
+++ Python/Dynamic Programming/L_ArithmeticSlicesII.py
@@ -14,3 +14,32 @@ def numberOfArithmeticSlices(nums) -> int:
                 ans += cnt
                 
         return ans
+    
+    
+    
+    
+    
+def numberOfArithmeticSlices1(nums):
+    n = len(nums)
+    dp = [[0] * n for _ in range(n)]
+    map = {}
+
+    for i in range(n):
+        temp = nums[i]
+        if temp not in map:
+            map[temp] = []
+        map[temp].append(i)
+
+    total_sum = 0
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            a = 2 * nums[i] - nums[j]
+            if a in map:
+                for k in map[a]:
+                    if k < i:
+                        dp[i][j] += dp[k][i] + 1
+                    else:
+                        break
+            total_sum += dp[i][j]
+
+    return total_sum

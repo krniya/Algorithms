d02c4cc41808913c67a21fcddf7f7685aa42c5eb
Nitish Kumar <krnitish@live.in>
2024-01-08 22:23:41 +0530

    Updated Leetcode 938. Range Sum of BST

--- Python/Binary Search Tree/L_rangeSumBST.py
+++ Python/Binary Search Tree/L_rangeSumBST.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def rangeSumBST(root, low: int, high: int) -> int:
         res = 0
         def preOrd(root):
@@ -10,3 +13,18 @@ def rangeSumBST(root, low: int, high: int) -> int:
             preOrd(root.right)
         preOrd(root)
         return res
+    
+    
+def rangeSumBST1(root, lo: int, hi: int) -> int:
+    res = 0
+    q = deque([root])
+    while q:
+        c = q.popleft()
+        v, l, r = c.val, c.left, c.right
+        if lo <= v and v <= hi:
+            res += v
+        if l and (lo < v or v > hi):
+            q.append(l)
+        if r and (lo > v or v < hi):
+            q.append(r)
+    return res

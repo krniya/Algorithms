2db56e96a1ba117bdc455a309a83c8819a9fd956
Nitish Kumar <krnitish@live.in>
2024-01-11 23:23:12 +0530

    Updated Leetcode 1026. Maximum Difference Between Node and Ancestor

--- Python/Binary Trees/L_maxAncestorDiff.py
+++ Python/Binary Trees/L_maxAncestorDiff.py
@@ -9,3 +9,20 @@ def maxAncestorDiff(root) -> int:
             return curMin, curMax
         dfs(root)
         return ans
+
+
+def maxAncestorDiff(root) -> int:
+    if not root:
+        return 0
+    diff = 0
+    helper(root, root.val, root.val, diff)
+    return diff
+
+def helper(root, min_val, max_val, diff):
+    if not root:
+        return
+    diff = max(diff, max(abs(min_val - root.val), abs(max_val - root.val)))
+    min_val = min(min_val, root.val)
+    max_val = max(max_val, root.val)
+    helper(root.left, min_val, max_val, diff)
+    helper(root.right, min_val, max_val, diff)

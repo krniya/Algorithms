3b8e7b9d572d1472bc1360752a8ef2a51909f250
Nitish Kumar <krnitish@live.in>
2024-01-12 23:48:16 +0530

    Updated Leetcode CPP 1704. Determine if String Halves Are Alike

--- cpp/String/L_detStrHalfAlike.cpp
+++ cpp/String/L_detStrHalfAlike.cpp
@@ -1,12 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool halvesAreAlike(string S) {
+bool halvesAreAlike(string S)
+{
     string vowels = "aeiouAEIOU";
     int mid = S.size() / 2, ans = 0;
-    for (int i = 0, j = mid; i < mid; i++, j++) {
-        if (~vowels.find(S[i])) ans++;
-        if (~vowels.find(S[j])) ans--;
+    for (int i = 0, j = mid; i < mid; i++, j++)
+    {
+        if (~vowels.find(S[i]))
+            ans++;
+        if (~vowels.find(S[j]))
+            ans--;
     }
     return ans == 0;
+}
+
+bool halvesAreAlike(string s)
+{
+    set<char> vow = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    int n = s.size() / 2, f = 0;
+    for (int i = 0; i < n; i++)
+    {
+        f += (vow.count(s[i]) - vow.count(s[n + i]));
+    }
+    return f == 0;
 }

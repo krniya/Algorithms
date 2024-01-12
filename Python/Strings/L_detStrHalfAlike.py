1f4bc2784157f678b5a2bc50c300ee1af7cc11ab
Nitish Kumar <krnitish@live.in>
2024-01-12 18:56:43 +0530

    Updated Leetcode 1704. Determine if String Halves Are Alike

--- Python/Strings/L_detStrHalfAlike.py
+++ Python/Strings/L_detStrHalfAlike.py
@@ -9,3 +9,16 @@ def halvesAreAlike(s: str) -> bool:
                 else:
                     count -= 1
         return count == 0
+    
+def halvesAreAlike1(s: str) -> bool:
+    def vowel_count(half: str) -> int:
+        vowels = "aeiouAEIOU"
+        count = 0
+        for ch in half:
+            if ch in vowels:
+                count +=1
+        return count
+    mid_point = len(s) // 2
+    return vowel_count(s[:mid_point]) == vowel_count(s[mid_point:])
+
+print((halvesAreAlike1("book")))

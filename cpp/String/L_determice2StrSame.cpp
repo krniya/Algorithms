5df2905c339767ec5f79df8ab0f5bcbb29dd7db1
Nitish Kumar <krnitish@live.in>
2024-01-14 23:14:11 +0530

    Updated Leetcode CPP 1657. Determine if Two Strings Are Close

--- cpp/String/L_determice2StrSame.cpp
+++ cpp/String/L_determice2StrSame.cpp
@@ -37,3 +37,22 @@ bool closeStrings(std::string word1, std::string word2)
 
     return true;
 }
+
+bool closeStrings1(string word1, string word2)
+{
+    unordered_map<char, int> mp1, mp2, mp3, mp4;
+    // existence checking
+    for (auto i : word1)
+        mp1[i]++, mp3[i] = 1;
+    for (auto i : word2)
+        mp2[i]++, mp4[i] = 1;
+    if (mp4 != mp3)
+        return false;
+    // checking if we can swap
+    multiset<int> s1, s2;
+    for (auto [c, f] : mp1)
+        s1.insert(f);
+    for (auto [c, f] : mp2)
+        s2.insert(f);
+    return s1 == s2;
+}

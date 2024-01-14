51254d4922defba6adc95d3379c27799bc95cd09
Nitish Kumar <krnitish@live.in>
2024-01-14 23:10:54 +0530

    Updated Leetcode 1657. Determine if Two Strings Are Close

--- Python/Strings/L_determice2StrSame.py
+++ Python/Strings/L_determice2StrSame.py
@@ -13,3 +13,27 @@ def closeStrings(word1: str, word2: str) -> bool:
         res1.sort()
         res2.sort()
         return res1 == res2 and set(word1) == set(word2)
+    
+    
+def closeStrings(word1: str, word2: str) -> bool:
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+
+        for ch in word1:
+            freq1[ord(ch) - ord('a')] += 1
+
+        for ch in word2:
+            freq2[ord(ch) - ord('a')] += 1
+
+        for i in range(26):
+            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
+                return False
+
+        freq1.sort()
+        freq2.sort()
+
+        for i in range(26):
+            if freq1[i] != freq2[i]:
+                return False
+
+        return True

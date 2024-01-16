ba801aaa2a3eb0b56eb1ca21197d993e22930dca
Nitish Kumar <krnitish@live.in>
2024-01-16 23:32:09 +0530

    Updated Leetcode 380. Insert Delete GetRandom O(1)

--- Python/Arrays/L_insertDeleteRandom.py
+++ Python/Arrays/L_insertDeleteRandom.py
@@ -1,4 +1,7 @@
 import random
+from operator import setitem
+
+
 class RandomizedSet:
     
     def __init__(self):
@@ -27,4 +30,21 @@ class RandomizedSet:
     
     def getRandom(self) -> int:
         return random.choice(self.data)
+    
+    
+class RandomizedSet:
+    def __init__(self):
+        self.a,self.d=[],{}
+
+
+    def insert(self, v: int) -> bool:
+        return (v in self.d)^1 and not (setitem(self.d,v,len(self.a)) or self.a.append(v))
+        
+
+    def remove(self, v: int) -> bool:
+        return v in self.d and not (lambda e,i:i<len(self.a) and (setitem(self.a,i,e) or setitem(self.d,e,i)))(self.a.pop(),self.d.pop(v))
+        
+
+    def getRandom(self) -> int:
+        return choice(self.a)
 

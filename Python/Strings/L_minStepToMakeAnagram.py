89ecc2d18c88340132edae18349195fceb596101
Nitish Kumar <krnitish@live.in>
2024-01-16 23:39:52 +0530

    Updated Leetcode 1347. Minimum Number of Steps to Make Two Strings Anagram

--- Python/Strings/L_minStepToMakeAnagram.py
+++ Python/Strings/L_minStepToMakeAnagram.py
@@ -13,3 +13,26 @@ def minSteps(s: str, t: str) -> int:
         steps += abs(count_s[i] - count_t[i])
 
     return steps // 2
+
+
+def minSteps(s: str, t: str) -> int:
+    smp = {}
+    tmp = {}
+    cnt = 0
+
+    for a in s:
+        smp[a] = smp.get(a, 0) + 1
+
+    for a in t:
+        tmp[a] = tmp.get(a, 0) + 1
+
+    for key, value in smp.items():
+        if key in tmp:
+            if value == tmp[key]:
+                cnt += value
+            else:
+                cnt += min(value, tmp[key])
+
+    return len(s) - cnt
+
+

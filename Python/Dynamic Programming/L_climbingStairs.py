5c50b95178836dee494b202d698d5915c6e749dd
Nitish Kumar <krnitish@live.in>
2024-01-18 22:43:11 +0530

    Updated Leetcode 70. Climbing Stairs

--- Python/Dynamic Programming/L_climbingStairs.py
+++ Python/Dynamic Programming/L_climbingStairs.py
@@ -8,3 +8,12 @@ def climbStairs(n: int) -> int:
         cache[n] = backtrack(n-1) + backtrack(n-2)
         return cache[n]
     return backtrack(n)
+
+
+def climbStairs1(n: int) -> int:
+    arr=[1,1]
+    if n <2:
+        return arr[n]
+    for i in range(2,n+1):
+        arr.append(arr[i-1]+arr[i-2])
+    return arr[n]

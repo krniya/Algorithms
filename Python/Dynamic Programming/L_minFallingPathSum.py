67b253960eb2159a3b662cad76af9100f828a073
Nitish Kumar <krnitish@live.in>
2024-01-19 23:40:53 +0530

    Updated Leetcode 931. Minimum Falling Path Sum

--- Python/Dynamic Programming/L_minFallingPathSum.py
+++ Python/Dynamic Programming/L_minFallingPathSum.py
@@ -1,4 +1,6 @@
 from functools import cache
+from typing import List
+
 
 def minFallingPathSum(matrix) -> int:
         row, col = len(matrix), len(matrix[0])
@@ -14,3 +16,28 @@ def minFallingPathSum(matrix) -> int:
         for i in range(col):
             res = min(res, dfs(0, i))
         return res
+
+def minFallingPathSum(matrix: List[List[int]]) -> int:
+    n = len(matrix)
+    m = len(matrix[0])
+    dp = [[0] * m for _ in range(n)]
+
+    for j in range(m):
+        dp[0][j] = matrix[0][j]
+
+    for i in range(1, n):
+        for j in range(m):
+            ld = rd = float('inf')
+            up = matrix[i][j] + dp[i - 1][j]
+
+            if j - 1 >= 0:
+                ld = matrix[i][j] + dp[i - 1][j - 1]
+            if j + 1 < m:
+                rd = matrix[i][j] + dp[i - 1][j + 1]
+
+            dp[i][j] = min(up, min(ld, rd))
+
+    mini = dp[n - 1][0]
+    for j in range(1, m):
+        mini = min(mini, dp[n - 1][j])
+    return mini

f82814f08cc96696aa8c29618c493d06b5c6cd05
Nitish Kumar <krnitish@live.in>
2024-01-20 22:50:10 +0530

    Added Leetcode CPP 907. Sum of Subarray Minimums

--- cpp/Dynamic Programming/L_sumOfSubarrMin.cpp
+++ cpp/Dynamic Programming/L_sumOfSubarrMin.cpp
@@ -1,18 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
-        vector<vector<int>> jobs;
-        for (int i = 0; i < n; ++i) {
-            jobs.push_back({endTime[i], startTime[i], profit[i]});
+using ll = long long;
+const int MOD = 1e9 + 7;
+
+int sumSubarrayMins(vector<int> &nums)
+{
+    int length = nums.size();
+    vector<int> left(length, -1);
+    vector<int> right(length, length);
+    stack<int> stk;
+
+    for (int i = 0; i < length; ++i)
+    {
+        while (!stk.empty() && nums[stk.top()] >= nums[i])
+        {
+            stk.pop();
         }
-        sort(jobs.begin(), jobs.end());
-        map<int, int> dp = {{0, 0}};
-        for (auto& job : jobs) {
-            int cur = prev(dp.upper_bound(job[1]))->second + job[2];
-            if (cur > dp.rbegin()->second)
-                dp[job[0]] = cur;
+        if (!stk.empty())
+        {
+            left[i] = stk.top();
         }
-        return dp.rbegin()->second;
+        stk.push(i);
     }
+
+    stk = stack<int>();
+
+    for (int i = length - 1; i >= 0; --i)
+    {
+        while (!stk.empty() && nums[stk.top()] > nums[i])
+        {
+            stk.pop();
+        }
+        if (!stk.empty())
+        {
+            right[i] = stk.top();
+        }
+        stk.push(i);
+    }
+
+    ll sum = 0;
+
+    for (int i = 0; i < length; ++i)
+    {
+        sum += static_cast<ll>(i - left[i]) * (right[i] - i) * nums[i] % MOD;
+        sum %= MOD;
+    }
+
+    return sum;
+}

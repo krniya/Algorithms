9e087e46f7be1d09a862d5a89c6feaa311963426
Nitish Kumar <krnitish@live.in>
2024-01-21 23:36:32 +0530

    Updated Leetcode 198. House Robber

--- Python/Dynamic Programming/L_houseRobber.py
+++ Python/Dynamic Programming/L_houseRobber.py
@@ -1,4 +1,6 @@
+
 from functools import cache
+from typing import List
 
 
 def rob(nums) -> int:
@@ -10,7 +12,7 @@ def rob(nums) -> int:
             return nums[i] + max(robs(i+2), robs(i+3))
         return max(robs(0), robs(1))
 
-def rob(nums) -> int:
+def rob1(nums) -> int:
         if len(nums) == 1:
             return nums[0]
         first = nums[0]
@@ -20,3 +22,9 @@ def rob(nums) -> int:
             first = second
             second = curr
         return second
+    
+def rob2(nums: List[int]) -> int:
+    prev1, prev2 = 0, 0
+    for i in nums:
+        prev1, prev2 = prev2, max(prev1+i, prev2)
+    return prev2

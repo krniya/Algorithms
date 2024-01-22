ce1944266e1f86ea9a84d6997af4609865e61b61
Nitish Kumar <krnitish@live.in>
2024-01-22 23:45:00 +0530

    Updated Leetcode CPP 645. Set Mismatch

--- cpp/Array/L_setMismatch.cpp
+++ cpp/Array/L_setMismatch.cpp
@@ -22,3 +22,27 @@ vector<int> findErrorNums(vector<int> &nums)
 
     return {duplicate, missing};
 }
+
+vector<int> findErrorNums1(vector<int> &nums)
+{
+    int n = nums.size();
+    int actual_sum = n * (n + 1) / 2;
+    int array_sum = 0;
+    int unique_sum = 0;
+    unordered_set<int> s(nums.begin(), nums.end());
+
+    for (int a : nums)
+    {
+        array_sum += a;
+    }
+
+    for (int a : s)
+    {
+        unique_sum += a;
+    }
+
+    int missing = actual_sum - unique_sum;
+    int duplicate = array_sum - unique_sum;
+
+    return {duplicate, missing};
+}

0bc89b3674d23ffe9ec950190724eb5937447694
Nitish Kumar <krnitish@live.in>
2024-01-29 23:28:01 +0530

    Updated Leetcode CPP 232. Implement Queue using Stacks

--- cpp/Stack&Queue/L_implementStackUsingQueue.cpp
+++ cpp/Stack&Queue/L_implementStackUsingQueue.cpp
@@ -36,3 +36,44 @@ public:
         return q.empty();
     }
 };
+
+class MyQueue
+{
+public:
+    stack<int> st1, st2;
+    MyQueue()
+    {
+    }
+    void push(int x)
+    {
+
+        while (!st1.empty())
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        st1.push(x);
+        while (!st2.empty())
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
+    }
+
+    int pop()
+    {
+        int x = st1.top();
+        st1.pop();
+        return x;
+    }
+
+    int peek()
+    {
+        return st1.top();
+    }
+
+    bool empty()
+    {
+        return st1.empty();
+    }
+};

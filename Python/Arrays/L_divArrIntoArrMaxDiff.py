465a1cb7d02b88c70b8243b964c74ee31bcce2a5
Nitish Kumar <krnitish@live.in>
2024-02-01 23:31:43 +0530

    Updated Leetcode 2966. Divide Array Into Arrays With Max Difference

--- Python/Arrays/L_divArrIntoArrMaxDiff.py
+++ Python/Arrays/L_divArrIntoArrMaxDiff.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def divideArray(nums, k):
     size = len(nums)
     if size % 3 != 0:
@@ -14,3 +17,21 @@ def divideArray(nums, k):
         else:
             return []
     return result
+
+
+def divideArray1(nums: List[int], k: int) -> List[List[int]]:
+        # Sort the input array
+        nums.sort()
+        res = []
+
+        # Iterate over the sorted array in steps of 3
+        for i in range(0, len(nums), 3):
+            # Check if there are at least 3 elements remaining
+            if i + 2 < len(nums):
+                # If the difference between the maximum and minimum elements in the subarray is greater than k, return an empty list
+                if nums[i + 2] - nums[i] > k:
+                    return []
+                # Append the current subarray to the result
+                res.append([nums[i], nums[i + 1], nums[i + 2]])
+
+        return res

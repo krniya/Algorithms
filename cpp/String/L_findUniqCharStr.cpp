59c6fc63b8a799e8cf80b6b140cc84ab6f40d0c9
Nitish Kumar <krnitish@live.in>
2024-02-05 22:32:15 +0530

    Added Leetcode CPP 387. First Unique Character in a String

---
+++ cpp/String/L_findUniqCharStr.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstUniqChar(string s)
+{
+    unordered_map<char, int> mp;
+    for (auto a : s)
+        mp[a]++;
+    for (int i = 0; i < s.size(); i++)
+        if (mp[s[i]] == 1)
+            return i;
+    return -1;
+}

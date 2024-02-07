54ad598f06f4792d656ee9336ae236090fd52481
Nitish Kumar <krnitish@live.in>
2024-02-07 23:37:20 +0530

    Updated Leetcode 451. Sort Characters By Frequency

--- Python/Strings/L_sortCharByFreq.py
+++ Python/Strings/L_sortCharByFreq.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def frequencySort(s: str) -> str:
         counter = {}
         for ch in s:
@@ -6,3 +9,14 @@ def frequencySort(s: str) -> str:
         for k in sorted(counter, key=counter.get, reverse=True):
             res += k * counter[k]
         return res
+    
+    
+def frequencySort(s: str) -> str:
+        counter = Counter(s)
+        pq = [(-freq, char) for char, freq in counter.items()]
+        heapq.heapify(pq)
+        result = ''
+        while pq:
+            freq, char = heapq.heappop(pq)
+            result += char * -freq
+        return result

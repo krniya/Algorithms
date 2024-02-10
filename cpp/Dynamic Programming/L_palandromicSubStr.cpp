88d37f84e7190fd81a7e462f11e274c642774b6b
Nitish Kumar <krnitish@live.in>
2024-02-10 23:50:31 +0530

    Updated Leetcode CPP 647. Palindromic Substrings

--- cpp/Dynamic Programming/L_palandromicSubStr.cpp
+++ cpp/Dynamic Programming/L_palandromicSubStr.cpp
@@ -1,20 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int countSubstrings(string s) {
-        int res = 0;
-        for(int i=0; i<s.size();i++) {
-            res += countPal(s, i, i);
-            res += countPal(s, i, i+1);
-        }
-        return res;
+int countSubstrings(string s)
+{
+    int res = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        res += countPal(s, i, i);
+        res += countPal(s, i, i + 1);
     }
-    int countPal(string s,int l,int r) {
-        int res = 0;
-        while(l >= 0 and r < s.size() and s[l] == s[r]) {
-            res++;
-            l--;
-            r++;
-        }
-        return res;
+    return res;
+}
+int countPal(string s, int l, int r)
+{
+    int res = 0;
+    while (l >= 0 and r < s.size() and s[l] == s[r])
+    {
+        res++;
+        l--;
+        r++;
     }
+    return res;
+}
+
+int countSubstrings(string s)
+{
+    int n = s.length(), ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int even = palindromeCount(s, i, i + 1);
+        int odd = palindromeCount(s, i, i);
+        ans += even + odd;
+    }
+    return ans;
+}
+
+int palindromeCount(const string &s, int left, int right)
+{
+    int count = 0;
+    while (left >= 0 && right < s.length() && s[left] == s[right])
+    {
+        --left;
+        ++right;
+        ++count;
+    }
+    return count;
+}

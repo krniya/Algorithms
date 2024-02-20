d8e3f9fe94cce4642c4f36364d56d9b2427ea462
Nitish Kumar <krnitish@live.in>
2024-02-20 22:49:26 +0530

    Updated Leetcode 268. Missing Number

--- Python/Bit Manipulation/L_MissingNumber.py
+++ Python/Bit Manipulation/L_MissingNumber.py
@@ -1,7 +1,23 @@
+from typing import List
+
+
 def missingNumber(nums) -> int:
         a = len(nums)
         for i,n in enumerate(nums):
             a ^= i ^ n
         return a
 
-print(missingNumber())
+
+def missingNumber(nums: List[int]) -> int:
+        n = len(nums)
+    v = [-1] * (n + 1)
+    for num in nums:
+        v[num] = num
+    for i in range(len(v)):
+        if v[i] == -1:
+            return i
+    return 0
+
+
+
+print(missingNumber([6,65,54]))

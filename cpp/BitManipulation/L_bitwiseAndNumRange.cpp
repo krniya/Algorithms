439ccd4d4e7fc7bac68b64ac24489230d693c878
Nitish Kumar <krnitish@live.in>
2024-02-21 22:48:21 +0530

    Updated Leetcode CPP 201. Bitwise AND of Numbers Range

--- cpp/BitManipulation/L_bitwiseAndNumRange.cpp
+++ cpp/BitManipulation/L_bitwiseAndNumRange.cpp
@@ -12,3 +12,14 @@ int rangeBitwiseAnd(int left, int right)
     }
     return (left << cnt);
 }
+
+int rangeBitwiseAnd1(int left, int right)
+{
+    while (right > left)
+    {
+        // The below statement is used to calculate the total number of bits in right and the NUMBER is updated accordingly.``
+        // Since we do not need to calculate till it reaches value 0, and right needs to be greater than left, while loop check condition is put that way
+        right = right & (right - 1);
+    }
+    return right;
+}

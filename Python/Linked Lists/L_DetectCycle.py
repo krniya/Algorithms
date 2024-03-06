549c97a804572d344bcdd62e02b57e3b1ee4dcee
Nitish Kumar <krnitish@live.in>
2024-03-06 21:19:58 +0530

    Updated Leetcode 141. Linked List Cycle

--- Python/Linked Lists/L_DetectCycle.py
+++ Python/Linked Lists/L_DetectCycle.py
@@ -9,3 +9,14 @@ def hasCycle(head) -> bool:
         cur = cur.next
 
     return False
+
+def hasCycle1(head) -> bool:
+    if not head: return False
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False

18d1b4dcebecab06b8f0aa23b3d4cd877fd0928a
Nitish Kumar <krnitish@live.in>
2024-03-07 21:55:44 +0530

    Updated Leetcode CPP 876. Middle of the Linked List

--- cpp/Linked List/L_middleOfLL.cpp
+++ cpp/Linked List/L_middleOfLL.cpp
@@ -1,20 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(NULL) {}
 };
- 
 
-ListNode* middleNode(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast and fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
+ListNode *middleNode(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast and fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
     }
+    return slow;
+}
+
+int listLength(ListNode *head)
+{
+    int len = 0;
+    while (head != NULL)
+    {
+        len++;
+        head = head->next;
+    }
+    return len;
+}
+
+ListNode *middleNode(ListNode *head)
+{
+    int len = listLength(head);
+    int middle = len / 2;
+    ListNode *temp = head;
+    int count = 0;
+    while (count < middle)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return temp;
+}

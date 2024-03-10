df57d53903895269d7a4725db332c0687d844f75
Nitish Kumar <krnitish@live.in>
2024-03-10 22:51:34 +0530

    Added Leetcode 349. Intersection of Two Arrays

---
+++ Python/Arrays/L_intersectionTwoArr.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+    num1 = set(nums1)
+    num2 = set(nums2)
+    return list(num1 & num2)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

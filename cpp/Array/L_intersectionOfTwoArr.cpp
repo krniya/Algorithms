1b7eb2ecd499a39ee2fb7e59d48d2e5ffaae58d4
Nitish Kumar <krnitish@live.in>
2024-03-12 22:24:33 +0530

    Added Leetcode CPP 349. Intersection of Two Arrays

---
+++ cpp/Array/L_intersectionOfTwoArr.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+{
+    int m = nums2.size();
+    vector<int> result;
+    unordered_map<int, int> mp;
+    for (auto &num : nums1)
+    {
+        mp[num]++;
+    }
+    for (auto &num : nums2)
+    {
+        if (mp.find(num) != mp.end())
+        {
+            result.push_back(num);
+            mp.erase(num);
+        }
+    }
+    return result;
+}

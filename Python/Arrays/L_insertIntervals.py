3ddbae939248a27f1ec1bffe643056304f864935
Nitish Kumar <krnitish@live.in>
2024-03-17 21:56:22 +0530

    Updated Leetcode 57. Insert Interval

--- Python/Arrays/L_insertIntervals.py
+++ Python/Arrays/L_insertIntervals.py
@@ -11,4 +11,24 @@ class Solution:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval)
         return res
+
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        merged = []
+        i = 0
+
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
         
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+            i += 1
+        merged.append(newInterval)
+        
+        while i < len(intervals):
+            merged.append(intervals[i])
+            i += 1
+        
+        return merged

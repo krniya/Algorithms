c3d275adae7f411ed7dbc44937b3d3d44a3d34e8
Nitish Kumar <krnitish@live.in>
2024-03-18 22:35:26 +0530

    Updated Leetcode 452. Minimum Number of Arrows to Burst Balloons

--- Python/Greedy Algorithms/L_minNoOfArrowToBrstBaloon.py
+++ Python/Greedy Algorithms/L_minNoOfArrowToBrstBaloon.py
@@ -7,3 +7,19 @@ def findMinArrowShots( points: List[List[int]]) -> int:
             if ans == 0 or start > arrow:
                 ans, arrow = ans + 1, end
         return ans
+
+
+class Solution(object):
+    def findMinArrowShots(self, points):
+        points.sort(key=lambda x: x[0])
+        arrows = 1
+        end = points[0][1]
+        
+        for balloon in points[1:]:
+            if balloon[0] > end: 
+                arrows += 1  
+                end = balloon[1] 
+            else:
+                end = min(end, balloon[1])
+        
+        return arrows

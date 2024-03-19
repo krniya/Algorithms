3e3f18031c5b03ee833d86ff1e17e39e28086766
Nitish Kumar <krnitish@live.in>
2024-03-19 22:06:27 +0530

    Updated Leetcode 621. Task Scheduler

--- Python/Heaps/L_taskScheduler.py
+++ Python/Heaps/L_taskScheduler.py
@@ -19,3 +19,19 @@ def leastInterval(tasks, n: int) -> int:
             if q and q[0][1] == time:
                 heapq.heappush(maxHeap, q.popleft()[0])
         return time
+
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
+        freq.sort()
+        chunk = freq[25] - 1
+        idle = chunk * n
+
+        for i in range(24, -1, -1):
+            idle -= min(chunk, freq[i])
+
+        return len(tasks) + idle if idle >= 0 else len(tasks)

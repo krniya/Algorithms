7a7fff4bd40c1f2040809dcc5248f7411c509a74
Nitish Kumar <krnitish@live.in>
2024-03-22 19:10:30 +0530

    Updated Leetcode CPP 143. Reorder List

--- cpp/Linked List/L_reorderList.cpp
+++ cpp/Linked List/L_reorderList.cpp
@@ -1,32 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-struct ListNode {
+struct ListNode
+{
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
- 
 
-
-void reorderList(ListNode* head) {
-    ListNode* slow = head;
-    ListNode* fast = head->next;
-    while(fast && fast->next) {
+void reorderList(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while (fast && fast->next)
+    {
         slow = slow->next;
         fast = fast->next->next;
     }
-    ListNode* second = slow->next;
+    ListNode *second = slow->next;
     slow->next = NULL;
-    ListNode* prev = NULL;
-    while(second) {
-        ListNode* curr = second;
+    ListNode *prev = NULL;
+    while (second)
+    {
+        ListNode *curr = second;
         second = second->next;
         curr->next = prev;
         prev = curr;
     }
-    ListNode *first = head; 
+    ListNode *first = head;
     second = prev;
-    while(second) {
+    while (second)
+    {
         ListNode *tem1 = first->next, *tem2 = second->next;
         first->next = second;
         second->next = tem1;
@@ -35,7 +38,41 @@ void reorderList(ListNode* head) {
     }
 }
 
+void reorderList(ListNode *head)
+{
+    stack<ListNode *> s;
+    ListNode *curr = head;
+    while (curr)
+    {
+        s.push(curr);
+        curr = curr->next;
+    }
+    curr = head;
+    unordered_map<ListNode *, bool> vis;
+    while (true)
+    {
+        ListNode *last = s.top();
+        s.pop();
+        ListNode *next = curr->next;
+        vis[curr] = true;
+        if (vis[last])
+        {
+            curr->next = NULL;
+            break;
+        }
+        curr->next = last;
+        vis[last] = true;
+        curr = curr->next;
+        if (vis[next])
+        {
+            curr->next = NULL;
+            break;
+        }
+        curr->next = next;
+        curr = curr->next;
+    }
+}
 
-int main() {
-
+int main()
+{
 }

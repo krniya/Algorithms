a060c61abbb20edbb3e3c96eea6fccdc18ec9dc2
Nitish Kumar <krnitish@live.in>
2024-03-23 19:15:15 +0530

    Updated Leetcode 143. Reorder List

--- Python/Linked Lists/L_reorderLinkedList.py
+++ Python/Linked Lists/L_reorderLinkedList.py
@@ -17,3 +17,38 @@ def reorderLinkedList(head):
         first.next = second
         second.next = temp1
         first, second = temp1, temp2
+
+def reverse(self, head):
+        if not head:
+            return None
+        prev = None
+        curr = head
+        nextNode = None
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        return prev
+
+def merge(self, list1, list2):
+    while list2:
+        nextNode = list1.next
+        list1.next = list2
+        list1 = list2
+        list2 = nextNode
+
+def reorderList(self, head):
+    if not head or not head.next:
+        return
+    slow = head
+    fast = head
+    prev = head
+    while fast and fast.next:
+        prev = slow
+        fast = fast.next.next
+        slow = slow.next
+    prev.next = None
+    list1 = head
+    list2 = self.reverse(slow)
+    self.merge(list1, list2)

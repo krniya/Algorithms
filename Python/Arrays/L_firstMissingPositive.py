c13bc9a4bc604807a9df5513d125afbdb7b2df02
Nitish Kumar <krnitish@live.in>
2024-03-26 21:29:58 +0530

    Updated Leetcode 41. First Missing Positive

--- Python/Arrays/L_firstMissingPositive.py
+++ Python/Arrays/L_firstMissingPositive.py
@@ -15,4 +15,22 @@ def firstMissingPositive(nums: List[int]) -> int:
     return n
 
 
+def firstMissingPositive1(nums: List[int]) -> int:
+    n = len(nums)
+    for i in range(n):
+        if nums[i] < 0:
+            nums[i] = 0
+    for i in range(n):
+        val = abs(nums[i])
+        if 1 <= val <= n:
+            if nums[val-1] > 0:
+                nums[val-1] *= -1
+            elif nums[val-1] == 0:
+                nums[val-1] *= -1 * (n + 1)
+    for i in range(1, n + 1):
+        if nums[i-1] > 0:
+            return i
+    return n + 1
+        
+
 print(firstMissingPositive([3,4,-1,1]))

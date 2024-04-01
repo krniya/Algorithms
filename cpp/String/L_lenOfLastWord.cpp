24593659d25b9cd13e57445a8e7799aea9d4a335
Nitish Kumar <krnitish@live.in>
2024-04-01 22:14:32 +0530

    Added Leetcode CPP 58. Length of Last Word

---
+++ cpp/String/L_lenOfLastWord.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLastWord(string s)
+{
+    int length = 0;
+    bool counting = false;
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (s[i] != ' ')
+        {
+            counting = true;
+            length++;
+        }
+        else if (counting)
+        {
+            break;
+        }
+    }
+    return length;
+}

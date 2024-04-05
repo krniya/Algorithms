3fb682e816e374f2bef337f79d1840f275efd937
Nitish Kumar <krnitish@live.in>
2024-04-05 21:50:18 +0530

    Updated Leetcode 1544. Make The String Great

--- Python/Stacks & Queue/L_makeTheStrGreat.py
+++ Python/Stacks & Queue/L_makeTheStrGreat.py
@@ -10,3 +10,13 @@ def makeGood(s: str) -> str:
             else:
                 result.append(c)
         return ''.join(result)
+    
+def makeGood1(s: str) -> str:
+    ss = []
+    for c in s: 
+        if ss and ss[-1] == c.swapcase():    # if the stack is not empty and the last letter on the stack is
+            ss.pop()                         # a match for the current letter (e.g., 'a' and 'A'), remove both
+        else: 
+            ss.append(c)                     # continue adding to stack to compare with next letter
+    
+    return "".join(ss)

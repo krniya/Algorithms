b9368b7035be9a9099b7e42e06f666979c9707c2
Nitish Kumar <krnitish@live.in>
2024-04-08 22:52:29 +0530

    Added Leetcode 1700. Number of Students Unable to Eat Lunch

---
+++ Python/Arrays/L_numberOfStdUnableToEatLunch.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def countStudents(students: List[int], sandwiches: List[int]) -> int:
+    counts = [0, 0]
+    for student in students:
+        counts[student] += 1
+
+    remaining = len(sandwiches)
+    for sandwich in sandwiches:
+        if counts[sandwich] == 0:
+            break
+        if remaining == 0:
+            break
+        counts[sandwich] -= 1
+        remaining -= 1
+    
+    return remaining

502bfb2fd9cf945138777690517fd84338409975
Nitish Kumar <krnitish@live.in>
2024-04-09 23:03:35 +0530

    Added Leetcode 2073. Time Needed to Buy Tickets

---
+++ Python/Arrays/L_timeNeededBuyTicket.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def timeRequiredToBuy(tickets: List[int], k: int) -> int:
+    total = 0
+    for i, x in enumerate(tickets):
+        if i <= k:
+            total += min(tickets[i], tickets[k])
+        else:
+            total += min(tickets[i], tickets[k] - 1)
+    return total

a789f6c3496cd57e9b44fea44e02125fe91a36cb
Nitish Kumar <krnitish@live.in>
2024-04-24 22:08:21 +0530

    Updated Leetcode 1137. N-th Tribonacci Number

--- Python/Dynamic Programming/L_triboncciNumber.py
+++ Python/Dynamic Programming/L_triboncciNumber.py
@@ -5,3 +5,21 @@ def tribonacci(n: int) -> int:
         for _ in range(n - 2):
             first, second, third = second, third, first + second + third
         return third
+    
+    
+def tribonacci2(n: int) -> int:
+        memo = {}
+        
+        def helper(n):
+            if n in memo:
+                return memo[n]
+            if n == 0:
+                return 0
+            elif n == 1 or n == 2:
+                return 1
+            else:
+                result = helper(n - 1) + helper(n - 2) + helper(n - 3)
+                memo[n] = result
+                return result
+        
+        return helper(n)

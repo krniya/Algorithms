1cd57429f1b37d28e3fd16028ba846f70bbbb8b3
Nitish Kumar <krnitish@live.in>
2024-04-30 22:55:47 +0530

    Added Leetcode 1915. Number of Wonderful Substrings

---
+++ Python/Strings/L_numOfWonderfulStr.py
@@ -0,0 +1,15 @@
+def wonderfulSubstrings(word):
+    count = [0] * 1024  # 2^10 to store XOR values
+    result = 0
+    prefix_xor = 0
+    count[prefix_xor] = 1
+    
+    for char in word:
+        char_index = ord(char) - ord('a')
+        prefix_xor ^= 1 << char_index
+        result += count[prefix_xor]
+        for i in range(10):
+            result += count[prefix_xor ^ (1 << i)]
+        count[prefix_xor] += 1
+    
+    return result

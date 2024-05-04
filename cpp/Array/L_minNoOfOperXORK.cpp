6160fd14c90f53aa189cbda831129667948dfa69
Nitish Kumar <krnitish@live.in>
2024-05-04 22:55:16 +0530

    Added Leetcode CPP 2997. Minimum Number of Operations to Make Array XOR Equal to K

---
+++ cpp/Array/L_minNoOfOperXORK.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minOperations(vector<int> &nums, int k)
+{
+    int finalXor = 0;
+    // XOR of all integers in the array.
+    for (int n : nums)
+    {
+        finalXor = finalXor ^ n;
+    }
+    int count = 0;
+    // Keep iterating until both k and finalXor becomes zero.
+    while (k || finalXor)
+    {
+        // k % 2 returns the rightmost bit in k,
+        // finalXor % 2 returns the rightmost bit in finalXor.
+        // Increment counter, if the bits don't match.
+        if ((k % 2) != (finalXor % 2))
+        {
+            count++;
+        }
+        // Remove the last bit from both integers.
+        k /= 2;
+        finalXor /= 2;
+    }
+    return count;
+}

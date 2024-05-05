e6b650e7c88c08ae58a4223ea3c222485d810e0b
Nitish Kumar <krnitish@live.in>
2024-05-05 22:12:22 +0530

    Updated Leetcode 237. Delete Node in a Linked List

--- Python/Linked Lists/L_deleteNode.py
+++ Python/Linked Lists/L_deleteNode.py
@@ -5,3 +5,19 @@ def deleteNode(node):
         """
         node.val = node.next.val
         node.next = node.next.next
+        
+        
+def deleteNode1(node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        while node.next.next:
+                # Copy the value of the next node to the current node
+                node.val = node.next.val
+                # Move to the next node
+                node = node.next
+        # Copy the value of the last node to the current node
+        node.val = node.next.val
+        # Remove the last node by setting the next pointer to None
+        node.next = None

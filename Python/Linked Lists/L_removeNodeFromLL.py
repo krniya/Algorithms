abf4d96c570913e1f0f2eec48307e07654ef9620
Nitish Kumar <krnitish@live.in>
2024-05-06 23:03:48 +0530

    Added Leetcode 2487. Remove Nodes From Linked List

---
+++ Python/Linked Lists/L_removeNodeFromLL.py
@@ -0,0 +1,16 @@
+def removeNodes(head):
+        cur = head
+        stack = []
+        while cur:
+            while stack and stack[-1].val < cur.val:
+                stack.pop()
+            stack.append(cur)
+            cur = cur.next
+        
+        nxt = None
+        while stack:
+            cur = stack.pop()
+            cur.next = nxt
+            nxt = cur
+        
+        return cur

184f1ec77398e8b26b34db4537a33f676677010a
Nitish Kumar <krnitish@live.in>
2024-05-08 23:34:33 +0530

    Added Leetcode 506. Relative Ranks

---
+++ Python/Arrays/L_relativeRank.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+def findRelativeRanks(score: List[int]) -> List[str]:
+    sorted_score = sorted(score, reverse=True)
+    medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+    rank_mapping = {score: medals[i] if i < 3 else str(i + 1) for i, score in enumerate(sorted_score)}
+    return [rank_mapping[score] for score in score]

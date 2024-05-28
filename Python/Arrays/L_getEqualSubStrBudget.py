deb69bd65865c143ad4c4bd0718b912f6ce22791
Nitish Kumar <krnitish@live.in>
2024-05-28 21:31:11 +0530

    Added Leetcode 1208. Get Equal Substrings Within Budget

---
+++ Python/Arrays/L_getEqualSubStrBudget.py
@@ -0,0 +1,14 @@
+def equalSubstring(s: str, t: str, maxCost: int) -> int:
+        n = len(s)
+        ans = 0
+        window = 0
+        left = 0
+        
+        for right in range(n):
+            window += abs(ord(s[right]) - ord(t[right]))
+            while window > maxCost:
+                window -= abs(ord(s[left]) - ord(t[left]))
+                left += 1
+            ans = max(ans, right - left + 1)
+        
+        return ans

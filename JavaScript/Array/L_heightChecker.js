0e6969d02d99878aced41617a52549a984d04e1f
Nitish Kumar <krnitish@live.in>
2024-06-10 21:06:05 +0530

    Added Leetcode JS 1051. Height Checker

---
+++ JavaScript/Array/L_heightChecker.js
@@ -0,0 +1,10 @@
+var heightChecker = function (heights) {
+    let ans = 0;
+    const sorted = heights.slice().sort((a, b) => a - b);
+    for (let i = 0; i < heights.length; i++) {
+        if (sorted[i] !== heights[i]) {
+            ans++;
+        }
+    }
+    return ans;
+};

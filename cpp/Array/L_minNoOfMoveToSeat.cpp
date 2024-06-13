edb9706e2676b024c7d4b3e03902c0ae0d30900b
Nitish Kumar <krnitish@live.in>
2024-06-13 20:31:00 +0530

    Added Leetcode CPP 2037. Minimum Number of Moves to Seat Everyone

---
+++ cpp/Array/L_minNoOfMoveToSeat.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minMovesToSeat(vector<int> &seats, vector<int> &students)
+{
+    sort(seats.begin(), seats.end());
+    sort(students.begin(), students.end());
+    int ans = 0;
+    for (int i = 0; i < seats.size(); i++)
+    {
+        ans += abs(seats[i] - students[i]);
+    }
+    return ans;
+}

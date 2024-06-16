3c595dbaacd137648b4e0e181fb4ebba6074663a
Nitish Kumar <krnitish@live.in>
2024-06-16 15:04:05 +0530

    Added Leetcode CPP 330. Patching Array

---
+++ cpp/Array/L_patchingArr.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minPatches(vector<int> &nums, int n)
+{
+    long long miss = 1;
+    int result = 0;
+    size_t i = 0;
+
+    while (miss <= n)
+    {
+        if (i < nums.size() && nums[i] <= miss)
+        {
+            miss += nums[i];
+            i++;
+        }
+        else
+        {
+            miss += miss;
+            result++;
+        }
+    }
+
+    return result;
+}

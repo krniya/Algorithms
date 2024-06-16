cd2694d87356efd8b8cb19cc66b105f8cb44f8ea
Nitish Kumar <krnitish@live.in>
2024-06-16 15:01:59 +0530

    Added Leetcode 330. Patching Array

---
+++ Python/Arrays/L_patchingArr.py
@@ -0,0 +1,14 @@
+def minPatches(nums, n: int) -> int:
+    miss = 1
+    result = 0
+    i = 0
+
+    while miss <= n:
+        if i < len(nums) and nums[i] <= miss:
+            miss += nums[i]
+            i += 1
+        else:
+            miss += miss
+            result += 1
+
+    return result

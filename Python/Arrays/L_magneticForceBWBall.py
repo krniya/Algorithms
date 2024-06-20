e0bbd48aea857f6633afac4c6b3b17d90a7f0d74
Nitish Kumar <krnitish@live.in>
2024-06-20 20:56:12 +0530

    Added Leetcode 1552. Magnetic Force Between Two Balls

---
+++ Python/Arrays/L_magneticForceBWBall.py
@@ -0,0 +1,26 @@
+def maxDistance(position, m):
+        position.sort()
+        lo, hi = 1, (position[-1] - position[0]) // (m - 1)
+        ans = 1
+        
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if canWePlace(position, mid, m):
+                ans = mid
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        
+        return ans
+    
+def canWePlace(arr, dist, balls):
+    countBalls = 1
+    lastPlaced = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] - lastPlaced >= dist:
+            countBalls += 1
+            lastPlaced = arr[i]
+        if countBalls >= balls:
+            return True
+    return False
+        

afd29fb92dfa4d0595aaf4abe36e35d0204ced77
Nitish Kumar <krnitish@live.in>
2024-07-09 21:14:31 +0530

    Updated Leetcode 3. Longest Substring Without Repeating Characters

--- Python/Strings/L_longestSubStrWORep.py
+++ Python/Strings/L_longestSubStrWORep.py
@@ -2,16 +2,15 @@ from typing import List
 
 
 def longestSubString(s: str) -> int:
-        i=j=res=0
-        seen = set()
-        while j<len(s):
-            while s[j] in seen:
-                seen.remove(s[i])
-                i+=1
-            seen.add(s[j])
-            res = max(res,j-i+1)
-            j+=1
-        return res
+    res = left = 0
+    alreadyHave = set()
+    for right, ch in enumerate(s):
+        while ch in alreadyHave:
+            alreadyHave.remove(s[left])
+            left+=1
+        alreadyHave.add(ch)
+        res = max(res, right - left + 1)
+    return res
 
 
 print(longestSubString("pwwkew"))

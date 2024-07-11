f34aa746b2cde435f3d08bc1bce1ae0ffe8c64ab
Nitish Kumar <krnitish@live.in>
2024-07-11 21:57:04 +0530

    Added Leetcode 1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ Python/Strings/L_reverseStrWithinParen.py
@@ -0,0 +1,16 @@
+def reverseParentheses(s):
+    stack = []
+    for char in s:
+        if char == ')':
+            rev = ""
+            while stack and stack[-1] != '(':
+                rev += stack.pop()
+            if stack:
+                stack.pop()  # pop the opening bracket
+            for c in rev:
+                stack.append(c)
+        else:
+            stack.append(char)
+
+    return ''.join(stack)
+        

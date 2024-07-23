16226afc09e739284f74ab37197a5c35b1ae5a70
Nitish Kumar <krnitish@live.in>
2024-07-23 22:29:19 +0530

    Added Leetcode 1636. Sort Array by Increasing Frequency

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,12 +1,1 @@
-var containsDuplicate = function (nums) {
-    let visited = new Set();
-    for (let num of nums) {
-        if (visited.has(num)) {
-            return true;
-        }
-        visited.add(num);
-    }
-    return false;
-};
 
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
---
+++ Python/Arrays/L_sortArrByIncrFreq.py
@@ -0,0 +1,6 @@
+def frequencySort(nums: list[int]) -> list[int]:
+    count: list[int] = [0 for _ in range(201)]
+    for num in nums:
+        count[num + 100] += 1
+    nums.sort(key=lambda val: (count[val + 100], -val))
+    return nums

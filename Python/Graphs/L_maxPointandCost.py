93513a4e48548e6a99e4d26be8d2ef4d5a59700d
Nitish Kumar <krnitish@live.in>
2024-08-17 22:06:51 +0530

    Added Leetcode 1937. Maximum Number of Points with Cost

---
+++ Python/Graphs/L_maxPointandCost.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def maxPoints(points: List[List[int]]) -> int:
+    r, c=len(points), len(points[0])
+    for i in range(1, r):
+        right=[0]*c
+        right[-1]=points[i-1][-1]
+        for j in range(c-2, -1, -1):
+            right[j]=max(right[j+1]-1, points[i-1][j])
+        
+        left=points[i-1][0]
+        points[i][0]=max(left, right[0])+points[i][0]
+        for j in range(1, c):
+            left=max(left-1, points[i-1][j])
+            points[i][j]=max(left, right[j])+points[i][j]
+            
+    return max(points[-1])

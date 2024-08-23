25210738ddcf449b505539a3ef5d68f9adb72ed9
krniya <krnitish@live.in>
2024-08-23 21:03:51 +0530

    Added Leetcode 592. Fraction Addition and Subtraction

---
+++ Python/Strings/L_fractAddSub.py
@@ -0,0 +1,16 @@
+from math import gcd
+import re
+
+
+def fractionAddition(expression: str) -> str:
+    nums = list(map(int, re.findall(r'[+-]?\d+', expression)))
+    numerator = 0
+    denominator = 1
+    
+    for i in range(0, len(nums), 2):
+        num, den = nums[i], nums[i + 1]
+        numerator = numerator * den + num * denominator
+        denominator *= den
+    
+    common_divisor = gcd(numerator, denominator)
+    return f"{numerator // common_divisor}/{denominator // common_divisor}"

6f894aea6336354344b8f865f946cd859d37acea
krniya <krnitish@live.in>
2024-09-14 21:00:37 +0530

    Added Leetcode 2419. Longest Subarray With Maximum Bitwise AND

---
+++ Python/Bit Manipulation/L_longestSubArrWithBitAnd.py
@@ -0,0 +1,16 @@
+def longestSubarray(nums) -> int:
+    n=len(nums)
+    maxLen, xMax, Len=0, 0, 0
+    i=0
+    while i<n:
+        while i<n and nums[i]==xMax:
+            i+=1
+            Len+=1
+        if i==n or nums[i]<xMax:
+            maxLen=max(maxLen, Len)
+            Len=0
+        else:
+            xMax=nums[i]
+            maxLen=Len=1
+        i+=1
+    return maxLen

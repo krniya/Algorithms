196e3cccff3098d6c5d5ed5dfac7c552350cbe5c
Nitish Kumar <krnitish@live.in>
2024-09-18 23:21:27 +0530

    Added Leetcode 179. Largest Number

---
+++ Python/Arrays/L_largestNumber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        # Convert integers to strings
+        array = list(map(str, nums))
+        
+        # Custom sorting with a lambda function
+        array.sort(key=lambda x: x*10, reverse=True)
+        
+        # Handle the case where the largest number is "0"
+        if array[0] == "0":
+            return "0"
+        
+        # Build the largest number from the sorted array
+        largest = ''.join(array)
+        
+        return largest

9cdb09e3d141bb3ab9f7434d86ca357ab4bc19a2
krniya <krnitish@live.in>
2024-09-22 21:33:27 +0530

    Added Leetcode 440. K-th Smallest in Lexicographical Order

---
+++ Python/Tries/L_kthSmallestLexical.py
@@ -0,0 +1,20 @@
+def getReqNum(a, b, n):
+    gap = 0
+    while a <= n:
+        gap += min(n + 1, b) - a
+        a *= 10
+        b *= 10
+    return gap
+
+def findKthNumber(n: int, k: int) -> int:
+    num = 1
+    i = 1
+    while i < k:
+        req = getReqNum(num, num + 1, n)
+        if i + req <= k:
+            i += req
+            num += 1
+        else:
+            i += 1
+            num *= 10
+    return num

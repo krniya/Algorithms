0e325167632525060face41191b02d55aeaec791
krniya <krnitish@live.in>
2024-10-01 18:37:19 +0530

    Added Leetcode JS 1497. Check If Array Pairs Are Divisible by k

---
+++ JavaScript/Array/L_checkIfArrPairDivByK.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {boolean}
+ */
+var canArrange = function (arr, k) {
+    const freq = new Array(k).fill(0);
+
+    for (const num of arr) {
+        let rem = num % k;
+        if (rem < 0) {
+            rem += k;
+        }
+        freq[rem]++;
+    }
+
+    if (freq[0] % 2 !== 0) {
+        return false;
+    }
+
+    for (let i = 1; i <= Math.floor(k / 2); i++) {
+        if (freq[i] !== freq[k - i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
